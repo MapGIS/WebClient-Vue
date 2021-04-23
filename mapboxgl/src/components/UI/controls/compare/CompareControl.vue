5e8657f0b73bb86b4aff847197a28e0765e01c6c
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-04-23 14:05:56 +0800

    【SDK】【MapboxGL】【实现左右地图槽机制的地图卷帘控制】

--- mapboxgl/src/components/UI/controls/compare/CompareControl.vue
+++ mapboxgl/src/components/UI/controls/compare/CompareControl.vue
@@ -1,118 +1,122 @@
-<template>
-    <div class="mapgis-compare-bar">
-        <slot></slot>
-    </div>
-</template>
-
 <script>
 import Compare from "@mapgis/mapbox-gl-compare";
-// const Compare = require('mapbox-gl-compare');
-import controlMixin from "../controlMixin";
-
-const StateEvents = {
-    update: "update",
-};
 
 export default {
-    name: "mapbox-compare",
-    mixins: [controlMixin],
-    props: {
-        default: {
-            type: Boolean,
-            default: true,
-        },
-        orientation: {
-            type: String,
-            default: "vertical",
-        },
+  name: "mapgis-compare",
+  // mixins: [controlMixin],
+  props: {
+    target: {
+      type: String,
+      default: "mapgis-compare-default-root"
     },
-    components: {},
+    default: {
+      type: Boolean,
+      default: true
+    },
+    orientation: {
+      type: String,
+      default: "vertical"
+    }
+  },
+  components: {},
 
-    data() {
-        return {
-            initial: true,
-            control: undefined,
-        };
-    },
+  data() {
+    return {
+      initial: true,
+      control: undefined,
+      beforeMap: undefined,
+      afterMap: undefined
+    };
+  },
 
-    created() {
-        this.control = new CompareControl(this.$props);
-        this.$_addControl();
-        let events = Object.keys(StateEvents);
-        this.$_bindSelfEvents(events, this.control);
-    },
+  created() {},
 
-    methods: {
-        /**
-         * @param {Object} a The first Mapbox GL Map
-         * @param {Object} b The second Mapbox GL Map
-         * @param {string|HTMLElement} container An HTML Element, or an element selector string for the compare container. It should be a wrapper around the two map Elements.
-         * @param {Object} options
-         * @param {string} [options.orientation=vertical] The orientation of the compare slider. `vertical` creates a vertical slider bar to compare one map on the left (map A) with another map on the right (map B). `horizontal` creates a horizontal slider bar to compare on mop on the top (map A) and another map on the bottom (map B).
-         * @param {boolean} [options.mousemove=false] If `true` the compare slider will move with the cursor, otherwise the slider will need to be dragged to move.
-         * @example
-         * var compare = new mapboxgl.Compare(beforeMap, afterMap, '#wrapper', {
-         *   orientation: 'vertical',
-         *   mousemove: true
-         * });
-         * @see [Swipe between maps](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-compare/)
-         */
-        compare(a, b, container, options) {
-            console.log("对比", Compare);
-            console.log("参数", a, b, container, options);
-            this.$compare = new Compare(a, b, container, options);
-        },
-        handleMap(map, parent) {
-            if (map[0] && map[1]) {
-                if (this.$compare) {
-                    this.$compare && this.$compare.remove();
-                    this.$compare = undefined;
-                }
-                this.compare(map[0], map[1], parent, {
-                    // Set this to enable comparing two maps by mouse movement:
-                    // mousemove: true,
-                    orientation: this.orientation,
-                });
-            }
-        },
+  mounted() {
+    this.bindEvent();
+  },
+
+  methods: {
+    /**
+     * @param {Object} a The first Mapbox GL Map
+     * @param {Object} b The second Mapbox GL Map
+     * @param {string|HTMLElement} container An HTML Element, or an element selector string for the compare container. It should be a wrapper around the two map Elements.
+     * @param {Object} options
+     * @param {string} [options.orientation=vertical] The orientation of the compare slider. `vertical` creates a vertical slider bar to compare one map on the left (map A) with another map on the right (map B). `horizontal` creates a horizontal slider bar to compare on mop on the top (map A) and another map on the bottom (map B).
+     * @param {boolean} [options.mousemove=false] If `true` the compare slider will move with the cursor, otherwise the slider will need to be dragged to move.
+     * @example
+     * var compare = new mapboxgl.Compare(beforeMap, afterMap, '#wrapper', {
+     *   orientation: 'vertical',
+     *   mousemove: true
+     * });
+     * @see [Swipe between maps](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-compare/)
+     */
+    compare(a, b, container, options) {
+      console.log("对比", Compare);
+      console.log("参数", a, b, container, options);
+      this.$compare = new Compare(a, b, container, options);
     },
+    bindEvent() {
+      if (this.$slots.beforeMap && this.$slots.afterMap) {
+        let left = this.$slots.beforeMap[0];
+        const beforeListeners = left.componentOptions.listeners;
+        left.componentOptions.listeners = Object.assign({}, beforeListeners, {
+          load: this.handleBeforeMapLoad
+        });
+        let right = this.$slots.afterMap[0];
+        const afterListeners = right.componentOptions.listeners;
+        right.componentOptions.listeners = Object.assign({}, afterListeners, {
+          load: this.handleAfterMapLoad
+        });
+      }
+    },
+    handleBeforeMapLoad(payload) {
+      this.beforeMap = payload.map;
+      this.handleMap();
+    },
+    handleAfterMapLoad(payload) {
+      this.afterMap = payload.map;
+      this.handleMap();
+    },
+    handleMap() {
+      if (this.beforeMap && this.afterMap) {
+        let parent = document.getElementById(this.target);
+        this.compare(this.beforeMap, this.afterMap, parent, {
+          // Set this to enable comparing two maps by mouse movement:
+          // mousemove: true,
+          orientation: this.orientation
+        });
+      }
+    }
+  },
+
+  render(h) {
+    let children;
+    if (this.$slots.beforeMap && this.$slots.afterMap) {
+      this.bindEvent();
+      children = [this.$slots.beforeMap, this.$slots.afterMap];
+    }
+    return h(
+      "div",
+      {
+        class: "mapgis-compare-bar",
+        attrs: {
+          id: this.target
+        }
+      },
+      children
+    );
+  }
 };
-
-class CompareControl {
-    constructor(option) {
-        this.option = option || {};
-    }
-
-    initDom(dom) {
-        dom.className = "mapboxgl-ctrl mapboxgl-compare-bar";
-        dom.style.display = "flex";
-
-        const controls = [""];
-    }
-
-    bindEvent() {}
-
-    unbindEvent() {}
-
-    onAdd(map) {
-        this._map = map;
-        this._container = document.createElement("div");
-        this._container.className = "mapboxgl-ctrl";
-        this.initDom(this._container);
-        this.bindEvent();
-        return this._container;
-    }
-
-    onRemove() {
-        this.unbindEvent();
-        this._container.parentNode.removeChild(this._container);
-        this._map = undefined;
-    }
-}
 </script>
-<style>
+<style lang="css">
 .mapgis-compare-bar {
-    position: relative;
-    height: 100vh;
+  position: relative;
+  height: 100vh;
+}
+.mapgis-compare-bar .mapboxgl-map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
 }
 </style>
