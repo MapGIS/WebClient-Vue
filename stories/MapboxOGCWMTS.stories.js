f47fa2c269e279a694cf9833a8ee51d2697640e0
killer1992 <killer1992@yeah.net>
2021-05-08 17:01:35 +0800

    【SDK】【mapbox-vue】【杨琨】【新增要素服务的MD文档】

--- stories/MapboxOGCWMTS.stories.js
+++ stories/MapboxOGCWMTS.stories.js
@@ -6,10 +6,14 @@ export default {
   component: MapgisOgcWmtsLayer,
   argTypes: {
     layer: {},
-    layerId: "igsLayer_layerId",
-    sourceId: "igsLayer_sourceId",
-    url:
-      "http://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk=2ddaabf906d4b5418aed0078e1657029",
+    baseUrl:
+        "http://develop.smaryun.com:6163/igs/rest/ogc/beijing/WMTSServer",
+    tileMatrixSet:"EPSG:4326_北京市_arcgis_GB",
+    wmtsLayer:"beijing",
+    version:"1.0.0",
+    wmtsStyle:"default",
+    format:"image/png",
+    zoomOffset:-1
   },
 };
 
@@ -18,19 +22,28 @@ const Template = (args, { argTypes }) => ({
   components: { MapgisWebMap, MapgisOgcWmtsLayer },
   template: `<mapgis-web-map crs="EPSG:4326">
     <mapgis-ogc-wmts-layer v-bind="$props" />
+    <button @click="test" style="position: absolute;left: 0;top: 0;z-index: 999;">改值</button>
+    <button @click="test2" style="position: absolute;left: 60px;top: 0;z-index: 999;">改值</button>
   </mapgis-web-map>`,
+  methods:{
+    test(){
+      this.$props.wmtsStyle = "qqqqq"
+    },
+    test2(){
+      this.$props.wmtsStyle = "default"
+    }
+  }
 });
 
 export const Image = Template.bind({});
 Image.args = {
   layerId: "tdt",
-  url:
-    "http://t0.tianditu.gov.cn/img_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=c&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk=2ddaabf906d4b5418aed0078e1657029",
-};
-
-export const Vector = Template.bind({});
-Vector.args = {
-  layerId: "tdt_layerid",
-  url:
-    "http://t0.tianditu.gov.cn/vec_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=c&FORMAT=tiles&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}&tk=2ddaabf906d4b5418aed0078e1657029",
+  baseUrl:
+    "http://develop.smaryun.com:6163/igs/rest/ogc/beijing/WMTSServer",
+  tileMatrixSet:"EPSG:4326_北京市_arcgis_GB",
+  wmtsLayer:"beijing",
+  version:"1.0.0",
+  wmtsStyle:"default",
+  format:"image/png",
+  zoomOffset:-1
 };
