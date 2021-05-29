5de7d32d06fa15a49648e1528106df90a39a5858
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-05-29 16:50:28 +0800

    【SDK】【MapboxGL】【实现通用的mapgis-iconfont图标组件】

---
+++ mapboxgl/src/ui.js
@@ -0,0 +1,20 @@
+export { MapgisIconfont } from "./components/UI/iconfont";
+
+import { default as Slider } from "ant-design-vue/es/slider";
+
+import { default as Select } from "ant-design-vue/es/select";
+let SelectOption = Select.Option;
+let SelectOptGroup = Select.OptGroup;
+
+import { default as Input } from "ant-design-vue/es/input";
+let InputGroup = Input.Group;
+let InputSearch = Input.Search;
+let InputTextArea = Input.TextArea;
+let InputPassword = Input.Password;
+
+export { Slider };
+export { Select, SelectOption, SelectOptGroup };
+export { Input, InputGroup, InputSearch, InputTextArea, InputPassword };
+
+export { default as Tree } from "ant-design-vue/es/tree/Tree";
+export { default as DirectoryTree } from "ant-design-vue/es/tree/DirectoryTree";
