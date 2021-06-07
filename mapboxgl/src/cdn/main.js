5bec924a2b967d0687ccea9cb19313837bcd66e8
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-06-07 20:10:56 +0800

    【SDK】【编译】【新增MapboxGL-cdn的引入方式】

--- mapboxgl/src/cdn/main.js
+++ mapboxgl/src/cdn/main.js
@@ -1,20 +1,7 @@
-import "ant-design-vue/dist/antd.css";
-import antDirective from "ant-design-vue/es/_util/antDirective";
-
 import * as MapComponents from "./component";
-import * as UIComponents from "./ui";
 
 const install = function(Vue, options) {
   options = options || {};
-  const { ui } = options;
-
-  if (ui) {
-    Vue.use(antDirective);
-    for (let name in UIComponents) {
-      const ui = UIComponents[name];
-      Vue.component(ui.options ? ui.options.name : ui.name, ui);
-    }
-  }
 
   for (let name in MapComponents) {
     const com = MapComponents[name];
