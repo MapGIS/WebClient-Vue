b95e46ca3117dce2115a5309e15b4ec59a8d6c7c
zhangying-zy618 <1057936309@qq.com>
2021-06-07 21:37:31 +0800

    【cdn添加echarts相关】

---
+++ mapboxgl/src/components/overlay/echarts/EchartsLayer.vue
@@ -0,0 +1,61 @@
+<template>
+  <div></div>
+</template>
+<script>
+import {EchartsLayer} from "./cdn/EchartsLayer";
+
+export default {
+  name: "mapgis-echarts",
+  props: {
+    options: {
+      type: Object,
+      default: () => {
+        return {
+          mapboxgl: {
+            roam: true
+          },
+          series: [{
+            // 坐标系
+            coordinateSystem: 'mapboxgl',
+            type: 'lines'
+          }]
+        }
+      }
+    }
+  },
+  inject: ["mapbox", "map"],
+  methods: {
+    createMapboxObject() {
+      const {map} = this;
+      return new EchartsLayer(map, this.options);
+    },
+    watchProp() {
+      this.$watch("options", {
+        handler(next){
+          if (!this.echartsLayer) {
+            return;
+          }
+          this.echartsLayer.update(next);
+        },
+        deep:true,
+        immediate:true
+      })
+    },
+    mount() {
+      this.echartsLayer = this.createMapboxObject();
+      this.echartsLayer.addTo(this.map);
+    },
+    unmount() {
+      const { echartsLayer} = this;
+      return echartsLayer.remove();
+    }
+  },
+  created() {
+    this.mount();
+    this.watchProp();
+  },
+  destroyed() {
+    this.unmount();
+  }
+};
+</script>
