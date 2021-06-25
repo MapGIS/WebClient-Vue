b45865e1b0c3a7d1590d2e15e25049199db19860
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-06-25 16:36:55 +0800

    【SDK】【Cesium】【记录vueKey到Cesium的webglobe对象上】

--- cesium/src/components/WebGlobe/WebGlobe.vue
+++ cesium/src/components/WebGlobe/WebGlobe.vue
@@ -117,6 +117,7 @@ export default {
       }
 
       this.webGlobe = webGlobe;
+      webGlobe.vueKey = vueKey;
       if (cameraView) {
         webGlobe.viewer.scene.camera.setView(cameraView);
       }
