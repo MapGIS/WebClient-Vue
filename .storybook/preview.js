8455fe0026778dc43bfc9fcdc3d171e349694ebd
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-06-28 11:59:41 +0800

    【UI】【修复】【修复UI的环境以便于支持故事模式的按需引入】

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,14 @@
-import Vue from 'vue';
+import Vue from "vue";
 
-import '@mapgis/mapbox-gl/dist/mapbox-gl.css';
-import MapgisMapboxComponents from './components/mapbox';
-import MapgisCesiumComponents from './components/cesium';
-import MapgisUIComponents from './components/ui';
+import "@mapgis/mapbox-gl/dist/mapbox-gl.css";
+import MapgisMapboxComponents from "./components/mapbox";
+import MapgisCesiumComponents from "./components/cesium";
+import AntdUIComponents from "./components/ui";
 
+import "../ui/dist-libs/webclient-vue-ui.css";
+import MapgisUIComponents from "../ui/src/index";
+
+Vue.use(AntdUIComponents, {});
 Vue.use(MapgisUIComponents, {});
 Vue.use(MapgisMapboxComponents, {});
 Vue.use(MapgisCesiumComponents, {});
