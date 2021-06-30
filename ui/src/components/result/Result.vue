87dbe7df62a1c0d5cf1f682bcc4f6572f5e5e1c8
王魁帅 <2692537835@qq.com>
2021-06-30 11:53:45 +0800

    【SDK】【UI】【补充Feedback类别的部分组件】

---
+++ ui/src/components/result/Result.vue
@@ -0,0 +1,20 @@
+<script>
+import Result, { ResultProps } from 'ant-design-vue/es/result';
+
+import PropTypes from "../../util/vue-types";
+import AntdMixin from "../../mixin/AntdMixin";
+import ThemeMixin from "../../mixin/ThemeMixin";
+
+export const resultTypes = {
+  ...ResultProps
+};
+
+export default {
+  name: "mapgis-ui-result",
+  wrapperUI: Result,
+  mixins: [AntdMixin, ThemeMixin],
+  inheritAttrs: false,
+  props: resultTypes,
+  methods: {}
+};
+</script>
