501267840dcd4c6edabdb5947b0938bcd682f79c
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-07-07 21:16:01 +0800

    【SDK】【MapboxGL】【新增MVTStyleLayer的样式的合并行为能力&故事模式】

--- mapboxgl/src/components/layer/vectortile/MvtStyleLayer.vue
+++ mapboxgl/src/components/layer/vectortile/MvtStyleLayer.vue
@@ -4,6 +4,7 @@
 
 <script>
 import withEvents from "../../../lib/withEvents";
+import { deepEqual } from "../../util/util";
 
 export default {
   name: "mapgis-mvt-style-layer",
@@ -16,7 +17,7 @@ export default {
     },
     mode: {
       type: String,
-      default: "add" // add set
+      default: "merge" // add set merge
     },
     before: {
       type: String
@@ -26,10 +27,11 @@ export default {
   watch: {
     mvtStyle: {
       handler(next) {
-        this.remove();
-        this.$_initStyle(this.mode, next);
-      },
-      deep: true
+        if (!deepEqual(next, this.mvtStyle)) {
+          this.remove();
+          this.$_initStyle(this.mode, next);
+        }
+      }
     }
   },
 
@@ -69,12 +71,24 @@ export default {
     async compareStyle(mvtStyle) {
       let oldStyle = this.map.getStyle();
       let newStyle = await this.$_getStyleObject(mvtStyle);
+      let newLayer = [];
+      switch (this.mode) {
+        case "add":
+          newLayer = this.addLayers(oldStyle.layers, newStyle.layers);
+          break;
+        case "merge":
+          newLayer = this.mergeLayers(oldStyle.layers, newStyle.layers);
+          break;
+      }
+
+      console.log("news", newLayer);
+
       let style = {
         version: oldStyle.version || newStyle.version,
         sprite: oldStyle.sprite || newStyle.sprite,
         glyphs: oldStyle.glyphs || newStyle.glyphs,
         sources: { ...oldStyle.sources, ...newStyle.sources },
-        layers: this.mergeLayers(oldStyle.layers, newStyle.layers)
+        layers: newLayer
       };
       return style;
     },
@@ -93,7 +107,7 @@ export default {
       return style;
     },
 
-    mergeLayers(olds, news) {
+    addLayers(olds, news) {
       news = news || [];
       if (!olds) return [].concat(news);
       let filters = olds.filter(layer => {
@@ -103,9 +117,24 @@ export default {
       return filters.concat(news);
     },
 
+    mergeLayers(olds, news) {
+      news = news || [];
+      if (!olds) return [].concat(news);
+      let merges = olds.map(layer => {
+        let find = news.find(l => l.id === layer.id);
+        return find ? find : layer;
+      });
+      console.log("merges", news, merges);
+      let unmerges = news.filter(layer => {
+        let find = merges.find(l => l.id === layer.id);
+        return find ? false : true;
+      });
+      return merges.concat(unmerges);
+    },
+
     $_initStyle(mode, style) {
       mode = mode || this.mode;
-      if (mode === "add") {
+      if (mode === "add" || mode === "merge") {
         this.$_addStyle(style);
       } else if (mode == "set") {
         this.$_setStyle(style);
--- mapboxgl/src/components/util/util.js
+++ mapboxgl/src/components/util/util.js
@@ -13,3 +13,29 @@ export const newGuid = function() {
   }
   return guid;
 };
+
+/**
+ * 深比较内容
+ * @param {*} a 对象a
+ * @param {*} b 对象b
+ * @returns 是否相等
+ */
+export function deepEqual(a, b) {
+  if (Array.isArray(a)) {
+    if (!Array.isArray(b) || a.length !== b.length) return false;
+    for (let i = 0; i < a.length; i++) {
+      if (!deepEqual(a[i], b[i])) return false;
+    }
+    return true;
+  }
+  if (typeof a === "object" && a !== null && b !== null) {
+    if (!(typeof b === "object")) return false;
+    const keys = Object.keys(a);
+    if (keys.length !== Object.keys(b).length) return false;
+    for (const key in a) {
+      if (!deepEqual(a[key], b[key])) return false;
+    }
+    return true;
+  }
+  return a === b;
+}
