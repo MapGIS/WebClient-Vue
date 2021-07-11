05d98503a45cf304ac3d2758efb30fd6d745d615
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-07-11 23:40:44 +0800

    【故事】【新增】【新增高级布局故事示例】

---
+++ stories/pro/layout/UIProLayout.stories.js
@@ -0,0 +1,25 @@
+import MapgisUiLayoutPro from "../../../ui/src/layout/base/BaseLayout.vue";
+
+export default {
+  title: "Pro/布局",
+  component: MapgisUiRow,
+  argTypes: {},
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { MapgisUiLayoutPro },
+  data() {
+    return {
+      value: 0,
+    };
+  },
+  template: `<div id="components-layout-demo-basic">
+    <mapgis-ui-layout-pro>
+
+    </mapgis-ui-layout-pro>
+  </div>`,
+});
+
+export const 基础使用 = Template.bind({});
+基础使用.args = {};
