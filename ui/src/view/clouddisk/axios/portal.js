c90b06e5d6cba0073a6afe2ef1fe38135fe22c59
王魁帅 <2692537835@qq.com>
2021-07-31 18:02:29 +0800

    【SDK】【UI】【对接云门户数据资源目录】

--- ui/src/view/clouddisk/axios/portal.js
+++ ui/src/view/clouddisk/axios/portal.js
@@ -1,9 +1,5 @@
 import API from "./API";
-import { getMapGISUrl, getMapgisToken } from "../config/mapgis";
-
-export function getPortalUrl () {
-  return 'http://192.168.176.1:6160/'
-}
+import { getPortalUrl, getMapGISUrl, getMapgisToken } from "../config/mapgis";
 
 /**
  * 获取门户的数据资源
@@ -12,12 +8,16 @@ export function getPortalUrl () {
  * @param {*} pageSize 
  */
 export function getPortalResource(resourceType, page, pageSize) {
-  const url = getPortalUrl();
+  let url = getPortalUrl();
   const api = new API();
   // const token = getMapgisToken();
   api.setBaseUrl(url);
   // api.setAuthorization(token);
+  let token = sessionStorage.getItem("mapgis-cloud-portal-jwt");
+  console.warn('【门户】【UI组件】【是否可以获取到session】', token);
+  api.setAuthorization(token);
   return api.get(
-    "/portal/ptResource/page?resourceType=" + resourceType + "&page=" + page + "&pageSize=" + pageSize + "&orderBy=create_time&search=&tagSearch="
+    "/portal/ptResource/page?resourceType=" + resourceType + "&page=" + page + "&pageSize=" + pageSize + "&datasourceType=1"
   );
+  // + '&Authorization=' + token
 }
