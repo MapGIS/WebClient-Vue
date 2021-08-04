0080195f86c4d9890c6c8a1cb0b5f51888796888
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-08-04 21:51:36 +0800

    【SDK】【UI】【新增Ribbon风格布局与组件】

---
+++ ui/src/components/ribbon/RibbonGallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mapgis-ui-ribbon-gallery"></div>
+</template>
+
+<script>
+import RibbonOption from "./RibbonOptions";
+export default {
+  name: "mapgis-ui-ribbon-gallery",
+  props: {
+    ...RibbonOption
+  },
+  created() {},
+  methods: {}
+};
+</script>
---
+++ ui/src/components/ribbon/RibbonOptions.js
@@ -0,0 +1,18 @@
+export default {
+  title: {
+    type: String,
+    default: "控制命令"
+  },
+  icon: {
+    type: String,
+    default: "mapgis-ArcGISzhituchengguozhuanhuangongju"
+  },
+  action: {
+    type: String,
+    default: "undefined"
+  },
+  description: {
+    type: String,
+    default: "当前命令描述"
+  }
+};
