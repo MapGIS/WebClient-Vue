983afdd8c57167c776b22016ae5309f73f521740
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-08-05 15:39:24 +0800

    【SDK】【MapboxGL】【优化聚类组件的自定槽&实现要素信息的查询与回调】

--- mapboxgl/docs/api/Layers/Cluster/cluster.md
+++ mapboxgl/docs/api/Layers/Cluster/cluster.md
@@ -15,18 +15,33 @@
 该功能的聚类的文字显示需要在 mapgis-web-map 的 style 中设置如下信息 [地图样式]()
 
 ```json
- {
-    version: 8,
-    sources: {},
-    layers: [ //自己的图层 ],
-    sprite: "http://localhost:6163/igs/rest/mrms/vtiles/sprite",
-    glyphs: "http://localhost:6163/igs/rest/mrms/vtiles/fonts/{fontstack}/{range}.pbf", //没有该字体信息无法显示聚类文字
+{
+  "version": 8,
+  "sources": {},
+  "layers": [
+    /*自己的图层 */
+  ],
+  "sprite": "http://localhost:6163/igs/rest/mrms/vtiles/sprite",
+  "glyphs": "http://localhost:6163/igs/rest/mrms/vtiles/fonts/{fontstack}/{range}.pbf" //没有该字体信息无法显示聚类文字
 }
 ```
 
 :::
 
 ## 属性
+
+### `feature`
+
+- **类型:** `Object`
+- **V-Modal 属性**
+- **描述:** 非聚类要素鼠标移入的时候查询对应的要素信息并返回给外部，该属性是`v-modal`属性， 改属性会随着人机交互实时变动
+- **默认值**
+  ```json
+  {
+    "coordinates": [0, 0],
+    "properties": { "属性名": "属性值" }
+  }
+  ```
 
 ### `geojson`
 
@@ -69,6 +84,73 @@
 - **非侦听属性**
 - **默认值** 100
 - **描述:** 聚类的最大值，如 1000 个数据按照聚类范围进行聚类，100 作为最小值表示 100 以上的聚类簇颜色和半径不再单独出处理
+
+### `uncluster`
+
+- **类型:** `Number`
+- **非侦听属性**
+- **默认值**
+  ```json
+  {
+    "paint": {
+      "circle-color": "#000000",
+      "circle-radius": 6,
+      "circle-stroke-width": 1,
+      "circle-stroke-color": "#fff"
+    }
+  }
+  ```
+- **描述:** 自定义非聚类的样式
+
+## 槽
+
+### `uncluster`
+
+- **描述:** 自定义非聚类点的`popup槽`，当鼠标移入的时候触发对应的 popup 弹出事件
+- **作用域** feature 选中要素的几何和属性信息
+
+![非聚类槽](./uncluster_popup.png)
+
+```vue
+<template>
+  <mapgis-cluster-layer
+      :geojson="clusterGeojson"
+      :clusterRadius="50"
+      v-model="unclusterfeature"
+    >
+    <mapgis-ui-border type="border5" class="mapgis-popup-custom">
+      <mapgis-ui-row
+        v-for="(f, i) in Object.keys(unclusterfeature.properties)"
+        :key="i"
+        class="mapgis-custom-row"
+      >
+        <mapgis-ui-col :span="12">
+          {{ f }}
+        </mapgis-ui-col>
+        <mapgis-ui-col :span="12">
+          {{ unclusterfeature.properties[f] }}
+        </mapgis-ui-col>
+      </mapgis-ui-row>
+    </mapgis-ui-border>
+  </mapgis-cluster-layer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      clusterGeojson: "http://localhost:8080/data/chinamobile_1000.geojson",
+      unclusterfeature: { coordinates: [0, 0], properties: { 属性名: "属性值" } },
+    }
+  }
+}
+<script>
+<style>
+.mapgis-custom-row {
+  height: 20px;
+  background: #03b3c4;
+}
+</style>
+```
 
 ## 事件
 
--- mapboxgl/src/components/layer/cluster/ClusterLayer.vue
+++ mapboxgl/src/components/layer/cluster/ClusterLayer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    <slot :coordinates="coordinates" :properties="properties"></slot>
+  </div>
 </template>
 
 <script>
@@ -28,22 +30,62 @@ export default {
     max: {
       type: Number,
       default: 100
+    },
+    uncluster: {
+      type: Object,
+      default: () => {
+        return {
+          paint: {
+            "circle-color": "#000000",
+            "circle-radius": 6,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff"
+          }
+        };
+      }
+    },
+    feature: {
+      coordinates: [0, 0],
+      properties: { 属性名: "属性值" }
     }
   },
   data() {
     return {
-      id: uuid()
+      id: uuid(),
+      coordinates: [0, 0],
+      properties: {
+        属性名: "属性值"
+      }
     };
+  },
+  model: {
+    prop: "feature",
+    event: "change-feature"
   },
   mounted() {
     this.$_deferredMount();
+  },
+  watch: {
+    coordinates: {
+      handler(next) {},
+      deep: true
+    }
   },
   beforeDestroy() {
     this.$_undeferredMount();
   },
   methods: {
     $_deferredMount() {
-      let { geojson, min, max, id, clusterMaxZoom, clusterRadius, map } = this;
+      let {
+        geojson,
+        min,
+        max,
+        id,
+        clusterMaxZoom,
+        clusterRadius,
+        uncluster,
+        map
+      } = this;
       let source = {
         type: "geojson",
         data: geojson,
@@ -54,7 +96,7 @@ export default {
 
       map.addSource(id, source);
       map.addLayer({
-        id: id + "_circle",
+        id: id + "_cluster_circle",
         type: "circle",
         source: id,
         filter: ["has", "point_count"],
@@ -96,17 +138,98 @@ export default {
           "text-halo-width": 2
         }
       });
+
+      if (uncluster && uncluster.icon) {
+        map.addLayer({
+          id: id + "_uncluster_icon",
+          type: "symbol",
+          source: id,
+          filter: ["!", ["has", "point_count"]],
+          layout: {
+            "icon-image": uncluster.icon,
+            "icon-size": 1
+          }
+        });
+      } else {
+        map.addLayer({
+          id: id + "_uncluster_circle",
+          type: "circle",
+          source: id,
+          filter: ["!", ["has", "point_count"]],
+          paint: uncluster.paint
+        });
+      }
+
       this.$emit("added", {
         map,
         component: this,
-        layerIds: [id + "_circle", id + "_label"]
+        layerIds: [
+          id + "_cluster_circle",
+          id + "_label",
+          id + "_uncluster_circle",
+          id + "_uncluster_icon"
+        ]
       });
+
+      this.$_bindEvent();
     },
     $_undeferredMount() {
-      let { map, id } = this;
-      map.removeLayer(id + "_circle");
+      let { map, id, uncluster } = this;
+      map.removeLayer(id + "_cluster_circle");
       map.removeLayer(id + "_label");
+      if (uncluster && uncluster.icon) {
+        map.removeLayer(id + "_uncluster_icon");
+      } else {
+        map.removeLayer(id + "_uncluster_circle");
+      }
       map.removeSource(id);
+      this.$_unbindEvent();
+    },
+    $_bindEvent() {
+      const vm = this;
+      let { map, id } = this;
+      let uncluster_circle = id + "_uncluster_circle";
+      // let uncluster_icon = id + "_uncluster_icon";
+      map.on("mouseenter", uncluster_circle, function(e) {
+        if (!e.features || e.features.length <= 0) return;
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var properties = e.features[0].properties;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        /* let feature = e.features[0];
+        feature.coordinates = coordinates; */
+        vm.coordinates = coordinates;
+        vm.properties = properties;
+
+        vm.$emit("change-feature", {
+          coordinates: coordinates,
+          properties: properties
+        });
+
+        vm.$nextTick(() => {
+          vm.updatePopup();
+        });
+      });
+    },
+    $_unbindEvent() {
+      let { map, id } = this;
+      let uncluster_circle = id + "_uncluster_circle";
+      map.off("mouseenter", uncluster_circle, function(e) {});
+    },
+    updatePopup() {
+      const { map, mapbox, coordinates } = this;
+      if (this.popup) {
+        this.popup.remove();
+      }
+      if (this.$slots.default !== undefined) {
+        this.popup = new mapbox.Popup({ closeButton: true })
+          .setLngLat(coordinates)
+          .setDOMContent(this.$slots.default[0].elm)
+          .addTo(map);
+      }
     }
   }
 };
