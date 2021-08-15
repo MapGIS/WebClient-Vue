049066729d6812be862e5c187fdce6c8b88cd9eb
liao-x-d <1627930520@qq.com>
2021-08-15 23:00:14 +0800

    【draw组件】【增加】【自带界面和update-radius事件】

--- mapboxgl/src/components/UI/controls/draw/BaseDraw.vue
+++ mapboxgl/src/components/UI/controls/draw/BaseDraw.vue
@@ -4,12 +4,51 @@
     <slot name="toolbar" />
     <!-- slot for toolbar-item -->
     <slot v-if="drawer" />
+    <div class="mapgis-default-control">
+      <mapgis-ui-space>
+        <mapgis-ui-tooltip v-for="(item, i) in draws" :key="i" placement="bottom">
+          <template slot="title">
+            <span>{{ item.tip }}</span>
+          </template>
+          <mapgis-ui-button
+              shape="circle"
+              :type="item.type"
+              @click="item.click"
+              :class="item.className"
+          >
+            <icon-font :type="item.icon" :class="item.className" theme="filled"/>
+          </mapgis-ui-button>
+        </mapgis-ui-tooltip>
+      </mapgis-ui-space>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+
+.mapgis-ui-space{
+  width: 40px!important;
+  overflow: hidden;
+  transition: width .5s ;
+}
+
+.bigbutton.mapgis-ui-btn{
+  width: 40px!important;
+  height: 40px!important;
+}
+.bigbutton.anticon{
+  font-size: 19px!important;
+}
+
+</style>
 
 <script>
+import { Icon } from 'ant-design-vue';
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2743527_adfkxoozjnc.js',
+});
+import * as turf from "@turf/turf";
+import mapboxgl from "@mapgis/mapbox-gl"
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import DrawRectangle from "mapbox-gl-draw-rectangle-mode";
 // import { CircleMode, DragCircleMode } from "mapbox-gl-draw-circle";
@@ -21,6 +60,7 @@ import {
   DirectMode,
   SimpleSelectMode
 } from "mapbox-gl-draw-circle";
+import StaticMode from "@mapbox/mapbox-gl-draw-static-mode";
 const modes = MapboxDrawCom.default.modes;
 const MapboxDraw = MapboxDrawCom.default;
 modes.draw_rectangle = DrawRectangle;
@@ -29,6 +69,7 @@ modes.draw_radius = CircleMode;
 modes.drag_radius = DragCircleMode;
 modes.direct_select = DirectMode;
 modes.simple_select = SimpleSelectMode;
+modes.static = StaticMode;
 
 import drawMixin from "./drawMixin";
 import controlMixin from "../withControlEvents";
@@ -60,7 +101,9 @@ const drawEvents = {
 export default {
   name: "mapgis-draw",
   mixins: [drawMixin, controlMixin],
-
+  components:{
+    IconFont
+  },
   //@see https://cn.vuejs.org/v2/guide/components-edge-cases.html#%E4%BE%9D%E8%B5%96%E6%B3%A8%E5%85%A5
   // inject: ["mapbox", "map"],
 
@@ -75,6 +118,10 @@ export default {
   },
 
   props: {
+    enableControl: {
+      type: Boolean,
+      default: true
+    },
     // mapbox drawer options
     keybindings: {
       type: Boolean,
@@ -135,7 +182,76 @@ export default {
     return {
       initial: true,
       drawer: undefined,
-      oldStyles: DefaultDrawStyle
+      oldStyles: DefaultDrawStyle,
+      draws: [
+        {
+          icon: "icon-huizhi1",
+          type: "primary",
+          tip:"展开",
+          click: this.changeFold,
+          className:"bigbutton"
+        },
+        {
+          icon: "icon-huizhidian2",
+          type: "primary",
+          tip: "画点",
+          click: this.togglePoint
+        },
+        {
+          icon: "icon-huizhixian1",
+          type: "primary",
+          tip: "画线",
+          click: this.togglePolyline
+        },
+        {
+          icon: "icon-huizhijuxing",
+          type: "primary",
+          tip: "画矩形",
+          click: this.toggleRect
+        },
+        {
+          icon: "icon-draw-polygon",
+          type: "primary",
+          tip: "画多边形",
+          click: this.togglePolygon
+        },
+        {
+          icon: "icon-huizhiyuan1",
+          type: "primary",
+          tip: "画圆",
+          click: this.toggleCircle
+        },
+        {
+          icon: "icon-icon_huizhiyuanxing",
+          type: "primary",
+          tip: "画半径",
+          click: this.toggleRadius
+        },
+        {
+          icon: "icon-shanchu_dianji",
+          type: "primary",
+          tip: "删除选中图元",
+          click: this.toggleDelete
+        }
+        /*{
+          icon: "mapgis-shanchudangqianziceng",
+          type: "primary",
+          tip: "删除全部",
+          click: this.toggleDeleteAll
+        },
+         {
+          icon: "mapgis-juxing1",
+          type: "default",
+          tip: "矩形查询",
+          click: this.toggleQueryByRect
+        },
+        {
+          icon: "mapgis-pinghangsibianxing1",
+          type: "default",
+          tip: "多边形查询",
+          click: this.toggleQueryByPolygon
+        } */
+      ]
     };
   },
 
@@ -157,6 +273,10 @@ export default {
 
   mounted() {
     this.$_initDraw();
+    let position = this.position;
+    let pos = position.split('-');
+    document.querySelector(".mapgis-default-control").style = pos[0]+ ": 10px;" + pos[1] + ": 10px;";
+
   },
 
   beforeDestroy() {
@@ -191,12 +311,14 @@ export default {
       // asControl 本身是拥有 $_bindSelfEvents 方法的，但是这里的draw组件并不是遵循的mapbox-gl.js的事件机制，
       // 因此我们需要覆盖该方法, 按照对应的业务方式实现
       const vm = this;
+      let listeners = ["drawUpdate"].concat(Object.keys(this.$listeners));
       // 使用vue的this.$listeners方式来订阅用户指定的事件
-      Object.keys(this.$listeners).forEach(eventName => {
+      // Object.keys(this.$listeners).forEach(eventName => {
+      listeners.forEach(eventName => {
         if (events.includes(eventName)) {
           this.$_bindDrawEvents(
-            drawEvents[eventName],
-            vm.$_emitDrawEvent.bind(vm, eventName)
+              drawEvents[eventName],
+              vm.$_emitDrawEvent.bind(vm, eventName)
           );
         }
       });
@@ -204,6 +326,21 @@ export default {
 
     // 按照@mapgis/webclient-vue-mapboxgl的规范 发送事件 ，其实就是用{type：eventName}包装事件名
     $_emitDrawEvent(eventName, eventData) {
+      // console.log("_emitDrawEvent", eventName, eventData);
+      let mode = this.drawer.getMode();
+      if (eventName == "drawUpdate" && mode == "direct_select") {
+        if (
+            eventData.action == "change_coordinates" &&
+            eventData.features &&
+            eventData.features.length >= 0
+        ) {
+          let feature = eventData.features[0];
+          let center = turf.center(feature);
+          let area = Math.round(turf.area(feature))/1000000;
+          let radiusinkm = Math.round(Math.sqrt(area / Math.PI))/1000;
+          this.$emit("update-radius", { area, radiusinkm, center });
+        }
+      }
       return this.$_emitSelfEvent({ type: eventName }, eventData);
     },
 
@@ -249,7 +386,72 @@ export default {
       this.$_removeDrawControl();
 
       this.$_emitEvent("removed");
-    }
+    },
+
+    changeFold(){
+      // document.querySelector(".mapgis-ui-space").style="backgroundColor:black;";
+      let space = document.querySelector(".mapgis-ui-space");
+      let width = getComputedStyle(space).width;
+      if (width == "40px"){
+        space.style= "width: 320px!important;overflow: hidden;transition: width .5s;"
+      }else{
+        space.style="width: 40px!important;overflow: hidden;transition: width .5s;"
+      }
+    },
+
+    toggleStatic(){
+      // this.enableDrawer();
+      this.drawer && this.drawer.changeMode("static");
+    },
+    toggleSimple() {
+      // this.enableDrawer();
+      this.drawer && this.drawer.changeMode("simple_select");
+    },
+    togglePoint() {
+      this.enableDrawer();
+      this.drawer && this.drawer.changeMode("draw_point");
+    },
+    togglePolyline() {
+      this.enableDrawer();
+      this.drawer && this.drawer.changeMode("draw_line_string");
+    },
+    togglePolygon() {
+      this.enableDrawer();
+      this.drawer && this.drawer.changeMode("draw_polygon");
+    },
+    toggleRect() {
+      this.enableDrawer();
+      this.drawer && this.drawer.changeMode("draw_rectangle");
+    },
+    toggleCircle() {
+      this.enableDrawer();
+      this.drawer && this.drawer.changeMode("draw_circle");
+    },
+    toggleRadius() {
+      this.enableDrawer();
+      this.drawer && this.drawer.changeMode("draw_radius");
+
+    },
+    toggleDelete() {
+      this.enableDrawer();
+      this.drawer && this.drawer.deleteAll();
+    },
+    toggleDeleteAll() {
+      this.enableDrawer();
+      this.drawer && this.drawer.deleteAll();
+    },
+    toggleQueryByRect() {},
+    toggleQueryByPolygon() {}
   }
 };
 </script>
+
+<style>
+.mapgis-default-control {
+  width: fit-content;
+  position: absolute;
+  /*top: 10px;*/
+  /*left: 10px;*/
+  z-index: 3000;
+}
+</style>
