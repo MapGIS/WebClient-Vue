504ff087bf14838fe3c4e98bf41a8d1c4a8c7e66
zhangying-zy618 <1057936309@qq.com>
2021-08-20 11:45:16 +0800

    【mapboxgl-vue】【geojsonLayer矢量图层示例】

--- stories/MapboxLayerGeoJSON.stories.js
+++ stories/MapboxLayerGeoJSON.stories.js
@@ -1,5 +1,5 @@
 import MapgisWebMap from "../mapboxgl/src/components/map/GlMap.vue";
-import MapgisGeojsonLayer from "../mapboxgl/src/components/layer/GeojsonLayer.js";
+import MapgisGeojsonLayer from "../mapboxgl/src/components/layer/GeojsonLayer.vue";
 
 export default {
   title: "二维/图层-GeoJSON",
