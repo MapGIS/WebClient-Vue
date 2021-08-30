9fc63b71a4364555cecb8704177a710fef15feb3
liao-x-d <1627930520@qq.com>
2021-08-30 15:43:26 +0800

    【cesium】【修改】【增加场景特效组件的导出】

--- cesium/src/cdn/cdn_component.js
+++ cesium/src/cdn/cdn_component.js
@@ -54,3 +54,8 @@ export { default as Mapgis3dDynamicCutting } from "../components/Analysis/Dynami
 export { default as Mapgis3dFill } from "../components/Analysis/Fill";
 export { default as Mapgis3dHeightlimited } from "../components/Analysis/HeightLimited";
 export { default as Mapgis3dShadow } from "../components/Analysis/Shadow";
+
+
+export { default as Mapgis3dRainEffect } from "./components/SceneEffect/RainEffect";
+export { default as Mapgis3dSnowEffect } from "./components/SceneEffect/SnowEffect";
+export { default as Mapgis3dFogEffect } from "./components/SceneEffect/FogEffect";
