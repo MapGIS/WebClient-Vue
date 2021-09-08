cf3003ec0162a7e77cd20c798c2029b55cb80864
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-09-08 22:44:15 +0800

    【SDK】【Cesium】【提供多屏联动的移动策略&自定义事件机制】

---
+++ cesium/src/lib/Event/ScreenSpaceEventType.js
@@ -0,0 +1,62 @@
+export const ScreenSpaceEventType = {
+  LEFT_CLICK: {
+    type: "LEFT_CLICK"
+    /* event: Cesium.ScreenSpaceEventType.LEFT_CLICK */
+  },
+  LEFT_DOUBLE_CLICK: {
+    type: "LEFT_DOUBLE_CLICK"
+    /* event: Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK */
+  },
+  LEFT_DOWN: {
+    type: "LEFT_DOWN"
+    /* event: Cesium.ScreenSpaceEventType.LEFT_DOWN */
+  },
+  LEFT_UP: {
+    type: "LEFT_UP"
+    /* event: Cesium.ScreenSpaceEventType.LEFT_UP */
+  },
+  MIDDLE_CLICK: {
+    type: "MIDDLE_CLICK"
+    /* event: Cesium.ScreenSpaceEventType.MIDDLE_CLICK */
+  },
+  MIDDLE_DOWN: {
+    type: "MIDDLE_DOWN"
+    /* event: Cesium.ScreenSpaceEventType.MIDDLE_DOWN */
+  },
+  MIDDLE_UP: {
+    type: "MIDDLE_UP"
+    /* event: Cesium.ScreenSpaceEventType.MIDDLE_UP */
+  },
+  MOUSE_MOVE: {
+    type: "MOUSE_MOVE"
+    /* event: Cesium.ScreenSpaceEventType.MOUSE_MOVE */
+  },
+  PINCH_END: {
+    type: "PINCH_END"
+    /* event: Cesium.ScreenSpaceEventType.PINCH_END */
+  },
+  PINCH_MOVE: {
+    type: "PINCH_MOVE"
+    /* event: Cesium.ScreenSpaceEventType.PINCH_MOVE */
+  },
+  PINCH_START: {
+    type: "PINCH_START"
+    /* event: Cesium.ScreenSpaceEventType.PINCH_START */
+  },
+  RIGHT_CLICK: {
+    type: "RIGHT_CLICK"
+    /* event: Cesium.ScreenSpaceEventType.RIGHT_CLICK */
+  },
+  RIGHT_DOWN: {
+    type: "RIGHT_DOWN"
+    /* event: Cesium.ScreenSpaceEventType.RIGHT_DOWN */
+  },
+  RIGHT_UP: {
+    type: "RIGHT_UP"
+    /* event: Cesium.ScreenSpaceEventType.RIGHT_UP */
+  },
+  WHEEL: {
+    type: "WHEEL"
+    /* event: Cesium.ScreenSpaceEventType.WHEEL */
+  }
+};
