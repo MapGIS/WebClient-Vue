47541174c6b28449238b08c102398841caef469d
王魁帅 <2692537835@qq.com>
2021-09-09 17:13:52 +0800

    【故事】【UI】【删除掉基础组件的storybook中的多余参数项】

--- stories/mapgisui/components/UISlider.stories.js
+++ stories/mapgisui/components/UISlider.stories.js
@@ -1,10 +1,18 @@
 import MapgisUiSlider from "../../../ui/src/components/slider/Slider.vue";
 
 export default {
-  title: "界面/数据输入/滑动",
+  title: "界面/数据输入/滑动输入条",
   component: MapgisUiSlider,
   argTypes: {
-    "default-value": 30,
+    defaultValue: 30,
+    prefixCls: { table: { disable: true } },
+    getPopupContainer: { table: { disable: true } },
+    csp: { table: { disable: true } },
+    locale: { table: { disable: true } },
+    background: { table: { disable: true } },
+    textColor: { table: { disable: true } },
+    colorGroup: { table: { disable: true } },
+    themeStyleChanged: { table: { disable: true } },
   },
 };
 
@@ -13,14 +21,14 @@ const Template = (args, { argTypes }) => ({
   components: { MapgisUiSlider },
   data() {
     return {
-      value: 0,
+      // value: 0,
     };
   },
-  template: `<mapgis-ui-slider v-model="value" style="width: 120px;height:40px;">
+  template: `<mapgis-ui-slider :default-value="defaultValue" style="width: 120px;height:40px;">
     </mapgis-ui-slider>`,
 });
 
 export const Slider = Template.bind({});
 Slider.args = {
-  "default-value": 30,
+  defaultValue: 30,
 };
