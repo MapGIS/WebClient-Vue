59670d2e5c703d11735822d24bdc2e11d4f66cdf
killer1992 <killer1992@yeah.net>
2021-09-14 17:36:38 +0800

    【ui】【杨琨】【新增混合组件】

--- ui/src/components/sliderRow/SliderRow.vue
+++ ui/src/components/sliderRow/SliderRow.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <mapgis-ui-row v-if="type === 'slider'">
-      <mapgis-ui-col :span="8">
+      <mapgis-ui-col :span="sliderProps.titleCol">
         <p style="margin-top: 0.8em">{{ title }}</p>
       </mapgis-ui-col>
-      <mapgis-ui-col :span="sliderProps.range ? 16 : 12">
+      <mapgis-ui-col :span="sliderProps.sliderCol">
         <mapgis-ui-slider
             v-model="valueCopy"
             :max="sliderProps.sliderMax"
@@ -21,7 +21,7 @@
             @afterChange="methods.afterChange"
         />
       </mapgis-ui-col>
-      <mapgis-ui-col :span="4" v-if="!sliderProps.range">
+      <mapgis-ui-col :span="sliderProps.inputCol" v-if="!sliderProps.range">
         <mapgis-ui-input-number
             style="margin-top: 5px;"
             :max="sliderProps.sliderMax"
@@ -31,10 +31,10 @@
       </mapgis-ui-col>
     </mapgis-ui-row>
     <mapgis-ui-row v-if="type === 'select'">
-      <mapgis-ui-col :span="8">
+      <mapgis-ui-col :span="selectProps.titleCol">
         <p style="margin-top: 0.8em">{{ title }}</p>
       </mapgis-ui-col>
-      <mapgis-ui-col :span="16">
+      <mapgis-ui-col :span="selectProps.selectCol">
         <mapgis-ui-select
             v-model="valueCopy"
             :autoFocus="selectProps.autoFocus"
@@ -80,15 +80,85 @@
       </mapgis-ui-col>
     </mapgis-ui-row>
     <mapgis-ui-row v-if="type === 'colorPicker'">
-      <mapgis-ui-col :span="8">
-        <p>颜色</p>
+      <mapgis-ui-col :span="colorPickerProps.titleCol">
+        <p>{{title}}</p>
       </mapgis-ui-col>
-      <mapgis-ui-col :span="16">
+      <mapgis-ui-col :span="colorPickerProps.colorCol">
         <div>
           <colorPicker
               :id="colorId"
+              v-model="valueCopy"
+              @change="methods.change"
           />
         </div>
+      </mapgis-ui-col>
+    </mapgis-ui-row>
+    <mapgis-ui-row v-if="type === 'grediantPicker'">
+      <mapgis-ui-col :span="grediantPickerProps.titleCol">
+        <p style="margin-top: 5px">{{title}}</p>
+      </mapgis-ui-col>
+      <mapgis-ui-col :span="grediantPickerProps.selectCol">
+        <mapgis-ui-select
+            v-model="valueCopy"
+            :getPopupContainer="grediantPickerProps.getPopupContainer"
+            @change="methods.change"
+            :style="grediantPickerProps.selectStyle"
+        >
+          <mapgis-ui-select-option v-for="(gradient,index) in grediantPickerProps.gradients" :key="index"
+                                   :value="gradient.key">
+            <div class="mix-row-gradient" :style="{background: gradient.value,...grediantPickerProps.optionStyle}"/>
+          </mapgis-ui-select-option>
+        </mapgis-ui-select>
+      </mapgis-ui-col>
+    </mapgis-ui-row>
+    <mapgis-ui-row v-if="type === 'input'">
+      <mapgis-ui-col :span="inputProps.titleCol">
+        <p
+            :style="inputProps.titleStyle"
+        >{{title}}</p>
+      </mapgis-ui-col>
+      <mapgis-ui-col :span="inputProps.inputCol">
+        <mapgis-ui-input
+          v-model="valueCopy"
+          :addonAfter="inputProps.addonAfter"
+          :addonBefore="inputProps.addonBefore"
+          :disabled="inputProps.disabled"
+          :id="inputProps.id"
+          :maxLength="inputProps.maxLength"
+          :prefix="inputProps.prefix"
+          :size="inputProps.size"
+          :suffix="inputProps.suffix"
+          :type="inputProps.type"
+          :allowClear="inputProps.allowClear"
+          @change="methods.change"
+          @pressEnter="methods.pressEnter"
+          :style="inputProps.inputStyle"
+        />
+      </mapgis-ui-col>
+    </mapgis-ui-row>
+    <mapgis-ui-row v-if="type === 'inputNumber'">
+      <mapgis-ui-col :span="inputNumberProps.titleCol">
+        <p
+            :style="inputNumberProps.titleStyle"
+        >{{title}}</p>
+      </mapgis-ui-col>
+      <mapgis-ui-col :span="inputNumberProps.inputCol">
+        <mapgis-ui-input-number
+            v-model="valueCopy"
+            :autoFocus="inputNumberProps.autoFocus"
+            :disabled="inputNumberProps.disabled"
+            :formatter="inputNumberProps.formatter"
+            :max="inputNumberProps.max"
+            :min="inputNumberProps.min"
+            :parser="inputNumberProps.parser"
+            :precision="inputNumberProps.precision"
+            :decimalSeparator="inputNumberProps.decimalSeparator"
+            :size="inputNumberProps.size"
+            :step="inputNumberProps.step"
+            @change="methods.change"
+            @pressEnter="methods.pressEnter"
+            :style="inputNumberProps.inputStyle"
+        />
       </mapgis-ui-col>
     </mapgis-ui-row>
   </div>
@@ -121,7 +191,10 @@ export default {
         //是否显示提示框
         tooltipVisible: true,
         //Tooltip 渲染父节点，默认渲染到 body上
-        getTooltipPopupContainer: undefined
+        getTooltipPopupContainer: undefined,
+        titleCol: 8,
+        sliderCol: 12,
+        inputCol: 4
       },
       selectProps: {
         //数据源
@@ -175,13 +248,67 @@ export default {
         //是否默认展开下拉菜单
         defaultOpen: undefined,
         //是否展开下拉菜单
-        open: undefined
+        open: undefined,
+        titleCol: 8,
+        selectCol: 16
       },
-      colorPickerProps: {},
+      colorPickerProps: {
+        //拾取器样式
+        style: {},
+        titleCol: 8,
+        colorCol: 16
+      },
+      grediantPickerProps: {
+        //菜单渲染父节点。默认渲染到 body 上，如果你遇到菜单滚动定位问题，试试修改为滚动的区域，并相对其定位。
+        getPopupContainer: undefined,
+        gradients: undefined,
+        selectStyle: {
+          width: "155px"
+        },
+        optionStyle: {
+          width: "155px"
+        },
+        titleCol: 8,
+        selectCol: 16
+      },
+      inputProps: {
+        addonAfter: undefined,
+        addonBefore: undefined,
+        disabled: false,
+        id: undefined,
+        maxLength: undefined,
+        prefix: undefined,
+        size: undefined,
+        suffix: undefined,
+        type: undefined,
+        allowClear: undefined,
+        titleCol: 8,
+        inputCol: 16,
+        inputStyle: {},
+        titleStyle: {}
+      },
+      inputNumberProps: {
+        autoFocus: undefined,
+        disabled: undefined,
+        formatter: undefined,
+        max: undefined,
+        min: undefined,
+        parser: undefined,
+        precision: undefined,
+        decimalSeparator: undefined,
+        size: undefined,
+        step: undefined,
+        titleCol: 8,
+        inputCol: 16,
+        inputStyle: {},
+        titleStyle: {}
+      },
       methods: {
         blur: function () {
         },
         change: function () {
+        },
+        pressEnter: function () {
         },
         afterChange: function () {
         },
@@ -245,6 +372,11 @@ export default {
       }
     },
   },
+  created() {
+    if (this.type === "colorPicker") {
+      this.valueCopy = "#000000";
+    }
+  },
   mounted() {
     this.valueCopy = this.value;
     this.fieldCopy = this.field;
@@ -270,18 +402,46 @@ export default {
       switch (this.type) {
         case "slider":
           this.sliderProps = Object.assign(this.sliderProps, this.options.sliderProps);
+          if(this.sliderProps.range && this.sliderProps.sliderCol === 16){
+            this.sliderProps.sliderCol = 12;
+          }
           break;
         case "select":
           this.selectProps = Object.assign(this.selectProps, this.options.selectProps);
           break;
+        case "colorPicker":
+          this.$_initColorStyle();
+          break;
+        case "grediantPicker":
+          this.grediantPickerProps = Object.assign(this.grediantPickerProps, this.options.grediantPickerProps);
+          if(!this.valueCopy && this.grediantPickerProps.gradients.length > 0){
+            this.valueCopy = this.grediantPickerProps.gradients[0].key;
+          }
+          break;
+        case "input":
+          this.inputProps = Object.assign(this.inputProps, this.options.inputProps);
+          break;
+        case "inputNumber":
+          this.inputNumberProps = Object.assign(this.inputNumberProps, this.options.inputNumberProps);
+          break;
       }
     },
-    $_initColorStyle(){
-
-    }
+    $_initColorStyle() {
+      let colorDocument = document.getElementById(this.colorId);
+      let colorBtn = colorDocument.childNodes;
+      let vm = this;
+      Object.keys(this.options.colorPickerProps.style).forEach(function (key) {
+        colorBtn[0].style[key] = vm.options.colorPickerProps.style[key];
+      });
+    },
   }
 }
 </script>
 
 <style scoped>
+.mix-row-gradient {
+  height: 15px;
+  margin: 8px 0 0;
+  border-radius: 3px;
+}
 </style>
