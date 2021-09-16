8efbf7df849be2f48792c5520d97fb073d0c40c9
zhuxiuli <zhuxiuli.julie@mapgis.com>
2021-09-16 12:04:43 +0800

    【SDK】 【修复】 【还原util.js文件, 并新增last和oneOf方法】

--- mapboxgl/src/components/util/util.js
+++ mapboxgl/src/components/util/util.js
@@ -75,3 +75,32 @@ export function deepEqual(a, b) {
   }
   return a === b;
 }
+
+/**
+ * 获取数组最后一个元素
+ * @param {array} array
+ * @returns  元素
+ */
+export function last(array) {
+  const length = array == null ? 0 : array.length;
+  return length ? array[length - 1] : undefined;
+}
+
+/**
+ * 判断数组中是否含有某个元素
+ * @param {*} valueOrFn
+ * @param {array} array
+ * @returns {boolean}
+ */
+export function oneOf(valueOrFn, array) {
+  const that = this;
+  return array.reduce((bool, item) => {
+    let flag = false;
+    if (typeof valueOrFn === "function") {
+      flag = valueOrFn.call(that, item);
+    } else if (item === valueOrFn) {
+      flag = true;
+    }
+    return bool || flag;
+  }, false);
+}
