6c143c1e1edd19e343ee4b21ffaa48ce7ae28fb2
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-10-18 19:49:50 +0800

    【SDK】【Cesium】【实现M3D-0.x/1.x版本的图层控制】

---
+++ cesium/src/components/M3D/M3dType.js
@@ -0,0 +1,24 @@
+export const M3dType = {
+  UnKnow: "UnKnow",
+  Model: "Model",
+  Instance: "Instance",
+  CloudPoint: "CloudPoint"
+};
+
+export const M3dType_0_0 = {
+  Model: 0,
+  Instance: 1,
+  CloudPoint: 2
+};
+
+export const M3dType_1_0 = {
+  Model: 0,
+  Instance: 1,
+  CloudPoint: 2
+};
+
+export const M3dType_2_0 = {
+  Model: 0,
+  Instance: 1,
+  CloudPoint: 2
+};
