eb2c17f4238e960e1868593f5e975e9b3ccbeb3f
gongyuejian <gongyuejian.000@mapgis.com>
2021-10-18 10:36:06 +0800

    【storybook】【cesium】【粒子特效移到可视化分组】

--- .storybook/components/cesium_coms.js
+++ .storybook/components/cesium_coms.js
@@ -63,6 +63,6 @@ export { default as Mapgis3dDynamicSection } from "../../cesium/src/components/A
 export { default as Mapgis3dShadow } from "../../cesium/src/components/Analysis/Shadow";
 export { default as Mapgis3dHeightlimited } from "../../cesium/src/components/Analysis/HeightLimited";
 export { default as Mapgis3dContour } from "../../cesium/src/components/Analysis/Contour";
-export { default as Mapgis3dParticleEffects } from "../../cesium/src/components/Analysis/ParticleEffects";
+export { default as Mapgis3dParticleEffects } from "../../cesium/src/components/Overlay/ParticleEffects";
 
 export { default as Mapgis3DComprehensiveQuery } from "../../cesium/src/components/service/comprehensive-query/ComprehensiveQuery";
