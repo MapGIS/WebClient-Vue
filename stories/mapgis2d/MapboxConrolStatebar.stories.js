bb1a2599434eb6031b8dbeae6b1078013e46c336
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-10-20 10:28:51 +0800

    【故事】【MapboxGL】【新增状态栏的使用示例】

---
+++ stories/mapgis2d/MapboxConrolStatebar.stories.js
@@ -0,0 +1,47 @@
+import MapgisWebMap from "../../mapboxgl/src/components/map/GlMap.vue";
+import MapgisStateControl from "../../mapboxgl/src/components/UI/controls/state/StateControl.vue";
+import "../style/state.css";
+
+export default {
+  title: "二维/地图子组件/状态栏",
+  component: MapgisStateControl,
+  argTypes: {
+    default: {
+      description: "是否启用默认的界面",
+      type: { name: "Boolean", required: false },
+      table: {
+        type: {
+          summary: "自定义界面",
+          detail: "",
+        },
+        defaultValue: { summary: true },
+      },
+      control: "object",
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { MapgisWebMap, MapgisStateControl },
+  template: `<mapgis-web-map crs="EPSG:4326" :center="[116.3, 40.5]" :zoom="5" style="height:95vh">
+    <mapgis-rastertile-layer
+      layerId="raster_tdt",
+      url="http://t0.tianditu.com/DataServer?T=vec_c&L={z}&Y={y}&X={x}&tk=9c157e9585486c02edf817d2ecbc7752" 
+    >
+    </mapgis-rastertile-layer>
+    <mapgis-state v-bind="$props">
+      <template v-slot:default="{state}">
+        <mapgis-ui-input-group size="default" class="mapgis-test-2d-statebar">
+          <mapgis-ui-input addon-before="级别" :value="state.level"/>
+          <mapgis-ui-input addon-before="比例尺" :value="state.scale"/>
+          <mapgis-ui-input addon-before="经度" :value="state.lng" />
+          <mapgis-ui-input addon-before="纬度" :value="state.lat"/>
+        </mapgis-ui-input-group>
+      </template>
+    </mapgis-state>
+  </mapgis-web-map>`,
+});
+
+export const 状态栏 = Template.bind({});
+状态栏.args = {};
