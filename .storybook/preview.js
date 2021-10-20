56f535a7149de72bf455e3406e3fefc444817f8e
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-10-20 12:10:21 +0800

    【SDK】【Cesium】【修复栅格瓦片的WebGLobe依赖】

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,6 +13,12 @@ Vue.use(MapgisUIComponents, {});
 Vue.use(MapgisMapboxComponents, {});
 Vue.use(MapgisCesiumComponents, {});
 
+// "http://develop.smaryun.com:8899/static/libs/cdn/cesium/Cesium.js";
+window.VueCesiumLibPath = "http://localhost:8888/static/libs/cdn/cesium-new/Cesium.js";
+
+// "http://develop.smaryun.com:8899/static/libs/cdn/zondyclient/webclient-cesium-plugin.min.js"
+window.VueCesiumPluginPath = "http://localhost:8888/static/libs/cdn/zondyclient/webclient-cesium-plugin.min.js";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
