470594c8f1101990ca72cf129a0f57812f44927d
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-10-20 15:03:47 +0800

    【故事】【Cesium】【补充Cesium的帮助示例】

--- stories/mapgis3d/CesiumBaseGlobe.stories.js
+++ stories/mapgis3d/CesiumBaseGlobe.stories.js
@@ -1,21 +1,41 @@
 import MapgisWebGlobe from "../../cesium/src/components/WebGlobe/WebGlobe.vue";
+import "../style/scene.css";
 
 export default {
   title: "三维/场景/地图场景",
   component: MapgisWebGlobe,
-  argTypes: {
-  },
+  argTypes: {},
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { MapgisWebGlobe },
+  data() {
+    return {
+      outStyle: {
+        position: "absolute",
+        zIndex: 700,
+        width: "200px",
+        padding: "10px",
+        right: "10px",
+        top: "10px",
+      },
+      position: "top-right",
+    };
+  },
   template: `<mapgis-web-globe v-bind="$props" style="height:95vh">
+    <mapgis-ui-collapse-card :outStyle="outStyle" :position="position">
+      <mapgis-ui-iconfont type="mapgis-map-pin" slot="icon-hiden" />
+      <span slot="extra">
+        <mapgis-ui-iconfont type="mapgis-map-pin" @click="hide" />
+      </span>
+      <mapigs-ui-button type="primary">销毁场景</mapigs-ui-button>
+    </mapgis-ui-collapse-card>
+    
     <mapgis-3d-raster-layer url="http://t0.tianditu.com/DataServer?T=vec_w&L={z}&Y={y}&X={x}&tk=9c157e9585486c02edf817d2ecbc7752" />
     <mapgis-3d-raster-layer url="http://t1.tianditu.com/DataServer?T=cia_w&L={z}&Y={y}&X={x}&tk=9c157e9585486c02edf817d2ecbc7752" />
-  </mapgis-web-globe>`
+  </mapgis-web-globe>`,
 });
 
 export const EmptyGlobe = Template.bind({});
-EmptyGlobe.args = {
-};
+EmptyGlobe.args = {};
