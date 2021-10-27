68c9bcfc6e887cf057b4270620330e131e38e42f
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-10-27 20:34:43 +0800

    【SDK】【Ceisum】【调整G3D的交互模式】

--- cesium/src/components/M3D/components/M3dMenuSetting.vue
+++ cesium/src/components/M3D/components/M3dMenuSetting.vue
@@ -1,30 +1,17 @@
 <template>
-  <span>
-     <mapgis-ui-statistic title="提示" value="请点击场景模型！" />
-  </span>
-  </template>
-<script>
-export default {
-  data() {
-    return {
-      deadline: Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30,
-    };
-  },
-  methods: {
-    onFinish() {
-      console.log('finished!');
-    },
-  },
-};
-</script> </span>
+  <div class="mapgis-3d-m3d-menu-setting">
+    <effect-setting layout="vertical"> </effect-setting>
+  </div>
 </template>
 
 <script>
 import VueOptions from "../../Base/Vue/VueOptions";
+import EffectSetting from "../../SceneEffect/Setting.vue";
 
 export default {
-  name: "mapgis-3d-m3d-menu-highlight",
+  name: "mapgis-3d-m3d-menu-setting",
   inject: ["Cesium", "CesiumZondy", "vueCesium", "viewer", "m3ds"],
+  components: { EffectSetting },
   props: {
     ...VueOptions,
     version: {
@@ -118,55 +105,18 @@ export default {
       }
 
       if (version == "0.0" || version == "1.0") {
-        let find = vueCesium.G3DManager.findSource(vueKey, vueIndex);
-        if (find && find.options && find.options.version_0_0) {
-          let {
-            current,
-            currentLayer,
-            analysisManager
-          } = find.options.version_0_0;
-          //根据鼠标点击位置选择对象
-          let pickedFeature = viewer.scene.pick(movement.position);
-
-          //判断current对象中要素有值，该值和鼠标点击位置不相同
-          if (
-            Cesium.defined(current.feature) &&
-            current.feature !== pickedFeature
-          ) {
-            currentLayer = [current.feature.tileset];
-            var title = current.feature.getProperty("name");
-            var values = title.split("_");
-            var vlueNumber = parseInt(values[2]);
-            var idList = [vlueNumber];
-            analysisManager.stopCustomDisplay(currentLayer);
-            current.feature = undefined;
-          }
-
-          //判断点击位置是否有值，该值和鼠标点击位置不相同
-          if (
-            Cesium.defined(pickedFeature) &&
-            current.feature !== pickedFeature
-          ) {
-            current.feature = pickedFeature;
-            currentLayer = [current.feature.tileset];
-            var title = current.feature.getProperty("name");
-            var values = title.split("_");
-            var vlueNumber = parseInt(values[2]);
-            var idList = [vlueNumber];
-            var options = {
-              color: new Cesium.Color(255 / 255, 255 / 255, 0 / 255, 1),
-              colorBlendMode: Cesium.Cesium3DTileColorBlendMode.REPLACE
-            };
-            analysisManager.stopCustomDisplay(currentLayer);
-            analysisManager.startCustomDisplay(currentLayer, idList, options);
-          }
-        }
       } else if (version == "2.0") {
-        var oid = viewer.scene.pickOid(movement.position);
-        tileset.pickedOid = oid;
-        tileset.pickedColor = new Cesium.Color(1.0, 1.0, 0.0, 0.6);
       }
     }
   }
 };
 </script>
+
+<style>
+.mapgis-3d-m3d-menu-setting > .mapgis-3d-setting {
+  display: block;
+  position: relative;
+  height: 400px;
+  width: 300px;
+}
+</style>
