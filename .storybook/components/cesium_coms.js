71c8338ad21ae0c693da9ed16eb498d742ac0dd3
killer1992 <killer1992@yeah.net>
2021-10-28 11:41:55 +0800

    【cesium】【杨琨】【添加数据流组件】

--- .storybook/components/cesium_coms.js
+++ .storybook/components/cesium_coms.js
@@ -8,6 +8,7 @@ export { default as Mapgis3dTable } from "../../cesium/src/components/UI/Control
 export { default as Mapgis3dMeasure } from "../../cesium/src/components/UI/Controls/Measure/Measure";
 export { default as Mapgis3dCompare } from "../../cesium/src/components/UI/Controls/Compare/Compare.vue";
 export { default as Mapgis3dArcgisLegend } from "../../cesium/src/components/UI/Controls/Legend/Legend.vue";
+export { default as Mapgis3DDataFlow } from "../../cesium/src/components/Layer/DataFlow/DataFlow";
 
 // 影像
 export { default as Mapgis3dImageryLayer } from "../../cesium/src/components/Provider/ImageProvider/ImageryLayer.vue";
