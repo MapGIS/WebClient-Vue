1ebf520d60cae10ce956ec497fc6db5f1716973c
killer1992 <killer1992@yeah.net>
2021-11-02 17:43:03 +0800

    【cesium】【杨琨】【新增标会组件UI】

---
+++ cesium/src/components/Layer/Plotting/PlottingLayer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <panel-one
+        @chooseTool="$_chooseTool"
+    />
+    <edit-panel
+        :show="showEditPanel"
+        :editOptions="toolOptions"
+    />
+  </div>
+</template>
+
+<script>
+import PanelOne from "./PanelOne"
+import EditPanel from "./EditPanel"
+import editOptions from "./panelOptions";
+
+export default {
+  name: "mapgis-3d-plotting-layer",
+  components: {
+    "panel-one": PanelOne,
+    "edit-panel": EditPanel,
+  },
+  data() {
+    return {
+      editOptions: editOptions,
+      toolOptions: undefined,
+      showEditPanel: false
+    }
+  },
+  mounted() {},
+  methods: {
+    $_chooseTool(tool) {
+      this.showEditPanel = true;
+      this.toolOptions = this.editOptions[tool];
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
