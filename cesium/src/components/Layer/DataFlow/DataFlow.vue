9e5e6614a2c3e5c45d0098ee930b31e2d3f0d78c
killer1992 <killer1992@yeah.net>
2021-11-17 14:46:22 +0800

    【cesium】【杨琨】【优化数据刘组件，新增popup】

--- cesium/src/components/Layer/DataFlow/DataFlow.vue
+++ cesium/src/components/Layer/DataFlow/DataFlow.vue
@@ -1,9 +1,26 @@
 <template>
-  <span></span>
+  <div>
+    <mapgis-3d-popup
+        v-for="(popup,index) in popups"
+        :key="index"
+        :position='{"longitude":popup.lng,"latitude":popup.lat,"height":popup.alt}'
+        :forceRender="true"
+        v-model="popup.show"
+        @change="$_toggle"
+        :vueIndex="popup.vueIndex"
+    >
+      <div>
+        <slot name="content" :popup="popup">
+          <div v-html="popup.container"></div>
+        </slot>
+      </div>
+    </mapgis-3d-popup>
+  </div>
 </template>
 
 <script>
 import {Style} from "@mapgis/webclient-es6-service";
+import { getPopupHtml } from "../../UI/Popup/popupUtil";
 
 const {PointStyle, ModelStyle, MarkerStyle} = Style;
 export default {
@@ -12,6 +29,10 @@ export default {
   props: {
     baseUrl: {
       type: String
+    },
+    UUID: {
+      type: String,
+      default: "imei"
     },
     type: {
       type: String,
@@ -40,7 +61,8 @@ export default {
   },
   data() {
     return {
-      firstAdd: true
+      firstAdd: true,
+      popups: [],
     }
   },
   mounted() {
@@ -56,8 +78,16 @@ export default {
     vueCesium.DataFlowManager.deleteSource(vueKey, vueIndex);
   },
   methods: {
+    $_toggle(flag, vueIndex) {
+      for (let i = 0; i < this.popups.length; i++) {
+        if (this.popups[i].vueIndex === vueIndex) {
+          this.popups[i].show = flag;
+          break;
+        }
+      }
+    },
     setModelDirection(longitude, latitude, direction) {
-      let center = Cesium.Cartesian3.fromDegrees(longitude, latitude, 40);
+      let center = Cesium.Cartesian3.fromDegrees(longitude, latitude, 20);
       let heading = Cesium.Math.toRadians(direction);
       let pitch = 0;
       let roll = 0;
@@ -66,11 +96,56 @@ export default {
 
       return orientation;
     },
+    //记得提取到公共函数
+    $_cartesian3ToLongLat(cartesian3) {
+      let position = {};
+      let graphicPosition = Cesium.Cartographic.fromCartesian(cartesian3);
+      position.lat = Cesium.Math.toDegrees(graphicPosition.latitude);
+      position.lng = Cesium.Math.toDegrees(graphicPosition.longitude);
+      position.alt = graphicPosition.height;
+      return position;
+    },
     $_addEntityLayer() {
       let vm = this;
       const {vueKey, vueIndex} = this;
       //开启长链接
       let ws = new WebSocket(this.baseUrl);
+      //设置点击事件
+      this.viewer.screenSpaceEventHandler.setInputAction(function onLeftClick(movement) {
+        let pickedFeature = vm.viewer.scene.pick(movement.position);
+        if (Cesium.defined(pickedFeature)) {
+          let hasPopup = false, index;
+          for (let i = 0;i<vm.popups.length;i++){
+            if(vm.popups[i].properties[vm.UUID] === pickedFeature.id.properties[vm.UUID].getValue()){
+              hasPopup = true;
+              index = i;
+              break;
+            }
+          }
+          if(!hasPopup){
+            let popup = vm.$_cartesian3ToLongLat(pickedFeature.id.position.getValue());
+            popup.height = 50;
+            popup.keys = pickedFeature.id.keys;
+            popup.show = true;
+            popup.vueIndex = vm.popups.length;
+            popup.properties = {};
+            for (let i = 0; i < pickedFeature.id.keys.length; i++) {
+              let key = pickedFeature.id.keys[i];
+              popup.properties[key] = pickedFeature.id.properties[key].getValue();
+            }
+            popup.container = getPopupHtml("default", {properties: popup.properties}, {
+              title: "测试标题",
+              fields: popup.keys,
+              style: {
+                containerStyle: { width: "360px" }
+              }
+            });
+            vm.popups.push(popup);
+          }else {
+            vm.popups[index].show = true;
+          }
+        }
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
       //接受消息
       ws.onmessage = function (evt) {
         let data = JSON.parse(evt.data);
@@ -95,7 +170,7 @@ export default {
         if (source) {
           points = source.source;
           for (let i = 0; i < points.length; i++) {
-            if (points[i].properties.imei._value === data.properties.imei) {
+            if (points[i].properties[vm.UUID].getValue() === data.properties[vm.UUID]) {
               addPoint = false;
               pointId = i;
               break;
@@ -109,13 +184,15 @@ export default {
             window.vueCesium.DataFlowManager.addSource(vueKey, vueIndex, points);
           }
           let point;
+          let keys = Object.keys(data.properties);
           switch (vm.layerStyle.type) {
             case "point":
               let pStyle = new PointStyle(vm.layerStyle);
               point = vm.viewer.entities.add({
-                position: Cesium.Cartesian3.fromDegrees(data.geometry.coordinates[0], data.geometry.coordinates[1], 0),
+                position: Cesium.Cartesian3.fromDegrees(data.geometry.coordinates[0], data.geometry.coordinates[1], 20),
                 point: pStyle.toCesiumStyle(Cesium),
-                properties: data.properties
+                properties: data.properties,
+                keys: keys
               });
               break;
             case "model":
@@ -124,30 +201,35 @@ export default {
                 position: Cesium.Cartesian3.fromDegrees(data.geometry.coordinates[0], data.geometry.coordinates[1], 20),
                 model: mStyle.toCesiumStyle(Cesium),
                 properties: data.properties,
-                positionBack: [data.geometry.coordinates[0], data.geometry.coordinates[1]]
+                keys: keys
               });
               break;
             case "marker":
               let markerStyle = new MarkerStyle();
               markerStyle = markerStyle.toCesiumStyle(vm.layerStyle, data, Cesium);
               point = vm.viewer.entities.add({
-                position: Cesium.Cartesian3.fromDegrees(data.geometry.coordinates[0], data.geometry.coordinates[1], 40),
+                position: Cesium.Cartesian3.fromDegrees(data.geometry.coordinates[0], data.geometry.coordinates[1], 20),
                 billboard: markerStyle.billboard,
                 label: markerStyle.label,
-                properties: data.properties
+                properties: data.properties,
+                keys: keys
               });
               break;
           }
           points.push(point);
         } else {
           //更新点
-          // let  r  = Math.atan2((data.geometry.coordinates[1]-points[pointId].positionBack[1]), (data.geometry.coordinates[0]-points[pointId].positionBack[0]))  //弧度  0.6435011087932844
-          // let  h  = r*( 180 /Math.PI);  //角度  36.86989764584402
-          // let direction = vm.setModelDirection(points[pointId].positionBack[0], points[pointId].positionBack[1], h)
-          // points[pointId].orientation = direction;
           points[pointId].properties = data.properties;
-          points[pointId].positionBack = [data.geometry.coordinates[0], data.geometry.coordinates[1]];
           points[pointId].position = Cesium.Cartesian3.fromDegrees(data.geometry.coordinates[0], data.geometry.coordinates[1], 20);
+          //更新popup
+          for (let i = 0; i < vm.popups.length; i++) {
+            if (vm.popups[i].properties[vm.UUID] === data.properties[vm.UUID]) {
+              vm.popups[i].lng = data.geometry.coordinates[0];
+              vm.popups[i].lat = data.geometry.coordinates[1];
+              vm.popups[i].alt = 50;
+              break;
+            }
+          }
         }
       };
     }
