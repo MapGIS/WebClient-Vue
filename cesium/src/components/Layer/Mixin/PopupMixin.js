4483119b3d03028eb0539eae09eec463b9516623
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-11-26 20:12:10 +0800

    【SDK】【Cesium】【优化card的逻辑判断】

--- cesium/src/components/Layer/Mixin/PopupMixin.js
+++ cesium/src/components/Layer/Mixin/PopupMixin.js
@@ -141,6 +141,8 @@ export default {
       );
     }
 
+    console.log('feature', feature);
+
     if (customPopup || customTips) {
       return (
         <Popup
--- cesium/src/components/UI/Popup/PopupLayer.js
+++ cesium/src/components/UI/Popup/PopupLayer.js
@@ -1,4 +1,4 @@
-import { updataPopupPosition } from './popup/popup';
+import { updataPopupPosition } from "./popup/popup";
 
 let popupsIdIndex = 0;
 
@@ -60,273 +60,320 @@ let popupsIdIndex = 0;
       });
  */
 export default class PopupLayer {
-    constructor(map, position, options, container) {
-        this.map = map;
-        this.scene = map.scene;
+  constructor(map, position, options, container) {
+    this.map = map;
+    this.scene = map.scene;
 
-        this.position = position;
-        this.options = options;
-        this.container = container;
+    this.position = position;
+    this.options = options;
+    this.container = container;
 
-        this.popupId = options.popupId || 'cesium-popup-id-' + popupsIdIndex++;
-        this.popupClass = options.popupClass || 'cesium-popup';
-        this.popupContentId = options.popupContentId || 'cesium-popup-content-id-' + popupsIdIndex++;
+    this.popupId = options.popupId || "cesium-popup-id-" + popupsIdIndex++;
+    this.popupClass = options.popupClass || "cesium-popup";
+    this.popupContentId =
+      options.popupContentId || "cesium-popup-content-id-" + popupsIdIndex++;
 
-        this.options.postRender = this.options.postRender === undefined ? true : this.options.postRender;
-        this.Cesium = options.Cesium || window['Cesium'];
+    this.options.postRender =
+      this.options.postRender === undefined ? true : this.options.postRender;
+    this.Cesium = options.Cesium || window["Cesium"];
 
-        this.scene = map.scene;
-        this.camera = map.camera;
-        this.isShow = true;
+    this.scene = map.scene;
+    this.camera = map.camera;
+    this.isShow = true;
 
-        if (options.callback) {
-            const { onShow, onHide, onSeparate } = options.callback;
-            this.onShow = onShow;
-            this.onHide = onHide;
-            this.onSeparate = onSeparate;
-        }
-
-        let ScreenSpaceEventHandler = this.Cesium.ScreenSpaceEventHandler || window['Cesium'].ScreenSpaceEventHandler;
-
-        this.handler = new ScreenSpaceEventHandler(this.scene.canvas);
-
-        this.infoDiv = null;
-        // this.px_position = null;
-        if (position.entity) {
-            this.cartesian = position.entity.position._value;
-        }
-
-        this.cartesian =
-            this.cartesian ||
-            this.position.cartesian ||
-            this.Cesium.Cartesian3.fromDegrees(this.position.longitude, this.position.latitude, this.position.height);
-
-        let vc = this.map.container;
-        let cesumWidgetContainer = undefined;
-        if (vc.children && vc.children.length > 0) {
-            if (vc.children[0].children && vc.children[0].children.length > 0) {
-                if (vc.children[0].children[0].children && vc.children[0].children[0].children.length > 0) {
-                    cesumWidgetContainer = vc.children[0].children[0].children[0];
-                }
-            }
-        }
-
-        if (!cesumWidgetContainer) {
-            let parents = document.getElementsByClassName('cesium-widget');
-            parent = parents.length > 0 ? parents[0] : map.container;
-            this.parent = parent;
-        } else {
-            this.parent = cesumWidgetContainer;
-        }
-
-        // this.initDevicePixelRatio();
-        this.showClose = options.showClose === undefined ? true : options.showClose;
-        this.popupType = options.popupType;
-        this.images = options.images;
-        this.description = options.description;
-        this.popup = this._createPopup();
-
-        this.moveStart = this.eventMoveStart.bind(this);
-        this.moveEnd = this.eventMoveEnd.bind(this);
-        this.movement = this.movement.bind(this);
-        this.update = this.update.bind(this);
-
-        this.bindEvent();
-
-        return this;
+    if (options.callback) {
+      const { onShow, onHide, onSeparate } = options.callback;
+      this.onShow = onShow;
+      this.onHide = onHide;
+      this.onSeparate = onSeparate;
     }
 
-    _createPopup() {
-        const self = this;
-        this.hide = this.hide.bind(this);
-        let infoDiv = window.document.createElement('div');
-        infoDiv.id = this.popupId;
-        infoDiv.style.display = 'none';
-        if (typeof this.container === 'string') {
-            infoDiv.innerHTML =
-                '<div id="' + this.popupContentId + '" class="cesium-popup">' +
-                    '<div class="cesium-popup-content-wrapper">' +
-                            this.container +
-                    '</div>' +
-                    '<div class="cesium-popup-tip-container">' +
-                        '<div class="cesium-popup-tip" />' +
-                     '</div>' +
-                '</div>';
-        } else {
-            let popupContentDiv = window.document.createElement('div');
-            popupContentDiv.id = this.popupContentId;
-            popupContentDiv.className = 'cesium-popup';
-            let popupContentWrapperDiv = window.document.createElement('div');
-            popupContentWrapperDiv.className = 'cesium-popup-content-wrapper';
-            popupContentWrapperDiv.appendChild(this.container);
-            popupContentDiv.appendChild(popupContentWrapperDiv);
+    let ScreenSpaceEventHandler =
+      this.Cesium.ScreenSpaceEventHandler ||
+      window["Cesium"].ScreenSpaceEventHandler;
 
-            let tipContainerDiv = window.document.createElement('div');
-            tipContainerDiv.className = 'cesium-popup-tip-container';
-            let tipDiv = window.document.createElement('div');
-            tipDiv.className = 'cesium-popup-tip';
-            tipContainerDiv.appendChild(tipDiv);
-            popupContentDiv.appendChild(tipContainerDiv);
-            infoDiv.appendChild(popupContentDiv);
-        }
+    this.handler = new ScreenSpaceEventHandler(this.scene.canvas);
 
-        let close = window.document.createElement('div');
-        close.className = 'cesium-popup-close-button';
-        close.addEventListener('click', () => self.hide());
-        close.innerText = 'x';
-        this.parent.appendChild(infoDiv);
-
-        if (this.popupType == 'table') {
-
-        } else if (this.popupType == 'card') {
-            let image = typeof this.images == 'string' ? this.images : 
-                this.images.length > 0 ?  this.images[0] : '';
-            let temptitle = window.document.createElement('div');          
-            let images = window.document.createElement('img');  
-            temptitle.className = 'cesium-popup-images-title';      
-            images.className = 'cesium-popup-images';
-            images.src = image;
-            let parent = window.document.getElementById(this.popupContentId);
-            let node = parent.children[0].children[0].children[0];
-            temptitle.innerText = node.innerText;
-            node.innerText = '';
-            node.style.height = 'fit-content';
-            node && node.appendChild(temptitle);
-            node && node.appendChild(images);
-            // parent && parent.insertBefore(images, node); 
-        } else if (this.popupType == 'rich-text') {
-            
-        }
-
-        let separate = window.document.createElement('div');        
-        separate.className = 'cesium-popup-separate-button';
-        separate.addEventListener('click', () => self.separate());
-        let icon = `<svg width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class="">` +
-        `<use xlink:href="#mapgis-Matchsizeup"></use></svg>`
-        separate.innerHTML = icon;
-        let parent = window.document.getElementById(this.popupContentId);
-        parent && parent.appendChild(separate);    
-        
-        // window.document.getElementById(this.popupId).style.display = 'block';
-        if (this.showClose) {
-            let parent = window.document.getElementById(this.popupContentId);
-            parent && parent.appendChild(close);
-        }
-        this.infoDiv = infoDiv;
+    this.infoDiv = null;
+    // this.px_position = null;
+    if (position.entity) {
+      this.cartesian = position.entity.position._value;
     }
 
-    bindEvent() {
-        let self = this;
-        this.handler.setInputAction(this.movement, this.Cesium.ScreenSpaceEventType.LEFT_CLICK);
-        if (!this.map) {
-            return;
-        }
+    this.cartesian =
+      this.cartesian ||
+      this.position.cartesian ||
+      this.Cesium.Cartesian3.fromDegrees(
+        this.position.longitude,
+        this.position.latitude,
+        this.position.height
+      );
 
-        if (this.options.postRender) {
-            this.map.scene.postRender.addEventListener(() => self.update());
-        } else {
-            this.map.camera.changed.addEventListener(() => self.update());
-            this.handler.setInputAction(this.moveStart, this.Cesium.ScreenSpaceEventType.LEFT_DOWN);
-            this.handler.setInputAction(this.moveEnd, this.Cesium.ScreenSpaceEventType.LEFT_UP);
-            this.map.scene.camera.moveEnd.addEventListener(() => self.update());
+    let vc = this.map.container;
+    let cesumWidgetContainer = undefined;
+    if (vc.children && vc.children.length > 0) {
+      if (vc.children[0].children && vc.children[0].children.length > 0) {
+        if (
+          vc.children[0].children[0].children &&
+          vc.children[0].children[0].children.length > 0
+        ) {
+          cesumWidgetContainer = vc.children[0].children[0].children[0];
         }
+      }
     }
 
-    unbindEvent() {
-        let self = this;
-        if (!this.map) {
-            return;
-        }
-
-        if (this.options.postRender) {
-            this.map.scene.postRender.removeEventListener(() => self.update());
-        } else {
-            this.map.camera.changed.removeEventListener(() => self.update());
-            this.map.scene.camera.moveEnd.removeEventListener(() => self.update());
-            this.handler.destroy();
-        }
+    if (!cesumWidgetContainer) {
+      let parents = document.getElementsByClassName("cesium-widget");
+      parent = parents.length > 0 ? parents[0] : map.container;
+      this.parent = parent;
+    } else {
+      this.parent = cesumWidgetContainer;
     }
 
-    movement(movement) {
-        let pickedPrimitive = this.map.scene.pick(movement.position);
-        let pickedEntity = this.Cesium.defined(pickedPrimitive) ? pickedPrimitive.id : undefined;
-        if (this.Cesium.defined(pickedEntity) /* && Cesium.defined(pickedEntity.billboard) */) {
-            if (this.position && this.position.entity) {
-                pickedPrimitive.id === this.position.entity.id;
-                this.show();
-            }
-        }
+    // this.initDevicePixelRatio();
+    this.showClose = options.showClose === undefined ? true : options.showClose;
+    this.popupType = options.popupType;
+    this.images = options.images;
+    this.description = options.description;
+    this.popup = this._createPopup();
+
+    this.moveStart = this.eventMoveStart.bind(this);
+    this.moveEnd = this.eventMoveEnd.bind(this);
+    this.movement = this.movement.bind(this);
+    this.update = this.update.bind(this);
+
+    this.bindEvent();
+
+    return this;
+  }
+
+  _createPopup() {
+    const self = this;
+    this.hide = this.hide.bind(this);
+    let infoDiv = window.document.createElement("div");
+    infoDiv.id = this.popupId;
+    infoDiv.style.display = "none";
+    if (typeof this.container === "string") {
+      infoDiv.innerHTML =
+        '<div id="' +
+        this.popupContentId +
+        '" class="cesium-popup">' +
+        '<div class="cesium-popup-content-wrapper">' +
+        this.container +
+        "</div>" +
+        '<div class="cesium-popup-tip-container">' +
+        '<div class="cesium-popup-tip" />' +
+        "</div>" +
+        "</div>";
+    } else {
+      let popupContentDiv = window.document.createElement("div");
+      popupContentDiv.id = this.popupContentId;
+      popupContentDiv.className = "cesium-popup";
+      let popupContentWrapperDiv = window.document.createElement("div");
+      popupContentWrapperDiv.className = "cesium-popup-content-wrapper";
+      popupContentWrapperDiv.appendChild(this.container);
+      popupContentDiv.appendChild(popupContentWrapperDiv);
+
+      let tipContainerDiv = window.document.createElement("div");
+      tipContainerDiv.className = "cesium-popup-tip-container";
+      let tipDiv = window.document.createElement("div");
+      tipDiv.className = "cesium-popup-tip";
+      tipContainerDiv.appendChild(tipDiv);
+      popupContentDiv.appendChild(tipContainerDiv);
+      infoDiv.appendChild(popupContentDiv);
     }
 
-    eventMoveStart(movement) {
-        this.hide();
+    let close = window.document.createElement("div");
+    close.className = "cesium-popup-close-button";
+    close.addEventListener("click", () => self.hide());
+    close.innerText = "x";
+    this.parent.appendChild(infoDiv);
+
+    if (this.popupType == "table") {
+    } else if (this.popupType == "card") {
+      let image =
+        typeof this.images == "string"
+          ? this.images
+          : this.images.length > 0
+          ? this.images[0]
+          : "";
+      let temptitle = window.document.createElement("div");
+      let images = window.document.createElement("img");
+      temptitle.className = "cesium-popup-images-title";
+      images.className = "cesium-popup-images";
+      images.src = image;
+      let parent = window.document.getElementById(this.popupContentId);
+      if (
+        parent &&
+        parent.children &&
+        parent.children[0] &&
+        parent.children[0].children &&
+        parent.children[0].children[0] &&
+        parent.children[0].children[0].children &&
+        parent.children[0].children[0].children[0]
+      ) {
+        let node = parent.children[0].children[0].children[0];
+        temptitle.innerText = node.innerText;
+        node.innerText = "";
+        node.style.height = "fit-content";
+        node && node.appendChild(temptitle);
+        node && node.appendChild(images);
+        // parent && parent.insertBefore(images, node);
+      }
+    } else if (this.popupType == "rich-text") {
     }
 
-    eventMoveEnd() {
-        this.update();
+    let separate = window.document.createElement("div");
+    separate.className = "cesium-popup-separate-button";
+    separate.addEventListener("click", () => self.separate());
+    let icon =
+      `<svg width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class="">` +
+      `<use xlink:href="#mapgis-Matchsizeup"></use></svg>`;
+    separate.innerHTML = icon;
+    let parent = window.document.getElementById(this.popupContentId);
+    parent && parent.appendChild(separate);
+
+    // window.document.getElementById(this.popupId).style.display = 'block';
+    if (this.showClose) {
+      let parent = window.document.getElementById(this.popupContentId);
+      parent && parent.appendChild(close);
+    }
+    this.infoDiv = infoDiv;
+  }
+
+  bindEvent() {
+    let self = this;
+    this.handler.setInputAction(
+      this.movement,
+      this.Cesium.ScreenSpaceEventType.LEFT_CLICK
+    );
+    if (!this.map) {
+      return;
     }
 
-    update() {
-        if (this.cartesian && this.isShow) {
-            updataPopupPosition(this.map, this.cartesian, this.popupId, this.popupContentId, this.options);
-        }
+    if (this.options.postRender) {
+      this.map.scene.postRender.addEventListener(() => self.update());
+    } else {
+      this.map.camera.changed.addEventListener(() => self.update());
+      this.handler.setInputAction(
+        this.moveStart,
+        this.Cesium.ScreenSpaceEventType.LEFT_DOWN
+      );
+      this.handler.setInputAction(
+        this.moveEnd,
+        this.Cesium.ScreenSpaceEventType.LEFT_UP
+      );
+      this.map.scene.camera.moveEnd.addEventListener(() => self.update());
+    }
+  }
+
+  unbindEvent() {
+    let self = this;
+    if (!this.map) {
+      return;
     }
 
-    /**
-     * 显示图层
-     * @function module:客户端可视化.PopupLayer.prototype.show
-     */
-    show() {
-        this.isShow = true;
-        if (this.onShow) {
-            this.onShow(this.isShow);
-        }
-        let node = window.document.getElementById(this.popupId);
-        if (node && node.style) {
-            node.style.display = 'block';
-        }
+    if (this.options.postRender) {
+      this.map.scene.postRender.removeEventListener(() => self.update());
+    } else {
+      this.map.camera.changed.removeEventListener(() => self.update());
+      this.map.scene.camera.moveEnd.removeEventListener(() => self.update());
+      this.handler.destroy();
     }
+  }
 
-    /**
-     * 隐藏图层
-     * @function module:客户端可视化.PopupLayer.prototype.hide
-     */
-    hide() {
-        this.isShow = false;
-        if (this.onHide) {
-            this.onHide(this.isShow);
-        }
-        let node = window.document.getElementById(this.popupId);
-        if (node && node.style) {
-            node.style.display = 'none';
-        }
+  movement(movement) {
+    let pickedPrimitive = this.map.scene.pick(movement.position);
+    let pickedEntity = this.Cesium.defined(pickedPrimitive)
+      ? pickedPrimitive.id
+      : undefined;
+    if (
+      this.Cesium.defined(
+        pickedEntity
+      ) /* && Cesium.defined(pickedEntity.billboard) */
+    ) {
+      if (this.position && this.position.entity) {
+        pickedPrimitive.id === this.position.entity.id;
+        this.show();
+      }
     }
+  }
 
-    /**
-     * 分离Popup
-     * @function module:客户端可视化.PopupLayer.prototype.
-     */
-     separate() {
-        this.hide();
-        if (this.onSeparate) {
-            this.onSeparate();
-        }
-    }    
+  eventMoveStart(movement) {
+    this.hide();
+  }
 
-    /**
-     * 删除图层
-     * @function module:客户端可视化.PopupLayer.prototype.remove
-     */
-    remove() {
-        this.unbindEvent();
-        let node = window.document.getElementById(this.popupId);
-        if (node) {
-            if (node.parentNode) {
-                node.parentNode.removeChild(node);
-            } else {
-                node.remove();
-            }
-        }
-        return this;
+  eventMoveEnd() {
+    this.update();
+  }
+
+  update() {
+    if (this.cartesian && this.isShow) {
+      updataPopupPosition(
+        this.map,
+        this.cartesian,
+        this.popupId,
+        this.popupContentId,
+        this.options
+      );
     }
+  }
+
+  /**
+   * 显示图层
+   * @function module:客户端可视化.PopupLayer.prototype.show
+   */
+  show() {
+    this.isShow = true;
+    if (this.onShow) {
+      this.onShow(this.isShow);
+    }
+    let node = window.document.getElementById(this.popupId);
+    if (node && node.style) {
+      node.style.display = "block";
+    }
+  }
+
+  /**
+   * 隐藏图层
+   * @function module:客户端可视化.PopupLayer.prototype.hide
+   */
+  hide() {
+    this.isShow = false;
+    if (this.onHide) {
+      this.onHide(this.isShow);
+    }
+    let node = window.document.getElementById(this.popupId);
+    if (node && node.style) {
+      node.style.display = "none";
+    }
+  }
+
+  /**
+   * 分离Popup
+   * @function module:客户端可视化.PopupLayer.prototype.
+   */
+  separate() {
+    this.hide();
+    if (this.onSeparate) {
+      this.onSeparate();
+    }
+  }
+
+  /**
+   * 删除图层
+   * @function module:客户端可视化.PopupLayer.prototype.remove
+   */
+  remove() {
+    this.unbindEvent();
+    let node = window.document.getElementById(this.popupId);
+    if (node) {
+      if (node.parentNode) {
+        node.parentNode.removeChild(node);
+      } else {
+        node.remove();
+      }
+    }
+    return this;
+  }
 }
