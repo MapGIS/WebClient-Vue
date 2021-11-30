91a50313cc66c4b5d14672821b445a45a8728422
killer1992 <killer1992@yeah.net>
2021-11-30 14:23:53 +0800

    【cesium】【杨琨】【屏蔽mapstory】

--- ui/src/component.js
+++ ui/src/component.js
@@ -10,7 +10,7 @@ export { default as MapgisUiIcon } from "./components/iconfont/IconSvgImg.vue";
 export { default as MapgisUiIconFont } from "./components/iconfont/Icon.vue";
 export { default as MapgisUiSvgIcon } from "./components/iconfont/SvgIcon";
 export { default as MapgisUiBase64Icon } from "./components/iconfont/Base64Icon";
-export { default as MapgisUiBase64IconsKeyValue } from "./components/iconfont/Base64IconsKeyValue";
+// export { default as MapgisUiBase64IconsKeyValue } from "./components/iconfont/Base64IconsKeyValue";
 export { default as MapgisUiIconsPanel } from "./components/iconfont/IconsPanel";
 export { default as MapgisUiIconsPanelScrollX } from "./components/iconfont/IconsPanelScrollX";
 export { default as MapgisUiTitleIcon } from "./components/iconfont/TitleIcon";
