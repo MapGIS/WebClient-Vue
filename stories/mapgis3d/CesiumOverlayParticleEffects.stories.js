7e527c5cb18411190a88467370e483cd1704abb2
zhangying-zy618 <1057936309@qq.com>
2021-12-01 18:35:53 +0800

    【SDK】【Cesium】【优化粒子特效】

--- stories/mapgis3d/CesiumOverlayParticleEffects.stories.js
+++ stories/mapgis3d/CesiumOverlayParticleEffects.stories.js
@@ -1,104 +1,65 @@
 import "../style/card.css";
-import fireImg from "../assets/fire.png";
-import smokeImg from "../assets/smoke.png";
-import Markdown from "../../cesium/docs/api/analysis/ParticleEffects.md";
+import Markdown from "../../cesium/docs/api/Overlay/ParticleEffects.md";
+
 
 export default {
-  title: "三维/可视化/粒子特效",
-  argTypes: {
-    emissionRate: {
-      description: "发射速率(个/秒)",
-      table: {
-        defaultValue: { summary: 20.0 },
-      },
-      control: "number",
+    title: "三维/可视化/粒子特效",
+    argTypes: {
+        particleList:{
+            description: "粒子特效参数配置集",
+            table: {
+                defaultValue: {
+                    summary: '[]',
+                },
+            },
+            control: "array",
+        },
+        symbolList:{
+            description: "符号库参数配置",
+            table: {
+                defaultValue: {
+                    summary:'[{\n' +
+                        '          guid: "C0EA27B2-0365-1F9F-C71A-B0586ADDCA0D",\n' +
+                        '          name: "火焰",\n' +
+                        '          image: "./fire.png",\n' +
+                        '          iconUrl: "mapgis-fire",\n' +
+                        '        },\n' +
+                        '        {\n' +
+                        '          guid: "B8AF7BAC-082F-14C6-BECD-8F7AB44C5019",\n' +
+                        '          name: "烟雾",\n' +
+                        '          image: "./smoke.png",\n' +
+                        '          iconUrl: "mapgis-smoke",\n' +
+                        '        }]'
+                },
+            },
+            control: "array",
+        }
     },
-    imageSize: {
-      description: "尺寸(像素)",
-      table: {
-        defaultValue: { summary: 5.0 },
-      },
-      control: "number",
-    },
-    minimumParticleLife: {
-      description: "粒子最小存在时间(秒)",
-      table: {
-        defaultValue: { summary: 2.0 },
-      },
-      control: "number",
-    },
-    maximumParticleLife: {
-      description: "粒子最大存在时间(秒)",
-      table: {
-        defaultValue: { summary: 3.0 },
-      },
-      control: "number",
-    },
-    minimumSpeed: {
-      description: "最小速度(个/秒)",
-      table: {
-        defaultValue: { summary: 9.0 },
-      },
-      control: "number",
-    },
-    maximumSpeed: {
-      description: "最大速度(个/秒)",
-      table: {
-        defaultValue: { summary: 9.5 },
-      },
-      control: "number",
-    },
-    startScale: {
-      description: "初始比例",
-      table: {
-        defaultValue: { summary: 1.0 },
-      },
-      control: "number",
-    },
-    endScale: {
-      description: "结束比例",
-      table: {
-        defaultValue: { summary: 4.0 },
-      },
-      control: "number",
-    },
-    emitterType: {
-      description:
-        "发射类型:可选 '盒状放射', '圆形放射', '锥形放射', '球形放射'",
-      table: {
-        defaultValue: { summary: "圆形放射" },
-      },
-      control: {
-        type: "select",
-        options: ["盒状放射", "圆形放射", "锥形放射", "球形放射"],
-      },
-    },
-  },
 };
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  data() {
-    return {
-      url: "http://t0.tianditu.gov.cn/img_c/wmts",
-      m3dUrl: `http://${window.webclient.ip}:${window.webclient.port}/igs/rest/g3d/ZondyModels`,
-      maximumScreenSpaceError: 8,
-      tileMatrixSet: "c",
-      tilingScheme: "EPSG:4326",
-      layer: "img",
-      format: "tiles",
-      token: {
-        key: "tk",
-        value: "2ddaabf906d4b5418aed0078e1657029",
-      },
-      autoReset:true,
-      particleMode: "",
-      imgUrl: "",
-      particleEffects: null,
-    };
-  },
-  template: `
-  <mapgis-web-scene :style="{height: '95vh'}">
+const Template = (args, {argTypes}) => ({
+    props: Object.keys(argTypes),
+    data() {
+        return {
+            url: "http://t0.tianditu.gov.cn/img_c/wmts",
+            m3dUrl: `http://${window.webclient.ip}:${window.webclient.port}/igs/rest/g3d/ZondyModels`,
+            // m3dUrl: `http://localhost:6163/igs/rest/g3d/BIM模型`,
+            maximumScreenSpaceError: 8,
+            tileMatrixSet: "c",
+            tilingScheme: "EPSG:4326",
+            layer: "img",
+            format: "tiles",
+            token: {
+                key: "tk",
+                value: "2ddaabf906d4b5418aed0078e1657029",
+            },
+            autoReset: true,
+            particleEffects: null,
+            showParticle: true
+        };
+    },
+    template: `
+      <mapgis-web-scene :style="{height: '95vh'}">
       <mapgis-3d-ogc-wmts-layer
           :baseUrl="url"
           :wmtsLayer="layer"
@@ -107,78 +68,97 @@ const Template = (args, { argTypes }) => ({
           :tilingScheme="tilingScheme"
           :token="token"
       ></mapgis-3d-ogc-wmts-layer>
-      <mapgis-3d-m3d-layer :autoReset="autoReset" :maximumScreenSpaceError="maximumScreenSpaceError" :url="m3dUrl"></mapgis-3d-m3d-layer>
-      <mapgis-ui-card class="storybook-ui-card thin">
-      <mapgis-ui-toolbar>
-      <mapgis-ui-toolbar-command-group>
-        <mapgis-ui-toolbar-command
-          title="火焰"
-          icon="mapgis-fire"
-          :active="particleMode === 'fire'"
-          @click="onCreateParticle('fire')"
-        />
-        <mapgis-ui-toolbar-command
-          title="烟雾"
-          icon="mapgis-smoke"
-          :active="particleMode === 'smoke'"
-          @click="onCreateParticle('smoke')"
-        />
-      </mapgis-ui-toolbar-command-group>
-      <mapgis-ui-toolbar-space />
-      <mapgis-ui-toolbar-command-group>
-        <mapgis-ui-toolbar-command
-          title="清除"
-          icon="mapgis-delete"
-          @click="onClearParticle"
-        />
-      </mapgis-ui-toolbar-command-group>
-    </mapgis-ui-toolbar>
-        <mapgis-3d-particle-effects
-            :emissionRate="emissionRate"
-            :imageSize="imageSize"
-            :minimumParticleLife="minimumParticleLife"
-            :maximumParticleLife="maximumParticleLife"
-            :minimumSpeed="minimumSpeed"
-            :maximumSpeed="maximumSpeed"
-            :startScale="startScale"
-            :endScale="endScale"
-            :emitterType="emitterType"
-            :imgUrl="imgUrl"
+      <mapgis-3d-m3d-layer :autoReset="autoReset" :maximumScreenSpaceError="maximumScreenSpaceError" :url="m3dUrl"
+                           @loaded="loaded"></mapgis-3d-m3d-layer>
+      <mapgis-ui-card v-if="showParticle"
+                      class="storybook-ui-card">
+        <mapgis-3d-particle-effects-manager
+            :symbolList="symbolList"
+            :particleList="particleList"
+            @changeParticel="changeParticle"
             @load="load"/>
       </mapgis-ui-card>
+      <mapgis-3d-statebar></mapgis-3d-statebar>
       </mapgis-web-scene>
     `,
-  methods: {
-    load(particleEffects) {
-      this.particleEffects = particleEffects;
+    methods: {
+        load(particleEffects) {
+            this.particleEffects = particleEffects;
+        },
+        loaded(e) {
+            this.showParticle = true;
+        },
+        changeParticle(e){
+            console.log("particleList",e);
+        }
     },
-    onCreateParticle(type) {
-      this.particleMode = type;
-      this.imgUrl = type === "fire" ? fireImg : smokeImg;
-      this.particleEffects.onCreateParticle();
-    },
-    onClearParticle() {
-      this.particleEffects.onClearParticle();
-    },
-  },
 });
 
 export const 粒子特效 = Template.bind({});
 粒子特效.args = {
-  emissionRate: 2.0, // 发射速率
-  imageSize: 5.0, // 尺寸
-  minimumParticleLife: 2.0, // 粒子最小存在时间
-  maximumParticleLife: 3.0, //粒子最大存在时间
-  minimumSpeed: 9.0, // 最小速度
-  maximumSpeed: 9.5, // 最大速度
-  startScale: 1.0, // 初始比例
-  endScale: 4.0, // 结束比例
-  emitterType: "圆形放射",
+    symbolList:[
+        {
+            guid: "9D09DB87-7955-9295-2E34-61E83C30D3AA",
+            name: "外部火焰",
+            image: `http://localhost:8895/img/fire.png`,
+            iconUrl: "mapgis-huoyan",
+        },
+        {
+            guid: "F9FDE880-8F5B-AEDF-CA95-ADC54F02A34F",
+            name: "外部烟雾",
+            image: `http://localhost:8895/img/smoke.png`,
+            iconUrl: "mapgis-yanwu",
+        },
+    ],
+    particleList:[
+        {
+            guid: "49A834D7-97C6-F452-4611-6F0739809B50",
+            name: "粒子名称1",
+            param: {
+                emitterType: "圆形放射",  //发射类型
+                emissionRate: 20.0,     //发射速率
+                imageSize: 5.0,         //尺寸
+                minimumParticleLife: 2.0,  //粒子最小存在时间
+                maximumParticleLife: 3.0,  //粒子最大存在时间
+                minimumSpeed: 9.0,         //最小速度
+                maximumSpeed: 10.0,        //最大速度
+                startScale: 1.0,           //初始比例
+                endScale: 4.0,             //结束比例
+                symbolGuid: "9D09DB87-7955-9295-2E34-61E83C30D3AA",
+                position: {
+                    longitude: 114.402023,
+                    latitude: 30.46666308,
+                    height: 7.160341631125318
+                }
+            }
+        },
+        {
+            guid: "36F335E8-1F3C-41E2-40AA-EE950D691761",
+            name: "粒子名称2",
+            param: {
+                emitterType: "球形放射",  //发射类型
+                emissionRate: 40.0,     //发射速率
+                imageSize: 6.0,         //尺寸
+                minimumParticleLife: 2.0,  //粒子最小存在时间
+                maximumParticleLife: 3.0,  //粒子最大存在时间
+                minimumSpeed: 9.0,         //最小速度
+                maximumSpeed: 10.0,        //最大速度
+                startScale: 1.0,           //初始比例
+                endScale: 4.0,             //结束比例
+                symbolGuid: "9D09DB87-7955-9295-2E34-61E83C30D3AA",
+                position: {
+                    longitude: 114.40092382,
+                    latitude: 30.46549092,
+                    height: 1
+                }
+            }
+        }
+    ]
 };
 粒子特效.parameters = {
-  docs: {
-    description: {
-      component: Markdown,
+    docs: {
+        description: {
+            component: Markdown,
+        },
     },
-  },
 };
