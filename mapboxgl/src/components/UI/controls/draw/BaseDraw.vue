94f9d44c895bb94dbc0ab7c926ba5b71623a1e5d
zhangying-zy618 <1057936309@qq.com>
2021-12-14 19:49:31 +0800

    【SDK】【mapboxgl】【修正绘制组件的画半径圆，画出椭圆的bug】

--- mapboxgl/src/components/UI/controls/draw/BaseDraw.vue
+++ mapboxgl/src/components/UI/controls/draw/BaseDraw.vue
@@ -287,8 +287,8 @@ export default {
 
   methods: {
     enableDrawer() {
-      this.$_initDraw();
       this.$_compareStyle();
+      this.$_initDraw();
       this.$_unbindEditEvents();
       this.$_unbindMeasureEvents();
       this.$_addDrawControl(this.drawer);
