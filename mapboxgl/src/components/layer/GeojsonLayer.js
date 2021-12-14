f8fb192fce45669e16a1efdf0339178b157903b2
killer1992 <killer1992@yeah.net>
2021-12-14 19:12:58 +0800

    【mapboxgl】【杨琨】【修复GeojsonLayer报id不存在的问题】

--- mapboxgl/src/components/layer/GeojsonLayer.js
+++ mapboxgl/src/components/layer/GeojsonLayer.js
@@ -562,7 +562,7 @@ export default {
             ...point.toMapboxStyle({ highlight: true })
           };
         }
-        if (!map.getLayer(highlight.id)) map.addLayer(highlight);
+        if (!map.getLayer(id) && highlight) map.addLayer(highlight);
       }
     }
   }
