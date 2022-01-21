eb50bb43045ea93feb25c4598a94efbf243c9ede
lvzhantian <2771997672@qq.com>
2022-01-21 14:23:29 +0800

    【cesium】【综合分析】【半径单位优化】

--- stories/mapgis3d/CesiumAnalysisOverlay.stories.js
+++ stories/mapgis3d/CesiumAnalysisOverlay.stories.js
@@ -1,22 +1,22 @@
 import { Style } from "@mapgis/webclient-es6-service";
-const { LineStyle, PointStyle, FillStyle, Shadow } = Style;
+const { LineStyle, PointStyle, FillStyle } = Style;
 
 export default {
   title: "三维/分析/叠加分析",
   argTypes: {
-    baseUrl:{
-      description: "http://localhost:6163",
-      table:{
-        defaultValue: { summary: "http://localhost:6163" },
-      },
-      control:'text'
-    },
     srcType: {
-      description: "缓冲数据源类型: 图层级叠加Layer/要素级叠加Feature",
+      description: "叠加数据源类型：图层级叠加Layer；要素级叠加Feature",
       table:{
         defaultValue: { summary: "Layer" },
       },
       control: 'text'
+    },
+    baseUrl:{
+      description: "输入图层的baseUrl",
+      table:{
+        defaultValue: { summary: "http://localhost:6163" },
+      },
+      control:'text'
     },
     srcALayer: {
       description: "输入被叠加图层的gdbp",
@@ -39,87 +39,44 @@ export default {
       },
       control:'object'
     },
-    srcBFeature: {
-      description: "输入叠加要素的GeoJSON数据",
-      table:{
-        defaultValue: { summary: {} },
-      },
-      control:'object'
-    },
-  },
+  }
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   data() {
     return {
-      finishL: false,
-      gdbps: "",
-      add: false,
-      feature: {
-        "type": "FeatureCollection",
-        "features": [
-          {
-            "type": "Feature",
-            "properties": {},
-            "id": "id1",
-            "geometry": {
-              "type": "Polygon",
-              "coordinates": [
-                [ [114,30], [114,31], [115,31], [115,30], [114,30] ]
-              ]
-            },
-          },
-          {
-            "type": "Feature",
-            "properties": {},
-            "id": "id2",
-            "geometry": {
-              "type": "Polygon",
-              "coordinates": [
-                [ [115,30] , [115,31] , [116,31] , [116,30] , [115,30] ]
-              ]
-            },
-          },
-        ]
-      },
-      layerStyle: new FillStyle({
-        color: "#ff0000",
-        outlineColor: "#ff0000",
-        outlineWidth: 2.5,
-        opacity: 1,
-      }),
+      finishedResult: false,
+      resultLayer: "",
+      addResultToLayer: false
     };
   },
   methods: {
     showLayer(data) {
-      this.finishL = true
-      this.gdbps = data
+      this.finishedResult = true
+      this.resultLayer = data
     },
     showAdd(data) {
-      this.add = data
+      this.addResultToLayer = data
     },
   },
   template: `
     <mapgis-web-scene style="height: 95vh">
-      <mapgis-3d-igs-dynamic-layer baseUrl="http://192.168.21.192:6163/igs/rest/mrms/layers" gdbps="gdbp://MapGisLocal/专题图数据/sfcls/县城驻地"></mapgis-3d-igs-dynamic-layer>
-      <mapgis-3d-igs-dynamic-layer baseUrl="http://192.168.21.192:6163/igs/rest/mrms/layers" gdbps="gdbp://MapGISLocal/Templates/sfcls/湖北省市级区划"></mapgis-3d-igs-dynamic-layer>
       <mapgis-ui-card class="storybook-ui-card">
         <mapgis-3d-overlay-analysis v-bind="$props" v-on:listenLayer="showLayer" v-on:listenOverlayAdd="showAdd"/>
       </mapgis-ui-card>
-      <mapgis-3d-igs-dynamic-layer v-if="finishL && add" baseUrl="http://localhost:6163/igs/rest/mrms/layers" :gdbps="gdbps"></mapgis-3d-igs-dynamic-layer>
+      <mapgis-3d-igs-dynamic-layer v-if="finishedResult && addResultToLayer" baseUrl="http://localhost:6163/igs/rest/mrms/layers" :gdbps="resultLayer"></mapgis-3d-igs-dynamic-layer>
     </mapgis-web-scene>
     `,
 });
 
 export const 叠加分析组件 = Template.bind({});
 叠加分析组件.args = {
-  baseUrl: "http://192.168.21.192:6163/",
   srcType: "Layer",
-  // srcALayer: "gdbp://MapGISLocalPlus/sample/sfcls/湖北省路网",
-  srcALayer: "gdbp://MapGisLocal/专题图数据/sfcls/县城驻地",
-  // srcBLayer: "gdbp://MapGISLocalPlus/sample/sfcls/武汉市轮廓",
-  srcBLayer: "gdbp://MapGISLocal/Templates/sfcls/湖北省市级区划",
+  // srcType: "Feature",
+  baseUrl: "http://localhost:6163",
+  srcALayer: "gdbp://MapGISLocalPlus/sample/sfcls/湖北省路网",
+  srcBLayer: "gdbp://MapGISLocalPlus/sample/sfcls/武汉市轮廓",
   srcAFeature: {
     "type": "FeatureCollection",
     "features": [
@@ -146,6 +103,5 @@ export const 叠加分析组件 = Template.bind({});
         },
       },
     ]
-  },
-  srcBFeature: {},
+  }
 };
