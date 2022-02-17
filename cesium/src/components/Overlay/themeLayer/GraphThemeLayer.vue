4b36eeed74ba0685028640a38297cec83f851166
gongyuejian <gongyuejian.000@mapgis.com>
2022-02-17 09:03:28 +0800

    【SDK】【Cesium】【升级Cesium统计专题服务】

--- cesium/src/components/Overlay/themeLayer/GraphThemeLayer.vue
+++ cesium/src/components/Overlay/themeLayer/GraphThemeLayer.vue
@@ -32,6 +32,21 @@ export default {
     addExtrudedHeight: {
       type: Boolean,
       default: true
+    },
+    // 标注字体
+    textFont: {
+      type: String,
+      default: "50px Helvetica"
+    },
+    // 标注颜色
+    textColor: {
+      type: String,
+      default: "#008000"
+    },
+    // 字体高度的偏移值
+    textHeightOffset: {
+      type: Number,
+      default: 10000
     }
   },
   computed: {
@@ -100,6 +115,11 @@ export default {
         this.thematicMapLayer.attributeName = this.attributeName;
         this.thematicMapLayer.addGeoGeometry = false;
         this.thematicMapLayer.attributeColor = this.ceisumColors;
+        this.thematicMapLayer.textFont = this.textFont;
+        this.thematicMapLayer.textColor = Cesium.Color.fromCssColorString(
+          this.textColor
+        );
+        this.thematicMapLayer.textHeightOffset = this.textHeightOffset;
         if (this.type === "Pie") {
           this.thematicMapLayer.addExtrudedHeight = this.addExtrudedHeight;
         }
