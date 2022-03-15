5b17d738c3cad5f1bc8a9e4a65b91cded962ac8b
gongyuejian <gongyuejian.000@mapgis.com>
2022-03-15 15:01:54 +0800

    【storybook】【Cesium】【增加截图示例】

---
+++ stories/mapgis3d/CesiumControlOutputImage.stories.js
@@ -0,0 +1,33 @@
+import Mapgis3dOutputImage from "../../cesium/src/components/UI/Controls/OutputImage/OutputImage.vue";
+import Markdown from "../../cesium/docs/api/ui/outputimage.md";
+
+export default {
+  title: "三维/场景子组件/截图",
+  component: Mapgis3dOutputImage,
+  argTypes: {},
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Mapgis3dOutputImage },
+  template: `
+  <mapgis-web-scene style="height:95vh">
+    <mapgis-ui-card class="storybook-ui-card">
+      <mapgis-3d-output-image />
+    </mapgis-ui-card>
+  </mapgis-web-scene>
+    `,
+  data() {
+    return {};
+  },
+});
+
+export const 截图 = Template.bind({});
+截图.args = {};
+截图.parameters = {
+  docs: {
+    description: {
+      component: Markdown,
+    },
+  },
+};
