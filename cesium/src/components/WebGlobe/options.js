4a4483e403cb9ed8729bcb9013de17b3def89a9c
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2022-03-18 18:10:06 +0800

    【SDK】【Cesium】【修复对应的相机视角】

--- cesium/src/components/WebGlobe/options.js
+++ cesium/src/components/WebGlobe/options.js
@@ -12,69 +12,69 @@ export default {
    */
   vueIndex: {
     typs: [String, Number],
-    default: () => (Math.random() * 1000000).toFixed(0)
+    default: () => (Math.random() * 1000000).toFixed(0),
   },
   container: {
     type: [String, HTMLElement],
     default() {
       return `cesium-${("" + Math.random()).split(".")[1]}`;
-    }
+    },
   },
   /**
    * @description 默认主页按钮不显示
    */
   homeButton: {
     type: Boolean,
-    default: false
+    default: false,
   },
   /**
    * @description 默认导航按钮不显示
    */
   navigationHelpButton: {
     type: Boolean,
-    default: false
+    default: false,
   },
   /**
    * @description 默认模型切换按钮不显示
    */
   sceneModePicker: {
     type: Boolean,
-    default: false
+    default: false,
   },
   /**
    * @description 默认动画控制不显示
    */
   animation: {
     type: Boolean,
-    default: false
+    default: false,
   },
   /**
    * @description 默认图层选择器不显示
    */
   baseLayerPicker: {
     type: Boolean,
-    default: false
+    default: false,
   },
   /**
    * @description 默认全屏控件不显示
    */
   fullscreenButton: {
     type: Boolean,
-    default: false
+    default: false,
   },
   /**
    * @description 默认VR控件不显示
    */
   vrButton: {
     type: Boolean,
-    default: false
+    default: false,
   },
   /**
    * @description 默认地理编码控件不显示
    */
   geocoder: {
     type: Boolean,
-    default: false
+    default: false,
   },
 
   /**
@@ -83,7 +83,7 @@ export default {
    * COLUMBUS_VIEW: 1 SCENE2D: 2 SCENE3D: 3
    */
   sceneMode: {
-    type: Number
+    type: Number,
   },
 
   /**
@@ -91,17 +91,35 @@ export default {
    */
   timeline: {
     type: Boolean,
-    default: false
+    default: false,
   },
 
   contextOptions: {
     type: Object,
-    default: function() {
+    default: function () {
       return {
         webgl: {
-          preserveDrawingBuffer: true
-        }
+          preserveDrawingBuffer: true,
+        },
       };
-    }
-  }
+    },
+  },
+
+  cameraView: {
+    type: Object,
+    default: function () {
+      return {
+        /* destination: {
+          x: -5087907.392038159,
+          y: 14207074.175879652,
+          z: 3655215.2541255946
+        },
+        orientation: {
+          heading: 6.1827568973283045,
+          pitch: -1.2409374391413084,
+          roll: 0.0003114284469649675
+        } */
+      };
+    },
+  },
 };
--- stories/mapgis3d/CesiumBaseGlobe.stories.js
+++ stories/mapgis3d/CesiumBaseGlobe.stories.js
@@ -4,7 +4,34 @@ import "../style/scene.css";
 export default {
   title: "三维/场景/地图场景",
   component: MapgisWebGlobe,
-  argTypes: {},
+  argTypes: {
+    homeButton: false,
+    navigationHelpButton: false,
+    sceneModePicker: false,
+    animation: false,
+    baseLayerPicker: false,
+    fullscreenButton: false,
+    vrButton: false,
+    geocoder: false,
+    timeline: false,
+    cameraView: {
+      type: Object,
+      default: function () {
+        return {
+          /* destination: {
+            x: -5087907.392038159,
+            y: 14207074.175879652,
+            z: 3655215.2541255946
+          },
+          orientation: {
+            heading: 6.1827568973283045,
+            pitch: -1.2409374391413084,
+            roll: 0.0003114284469649675
+          } */
+        };
+      }
+    }
+  },
 };
 
 const Template = (args, { argTypes }) => ({
@@ -21,6 +48,7 @@ const Template = (args, { argTypes }) => ({
         top: "10px",
       },
       position: "top-left",
+      info: "{}"
     };
   },
   methods: {
@@ -37,6 +65,14 @@ const Template = (args, { argTypes }) => ({
       viewer.entities.removeAll();
       // viewer.destroy();
     },
+    camera() {
+      let viewer = window.viewer;
+      let view = viewer.camera.getView();
+      delete view.endTransform;
+      delete view.positionWC;
+      let info = JSON.stringify(view);
+      this.info = info;
+    },
   },
   template: `<mapgis-web-scene v-bind="$props" style="height:95vh" @load="load">
     <mapgis-ui-collapse-card :outStyle="outStyle" :position="position" ref="card">
@@ -44,7 +80,9 @@ const Template = (args, { argTypes }) => ({
       <span slot="extra">
         <mapgis-ui-iconfont type="mapgis-map-pin" @click="hide" />
       </span>
-      <mapgis-ui-button type="primary" @click="remove">销毁场景</mapgis-ui-button>
+      <mapgis-ui-button size="small" type="primary" @click="remove">销毁场景</mapgis-ui-button>
+      <mapgis-ui-button size="small" type="primary" @click="camera">场景视角</mapgis-ui-button>
+      <mapgis-ui-textarea :autoSize='{"minRows":5,"maxRows":10}' v-model="info" placeholder="相机视角"></mapgis-ui-textarea>
     </mapgis-ui-collapse-card>
     
     <mapgis-3d-raster-layer url="http://t0.tianditu.com/DataServer?T=vec_w&L={z}&Y={y}&X={x}&tk=9c157e9585486c02edf817d2ecbc7752" />
@@ -53,4 +91,18 @@ const Template = (args, { argTypes }) => ({
 });
 
 export const EmptyGlobe = Template.bind({});
-EmptyGlobe.args = {};
+EmptyGlobe.args = {
+  homeButton: false,
+  navigationHelpButton: false,
+  sceneModePicker: false,
+  animation: false,
+  baseLayerPicker: false,
+  fullscreenButton: false,
+  vrButton: false,
+  geocoder: false,
+  timeline: false,
+  cameraView: {
+    "destination": { "x": -2526389.3359068986, "y": 11433026.459723352, "z": 4315159.693765257 },
+    "orientation": { "heading": 6.2728601782499815, "pitch": -1.2663199109196879, "roll": 6.283182807671632 }
+  }
+};
