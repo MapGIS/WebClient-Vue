745bd514c1ce3f3eee543b010d76ac9c21ebfc8e
gongyuejian <gongyuejian.000@mapgis.com>
2022-03-22 10:47:29 +0800

    【SDK】【UI】【解决video组件报dom找不到bug】

--- ui/src/components/media/Video.vue
+++ ui/src/components/media/Video.vue
@@ -1,16 +1,18 @@
 <template>
-  <div v-if="videoUrl && videoUrl !== ''">
-    <video
-      ref="videoPlayer"
-      class="video-js vjs-default-skin"
-      controls
-      preload="auto"
-      :width="width"
-      :height="height"
-    >
-      <source :src="videoUrl" :type="playerType" />
-    </video>
-  </div>
+  <video
+    ref="videoPlayer"
+    class="video-js vjs-default-skin"
+    controls
+    preload="auto"
+    :width="width"
+    :height="height"
+  >
+    <source
+      v-if="videoUrl && videoUrl !== ''"
+      :src="videoUrl"
+      :type="playerType"
+    />
+  </video>
 </template>
 
 <script>
@@ -60,7 +62,7 @@ export default {
     };
   },
   mounted() {
-    this.init();
+    this.addVideo();
   },
   beforeDestroy() {
     if (this.player) {
@@ -68,35 +70,30 @@ export default {
     }
   },
   methods: {
-    init() {
-      if (this.player) {
-        return;
-      }
+    initPlayer() {
+      const options = {
+        // 播放速度  playbackRates: [0.7, 1.0, 1.5, 2.0],
+        autoplay: true, // 如果true,浏览器准备好时开始回放。
+        loop: false, // 导致视频一结束就重新开始。
+        preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+      };
       const vm = this;
-      this.$nextTick(() => {
-        const options = {
-          // 播放速度  playbackRates: [0.7, 1.0, 1.5, 2.0],
-          autoplay: true, // 如果true,浏览器准备好时开始回放。
-          loop: false, // 导致视频一结束就重新开始。
-          preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-          aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-          fluid: true // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        };
-        vm.player = videojs(
-          vm.$refs.videoPlayer,
-          options,
-          function onPlayerReady() {
-            vm.$emit("onPlayerReady", this);
-          }
-        );
-        vm.addVideo();
-      });
+      vm.player = videojs(
+        vm.$refs.videoPlayer,
+        options,
+        function onPlayerReady() {
+          vm.$emit("onPlayerReady", vm.player);
+        }
+      );
+      this.addVideo();
     },
     addVideo() {
-      if (!this.player) {
-        return;
-      }
       if (this.videoUrl.length > 0) {
+        if (!this.player) {
+          this.initPlayer();
+        }
         this.player.src({
           src: this.videoUrl,
           type: this.playerType
