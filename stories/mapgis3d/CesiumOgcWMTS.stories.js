fb3286b36a649d9d31958eeccd831a1627e03de9
liao-x-d <1627930520@qq.com>
2022-03-31 16:04:45 +0800

    【stories】【Cesium】【OGC/WMTS图层增加options中tileMatrixLabels参数的描述】

--- stories/mapgis3d/CesiumOgcWMTS.stories.js
+++ stories/mapgis3d/CesiumOgcWMTS.stories.js
@@ -1,20 +1,46 @@
 import MapgisOgcWmtsLayer from "../../cesium/src/components/Layer/OGC/OGCWMTSLayer.vue";
+import TileInfo from "@mapgis/webclient-es6-service/utils/tile/TileInfo";
 
 export default {
   title: "三维/图层/OGC/WMTS",
   component: MapgisOgcWmtsLayer,
   argTypes: {
-    baseUrl:"http://develop.smaryun.com:6163/igs/rest/ogc/beijing/WMTSServer",
-    wmtsLayer: "beijing",
-    tileMatrixSet : "EPSG:4326_北京市_028mm_GB",
-    tilingScheme : "EPSG:4326",
-    layerStyle: {
-      visible: true,
-      opacity: 1,
-      zIndex: 10
-    },
-    vueIndex: 1,
-    options: {}
+    options: {
+      description:'当出现瓦片错级的情况(如使用新版本发布老版本的平台裁剪出来的瓦片）时，需要手动传入options中的tileMatrixLabels参数，其值为xml文件中TileMatrix字段下的ows:Identifier字段值组成的数组',
+      table:{
+        type:{
+          detail:'示例:通过`http://${window.webclient.ip}:${window.webclient.port}/igs/rest/ogc/{serverName}/WMTSServer`可以获取xml文件信息，以下为xml信息片段。\n'+
+          "当出现igserver与平台版本不一致导致的瓦片错级时，以以下xml信息为例需要传入`options: {tileMatrixLabels:['EPSG:4326_北京市1_028mm_GB:1','EPSG:4326_北京市1_028mm_GB:2']}`\n"+ 
+          '<TileMatrix>\n'+ 
+          '<ows:Identifier>EPSG:4326_北京市1_028mm_GB:0</ows:Identifier>\n'+
+          '<ScaleDenominator>2.798544199868153E8</ScaleDenominator>\n'+
+          '<TopLeftCorner>90.0 -180.0</TopLeftCorner>\n'+
+          '<TileWidth>512</TileWidth>\n'+
+          '<TileHeight>512</TileHeight>\n'+
+          '<MatrixWidth>1</MatrixWidth>\n'+
+          '<MatrixHeight>1</MatrixHeight>\n'+
+          '</TileMatrix>\n'+
+          '<TileMatrix>\n'+
+          '<ows:Identifier>EPSG:4326_北京市1_028mm_GB:1</ows:Identifier>\n'+
+          '<ScaleDenominator>1.3992720999340764E8</ScaleDenominator>\n'+
+          '<TopLeftCorner>90.0 -180.0</TopLeftCorner>\n'+
+          '<TileWidth>512</TileWidth>\n'+
+          '<TileHeight>512</TileHeight>\n'+
+          '<MatrixWidth>1</MatrixWidth>\n'+
+          '<MatrixHeight>1</MatrixHeight>\n'+
+          '</TileMatrix>\n'+
+          '<TileMatrix>\n'+
+          '<ows:Identifier>EPSG:4326_北京市1_028mm_GB:2</ows:Identifier>\n'+
+          '<ScaleDenominator>6.996360499670382E7</ScaleDenominator>\n'+
+          '<TopLeftCorner>90.0 -180.0</TopLeftCorner>\n'+
+          '<TileWidth>512</TileWidth>\n'+
+          '<TileHeight>512</TileHeight>\n'+
+          '<MatrixWidth>1</MatrixWidth>\n'+
+          '<MatrixHeight>1</MatrixHeight>\n'+
+          '</TileMatrix>',
+        }
+      },
+    }
   },
 };
 
