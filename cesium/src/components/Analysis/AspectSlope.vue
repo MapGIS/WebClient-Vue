5c921b053acc53515f401429c017116479244910
lvzhantian <2771997672@qq.com>
2022-05-06 20:50:50 +0800

    【cesium】【坡度坡向分析】【修复坡度分析颜色基本不对照问题】

--- cesium/src/components/Analysis/AspectSlope.vue
+++ cesium/src/components/Analysis/AspectSlope.vue
@@ -214,6 +214,7 @@ export default {
                 viewer,
                 Cesium,
                 rampColorsCopy,
+                gradual
             } = this;
             const vm = this;
 
@@ -232,8 +233,8 @@ export default {
             );
             const colors = [];
             const ramp = [];
-            rampColorsCopy.forEach(({ max, color }) => {
-                ramp.push((max / 90).toFixed(2));
+            rampColorsCopy.forEach(({ min, max, color }) => {
+                ramp.push((min / 90).toFixed(2));
                 colors.push(color);
             });
             const rampColor = this._transformColor(colors);
@@ -249,6 +250,7 @@ export default {
                         new Cesium.TerrainAnalyse(viewer, {
                             slopeRampColor: rampColor,
                             slopeRamp: ramp,
+                            gradual
                         });
 
                     aspectSlopeAnalysis.changeColorTable(rampColor,'slope')
