9e187cd6efabd60b3fecc830b2204918ad6a8296
lvzhantian <2771997672@qq.com>
2022-06-14 10:20:10 +0800

    【stories】【示例】【优化剖切分析示例】

---
+++ stories/mapgis3d/CesiumAnalysisDynamicSection3DTiles.stories.js
@@ -0,0 +1,82 @@
+import "../style/card.css";
+import Mapgis3dTileset from "../../cesium/src/components/Layer/M3D/3dTileset.vue";
+import Markdown from "../../cesium/docs/api/analysis/DynamicSection.md";
+
+export default {
+  title: "三维/分析/剖切分析/3dTiles",
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Mapgis3dTileset },
+  data() {
+    return {
+      url: "http://t0.tianditu.gov.cn/img_c/wmts",
+      //地形url TODO这里地址打包的时候改一下
+      //terrainUrl: "http://192.168.21.191:6163/igs/rest/g3d/terrain",
+      terrainUrl: `http://${window.webclient.ip}:${window.webclient.port}/igs/rest/g3d/terrain`,
+      tileMatrixSet: "c",
+      tilingScheme: "EPSG:4326",
+      layer: "img",
+      format: "tiles",
+      token: {
+        key: "tk",
+        value: "9c157e9585486c02edf817d2ecbc7752",
+      },
+      m3dUrl1: `http://${window.webclient.ip}:${window.webclient.port}/igs/rest/g3d/钻孔_2_钻孔模型s`,
+      m3dUrl2: `http://${window.webclient.ip}:${window.webclient.port}/igs/rest/g3d/钻孔分层点_Sur_000_Ent`,
+      m3dUrl3: `http://192.168.88.204:8089/3DTileset/dayantaresult/tileset.json`
+    };
+  },
+    template: `
+      <mapgis-web-scene style="height: 95vh"
+          v-on:load="handleLoad"
+      >
+        <mapgis-3d-tileset 
+          :vueIndex="$props.models[0].vueIndex"
+          :url="m3dUrl3" 
+        />
+        <mapgis-ui-card class="storybook-ui-card">
+          <mapgis-3d-dynamic-section :models="models" :axis="axis" :color="color" :time="time" :distance="distance"/>
+        </mapgis-ui-card>
+      </mapgis-web-scene>
+    `,
+
+  methods: {
+    handleLoad(e) {
+      const { component, Cesium } = e;
+      Cesium.Ion.defaultAccessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiM2Q0ZGMxYy1iZGZkLTQ4OWItODlhMy1iOWNkMDE0M2U3YWEiLCJpZCI6NTEzNSwiaWF0IjoxNjA2MjE0OTkyfQ.2aktNrUASlLsPwSFtkgKBTQLJTAnOTyjgKDRQmnafiE";
+    },
+  },
+});
+
+export const DynamicSection3DTiles = Template.bind({});
+DynamicSection3DTiles.args = {
+  models: [
+    {
+      range: {
+        xmin: -10000,
+        xmax: 10000,
+        ymin: -10000,
+        ymax: 10000,
+        zmin: -10000,
+        zmax: 10000,
+      },
+      vueIndex: 1,
+      title: "大雁塔",
+    },
+  ],
+  axis: "X",
+  color: "rgb(200,200,200,0.5)",
+  time: 10,
+  distance: 0,
+};
+
+DynamicSection3DTiles.parameters = {
+  docs: {
+    description: {
+      component: Markdown,
+    },
+  },
+};
