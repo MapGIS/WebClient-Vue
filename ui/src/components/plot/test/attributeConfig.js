56cd87b4d0d6c4d2c93582c44e88e472dd19ae6f
liao-x-d <1627930520@qq.com>
2022-07-01 16:26:38 +0800

    【SDK】【标绘更改部分配置】

--- ui/src/components/plot/test/attributeConfig.js
+++ ui/src/components/plot/test/attributeConfig.js
@@ -4,111 +4,116 @@ export const styleAttributes = {
     type: "number", //number,text,color,select,boolean,nodes
     groupId: 0,
     range: [0, 100000],
-    disabled: true
+    disabled: true,
   },
   symbolName: {
     title: "符号名称",
     type: "text",
     groupId: 0,
-    disabled: true
+    disabled: true,
   },
   show: {
     title: "显示",
     groupId: 1,
-    type: "boolean"
+    type: "boolean",
   },
   featureId: {
     title: "图元ID",
     type: "text",
     groupId: 1,
-    disabled: true
+    disabled: true,
   },
   compareLine: {
     title: "开启衬线",
     type: "select",
     groupId: 3,
     options: {
-      0: '不开启衬线',
-      1: '开启衬线'
-    }
+      0: "不开启衬线",
+      1: "开启衬线",
+    },
   },
   compareLineWidth: {
     title: "衬线宽度",
     type: "number",
     groupId: 3,
-    range: [0,1000]
+    range: [0, 1000],
   },
   compareLineColor: {
     title: "衬线颜色",
     type: "color",
-    groupId: 3
+    groupId: 3,
   },
   isOpenWall: {
-    title:"开启墙体",
+    title: "开启墙体",
     type: "boolean",
-    groupId:2
+    groupId: 2,
   },
   dimModHeight: {
-    title:"墙体高度",
+    title: "墙体高度",
     type: "number",
-    groupId:2
+    groupId: 2,
   },
   dimModAttitude: {
-    title:"三维姿态",
+    title: "三维姿态",
     type: "select",
-    groupId:2,
+    groupId: 2,
     options: {
       0: "平躺",
-      1: "直立"
-    }
+      1: "直立",
+    },
   },
   wallColor: {
-    title:"墙体颜色",
+    title: "墙体颜色",
     type: "color",
-    groupId:2
+    groupId: 2,
   },
   nodeStyles: {
     // title:"符号部件",
-    type: "nodes",//符号部件
-    groupId: 4
+    type: "nodes", //符号部件
+    groupId: 4,
   },
   fillStyle: {
     title: "填充颜色",
     type: "color",
-    groupId: 6
+    groupId: 6,
   },
-  fillRule: {
-    title: "填充规则",
+  // fillRule: {
+  //   title: "填充规则",
+  //   type: "text",
+  //   groupId: 6
+  // },
+  text: {
+    title: "文字内容",
     type: "text",
-    groupId: 6
+    groupId: 7,
   },
   fontStyle: {
     title: "字体样式",
     type: "select",
     groupId: 7,
-    options: ["normal","italic"]
+    options: ["normal", "italic"],
   },
   fontVariant: {
     title: "字体变体",
     type: "select",
     groupId: 7,
-    options: ["normal","small-caps"]
+    options: ["normal", "small-caps"],
   },
   fontWeight: {
     title: "字体粗细",
     type: "select",
     groupId: 7,
-    options: ["normal","bold","bolder","lighter"]
+    options: ["normal", "bold", "bolder", "lighter"],
   },
   fontSize: {
     title: "字体大小",
     type: "number",
-    groupId: 7
+    groupId: 7,
   },
   fontFamily: {
     title: "字体",
     type: "text",
-    groupId: 7
+    groupId: 7,
   },
   strokeStyle: {
     title: "笔触类型",
@@ -118,13 +123,14 @@ export const styleAttributes = {
       none: "无",
       color: "纯色",
       gradient: "渐变",
-      pattern: "指定方向重复"
-    }
+      pattern: "指定方向重复",
+    },
   },
-  strokeColor: {//线区的strokeStyle
+  strokeColor: {
+    //线区的strokeStyle
     title: "笔画颜色",
     type: "color",
-    groupId: 5
+    groupId: 5,
   },
   lineCap: {
     title: "线端类型",
@@ -134,7 +140,7 @@ export const styleAttributes = {
       butt: "平直",
       round: "圆形",
       square: "正方形",
-    }
+    },
   },
   lineJoin: {
     title: "拐角类型",
@@ -144,40 +150,40 @@ export const styleAttributes = {
       bevel: "斜角",
       round: "圆角",
       miter: "尖角",
-    }
+    },
   },
   miterLimit: {
-    title:"最大斜接长度",
-    type: "number",
-    groupId: 5
-  },
-  lineWidth: {
-    title:"笔画宽",
+    title: "最大斜接长度",
     type: "number",
     groupId: 5,
-    range: [0,1000]
+  },
+  lineWidth: {
+    title: "笔画宽",
+    type: "number",
+    groupId: 5,
+    range: [0, 1000],
   },
 };
 
 export const groupArr = [
   {
     id: 0,
-    label: "符号信息"
+    label: "符号信息",
     // key:"symbol"
   },
   {
     id: 1,
-    label: "基本属性"
+    label: "基本属性",
     // key:"properties"
   },
   {
     id: 2,
-    label: "墙体"
+    label: "墙体",
     // key:"wallStyle"
   },
   {
     id: 3,
-    label: "衬线"
+    label: "衬线",
     // key: "compareLineStyle"
   },
   {
@@ -187,17 +193,17 @@ export const groupArr = [
   },
   {
     id: 5,
-    label: "笔画样式"
+    label: "笔画样式",
     // key: "lineStyle"
   },
   {
     id: 6,
-    label: "填充样式"
+    label: "填充样式",
     // key: "fillStyle"
   },
   {
     id: 7,
-    label: "文字样式"
+    label: "文字样式",
     // key: "textStyle"
-  }
+  },
 ];
