5896c4042e5e17bb911a24fd1a63558fb9645889
killer1992 <killer1992@yeah.net>
2022-07-08 10:07:49 +0800

    【SDK】【stories】【优化标绘-二三维联动】

--- stories/mapgis2d/MapboxPlotLink2DComponent.stories.js
+++ stories/mapgis2d/MapboxPlotLink2DComponent.stories.js
@@ -27,26 +27,43 @@ const Template = (args, { argTypes }) => ({
       symbolUrl: `http://${window.webclient.ip}:${window.webclient.port}/标绘/symbols.json`,
       vueIndex: 12345,
       containers:[{vueIndex:12345,vueKey: "default",type:"mapbox"},{vueIndex:12345,vueKey: "default",type:"cesium"}],
-      layers: [{vueIndex:23456,vueKey: "default"}]
+      layers: [{vueIndex:23456,vueKey: "default"}],
+      manager: undefined,
+      mapboxActive: false,
+      rectCount: 0
     };
   },
   watch: {
     rect: function (next) {
-      const { east, north, south, west } = next["2d"];
-      console.log(east, north, south, west);
-      let map = this.map || window.map;
-      let bbox = [
-        [west, south],
-        [east, north],
-      ];
-      map.fitBounds(bbox, {
-        duration: 0,
-      });
+      this.rectCount++;
+      if(this.rectCount > 1){
+        const { east, north, south, west } = next["2d"];
+        console.log(east, north, south, west);
+        let map = this.map || window.map;
+        let bbox = [
+          [west, south],
+          [east, north],
+        ];
+        map.fitBounds(bbox, {
+          duration: 0,
+        });
+      }
     },
   },
   mounted() {
   },
   methods: {
+    webGlobeLoaded() {
+      viewer.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(117.7646, 33.0881, 210000),
+        orientation: {
+          heading: Cesium.Math.toRadians(0),
+          pitch: Cesium.Math.toRadians(-45),
+          roll: 0,
+        },
+        duration: 1
+      })
+    },
     handleLoaded(e) {
       this.vueIndex1 = e.vueIndex;
       this.vueKey1 = e.vueKey;
@@ -54,23 +71,64 @@ const Template = (args, { argTypes }) => ({
     changeMode() {
       this.link = !this.link;
     },
+    flyToRect(bounds) {
+      viewer.camera.flyTo({
+        destination: Cesium.Rectangle.fromDegrees(bounds._sw.lng,bounds._sw.lat,bounds._ne.lng,bounds._ne.lat),
+        orientation: {
+          heading: Cesium.Math.toRadians(0),
+          pitch: Cesium.Math.toRadians(-90),
+          roll: 0,
+        },
+        duration: 0.1
+      })
+    },
     handle2dLoad(e) {
+      let vm = this;
       window.map = this.map = e.map;
+      let bbox = [
+        [116.2396164394222, 36.857699114405676],
+        [119.38483688908019,33.044482287500756],
+      ];
+      this.map.fitBounds(bbox, {
+        duration: 0,
+      });
+      this.map.on('wheel', function (e) {
+        let bounds = vm.map.getBounds();
+        vm.flyToRect(bounds);
+      });
+      this.map.on('wheel', function (e) {
+        let bounds = vm.map.getBounds();
+        vm.flyToRect(bounds);
+      });
+      this.map.on('mousedown', function (e) {
+        vm.mapboxActive = true;
+      });
+      this.map.on('mouseup', function (e) {
+        vm.mapboxActive = false;
+      });
+      let mapboxManager = window.vueMap.MapManager.findSource("default", this.vueIndex);
+      let fabricCanvas = mapboxManager.options.canvas.getFabricCanvas();
+      fabricCanvas.on("mouse:move",function () {
+        if(vm.mapboxActive){
+          let bounds = vm.map.getBounds();
+          vm.flyToRect(bounds);
+        }
+      });
     }
   },
   template: `<div class="mapgis-link-test">
     <div class="mapbox-item top-left">
       <mapgis-web-map crs="EPSG:3857"  @load="handle2dLoad" :vueIndex="vueIndex">
-        <mapgis-2d-plot-layer :vueIndex="23456" :symbolUrl="symbolUrl" @loaded="handleLoaded" :dataSource="jsonUrl"></mapgis-2d-plot-layer>
-        <mapgis-2d-plot :symbolUrl="symbolUrl" :vueIndex="vueIndex1" :vueKey="vueKey1" v-if="vueIndex1 && vueKey1" class="storybook-ui-card"/>
+        <mapgis-2d-plot-layer :vueIndex="23456" :symbolUrl="symbolUrl" @loaded="handleLoaded" :dataSource="jsonUrl" v-if="manager"></mapgis-2d-plot-layer>
+        <mapgis-2d-plot :symbolUrl="symbolUrl" :vueIndex="vueIndex1" :vueKey="vueKey1" @loaded="manager=true" class="storybook-ui-card"/>
         <mapgis-2D-plot-link :layers="layers" :containers="containers"></mapgis-2D-plot-link>
         <mapgis-rastertile-layer :url="url2" layerId="raster_tdt" />
       </mapgis-web-map>
     </div>
     <div class="cesium-item top-right">
-      <mapgis-web-scene :vueIndex="vueIndex">
+      <mapgis-web-scene @load="webGlobeLoaded" :vueIndex="vueIndex">
         <mapgis-3d-raster-layer :url="url1"> </mapgis-3d-raster-layer>
-        <mapgis-3d-link :enable="link" v-model="rect" ></mapgis-3d-link>
+        <mapgis-3d-link :timestamp="100" :enable="link" v-model="rect" ></mapgis-3d-link>
       </mapgis-web-scene>
     </div>
     <div :class="{'control-2d-3d': true, 'link-active': link}" v-on:click="changeMode"></div>
--- stories/mapgis3d/CesiumPlotLinkComponent.stories.js
+++ stories/mapgis3d/CesiumPlotLinkComponent.stories.js
@@ -1,10 +1,12 @@
 import Mapgis3dLink from "../../cesium/src/components/UI/Controls/Link/Link.vue";
+import MapgisStateControl from "../../mapboxgl/src/components/UI/controls/state/StateControl.vue";
 import "../style/link.css";
+import "../style/state.css";
 import {SymbolManager,PlotLayer3D,PlotLayer3DGroup,PlotLayer2D,PlotLayer2DGroup,DrawTool,LinkTool} from "@mapgis/webclient-es6-service";
 import { FabricLayer } from "@mapgis/webclient-es6-mapboxgl";
 
 export default {
-  title: "三维/场景子组件/二三维联动/标绘(组件)",
+  title: "三维/场景子组件/二三维联动/标绘",
   component: Mapgis3dLink,
   argTypes: {
     timestamp: 100,
@@ -28,21 +30,26 @@ const Template = (args, { argTypes }) => ({
       vueIndex: 12345,
       containers:[{vueIndex:12345,vueKey: "default",type:"cesium"},{vueIndex:12345,vueKey: "default",type:"mapbox"}],
       layers: [{vueIndex:23456,vueKey: "default"}],
-      manager: undefined
+      manager: undefined,
+      mapboxActive: false,
+      rectCount: 0
     };
   },
   watch: {
     rect: function (next) {
-      const { east, north, south, west } = next["2d"];
-      console.log(east, north, south, west);
-      let map = this.map || window.map;
-      let bbox = [
-        [west, south],
-        [east, north],
-      ];
-      map.fitBounds(bbox, {
-        duration: 0,
-      });
+      this.rectCount++;
+      if(this.rectCount > 1){
+        const { east, north, south, west } = next["2d"];
+        console.log(east, north, south, west);
+        let map = this.map || window.map;
+        let bbox = [
+          [west, south],
+          [east, north],
+        ];
+        map.fitBounds(bbox, {
+          duration: 0,
+        });
+      }
     },
   },
   mounted() {
@@ -64,14 +71,48 @@ const Template = (args, { argTypes }) => ({
     changeMode() {
       this.link = !this.link;
     },
+    flyToRect(bounds) {
+      viewer.camera.flyTo({
+        destination: Cesium.Rectangle.fromDegrees(bounds._sw.lng,bounds._sw.lat,bounds._ne.lng,bounds._ne.lat),
+        orientation: {
+          heading: Cesium.Math.toRadians(0),
+          pitch: Cesium.Math.toRadians(-90),
+          roll: 0,
+        },
+        duration: 0.1
+      })
+    },
     handle2dLoad(e) {
+      let vm = this;
       window.map = this.map = e.map;
       let bbox = [
-        [116.1652946582577, 32.45693126275157],
-        [117.60550985236218,34.646652601869185],
+        [116.2396164394222, 36.857699114405676],
+        [119.38483688908019,33.044482287500756],
       ];
       this.map.fitBounds(bbox, {
         duration: 0,
+      });
+      this.map.on('wheel', function (e) {
+        let bounds = vm.map.getBounds();
+        vm.flyToRect(bounds);
+      });
+      this.map.on('wheel', function (e) {
+        let bounds = vm.map.getBounds();
+        vm.flyToRect(bounds);
+      });
+      this.map.on('mousedown', function (e) {
+        vm.mapboxActive = true;
+      });
+      this.map.on('mouseup', function (e) {
+        vm.mapboxActive = false;
+      });
+      let mapboxManager = window.vueMap.MapManager.findSource("default", this.vueIndex);
+      let fabricCanvas = mapboxManager.options.canvas.getFabricCanvas();
+      fabricCanvas.on("mouse:move",function () {
+        if(vm.mapboxActive){
+          let bounds = vm.map.getBounds();
+          vm.flyToRect(bounds);
+        }
       });
     }
   },
@@ -86,9 +127,8 @@ const Template = (args, { argTypes }) => ({
         <mapgis-3d-plot-layer :vueIndex="23456" :symbolUrl="symbolUrl" @loaded="handleLoaded" :dataSource="jsonUrl" v-if="manager"></mapgis-3d-plot-layer>
         <mapgis-3d-plot :symbolUrl="symbolUrl" :vueIndex="vueIndex1" :vueKey="vueKey1" @loaded="manager=true" class="storybook-ui-card"/>
         <mapgis-3d-raster-layer :url="url1"> </mapgis-3d-raster-layer>
-        <mapgis-3d-link :enable="link" v-model="rect" ></mapgis-3d-link>
+        <mapgis-3d-link :timestamp="100" :enable="link" v-model="rect" ></mapgis-3d-link>
         <mapgis-3D-plot-link :layers="layers" :containers="containers"></mapgis-3D-plot-link>
-        <mapgis-3d-statebar/>
       </mapgis-web-scene>
     </div>
     <div :class="{'control-2d-3d': true, 'link-active': link}" v-on:click="changeMode"></div>
