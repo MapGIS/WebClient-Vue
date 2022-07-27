16e7fb782376ef57085d10a1284e2a4b62a68a60
gongyuejian <gongyuejian.000@mapgis.com>
2022-07-27 10:07:05 +0800

    【SDK】【Cesium】【场景设置支持初始化设置和保存配置】

--- cesium/src/components/SceneEffect/components/EffectSetting.vue
+++ cesium/src/components/SceneEffect/components/EffectSetting.vue
@@ -1,182 +1,218 @@
 <template>
-    <div class="effect-setting">
-        <mapgis-ui-row>
-            <mapgis-ui-col :span="12">
-                <mapgis-ui-switch-panel
-                    size="small"
-                    label="黑白照片"
-                    :checked="blckWhite"
-                    @changeChecked="blackAndWhiteChange"
-                />
-            </mapgis-ui-col>
-            <mapgis-ui-col :span="12">
-                <mapgis-ui-switch-panel
-                    size="small"
-                    label="夜视效果"
-                    :checked="ntVision"
-                    @changeChecked="nightVision"
-                />
-            </mapgis-ui-col>
-        </mapgis-ui-row>
-
-        <!-- <div class="dividerWrapper"><div class="divider" /></div> -->
-
+  <div class="effect-setting">
+    <mapgis-ui-row>
+      <mapgis-ui-col :span="12">
         <mapgis-ui-switch-panel
-            size="default"
-            label="场景泛光"
-            :checked="bloom"
-            @changeChecked="enableBloom"
-        >
-            <mapgis-ui-input-number-panel
-                size="large"
-                label="亮度"
-                :value="bloomBrt"
-                :range="bloomBrtRange"
-                :step="0.05"
-                @change="bloomBrtChange"
-            />
-            
-            <mapgis-ui-input-number-panel
-                size="large"
-                label="对比度"
-                :value="bloomCtrst"
-                :range="bloomCtrstRange"
-                :step="10"
-                @change="bloomCtrstChange"
-            />
-        </mapgis-ui-switch-panel>
-    </div>
+          size="small"
+          label="黑白照片"
+          :checked="effectSetting.blckWhite"
+          @changeChecked="blackAndWhiteChange"
+        />
+      </mapgis-ui-col>
+      <mapgis-ui-col :span="12">
+        <mapgis-ui-switch-panel
+          size="small"
+          label="夜视效果"
+          :checked="effectSetting.ntVision"
+          @changeChecked="nightVision"
+        />
+      </mapgis-ui-col>
+    </mapgis-ui-row>
+
+    <!-- <div class="dividerWrapper"><div class="divider" /></div> -->
+
+    <mapgis-ui-switch-panel
+      size="default"
+      label="场景泛光"
+      :checked="effectSetting.bloom"
+      @changeChecked="enableBloom"
+    >
+      <mapgis-ui-input-number-panel
+        size="large"
+        label="亮度"
+        :value="effectSetting.bloomParams.bloomBrt"
+        :range="bloomBrtRange"
+        :step="0.05"
+        @change="bloomBrtChange"
+      />
+
+      <mapgis-ui-input-number-panel
+        size="large"
+        label="对比度"
+        :value="effectSetting.bloomParams.bloomCtrst"
+        :range="bloomCtrstRange"
+        :step="10"
+        @change="bloomCtrstChange"
+      />
+    </mapgis-ui-switch-panel>
+  </div>
 </template>
 
 <script>
 import ServiceLayer from "../../UI/Controls/ServiceLayer";
 export default {
-    name: "EffectSetting",
-    mixins: [ServiceLayer],
-    data() {
+  name: "EffectSetting",
+  mixins: [ServiceLayer],
+  props: {
+    initEffectSetting: {
+      type: Object,
+      default: () => {
         return {
-            bloom: false,
-            bloomParams: undefined,
+          blckWhite: false,
+          ntVision: false,
+          bloom: false,
+          bloomParams: {
             bloomBrt: -0.3,
-            bloomBrtRange: [-0.6, 0.2],
-            bloomCtrst: 128,
-            bloomCtrstRange: [-255, 255],
-
-            blckWhite: false,
-            ntVision: false,
+            bloomCtrst: 128
+          }
         };
+      }
+    }
+  },
+  computed: {
+    effectSetting: {
+      get() {
+        return this.initEffectSetting;
+      },
+      set() {
+        this.$emit("updateEffectSetting", this.effectSetting);
+      }
+    }
+  },
+  data() {
+    return {
+      bloomBrtRange: [-0.6, 0.2],
+      bloomCtrstRange: [-255, 255]
+    };
+  },
+  watch: {
+    initEffectSetting(e) {
+      this.init();
+    }
+  },
+  methods: {
+    init() {
+      if (!this.effectSetting) {
+        return;
+      }
+      const { blckWhite, ntVision, bloom, bloomParams } = this.effectSetting;
+      const { bloomBrt, bloomCtrst } = bloomParams;
+      this.enableBloom(bloom);
+      if (bloom) {
+        this.bloomBrtRange(bloomBrt);
+        this.bloomCtrstRange(bloomCtrst);
+      }
+      this.blackAndWhiteChange(blckWhite);
+      this.nightVision(ntVision);
     },
-    methods: {
-        /*
-         * 场景泛光
-         * */
-        enableBloom(e) {
-            const { viewer } = this;
-            this.bloom = e;
-            this.$emit("updateSpin", true);
-            let vm = this;
+    /*
+     * 场景泛光
+     * */
+    enableBloom(e) {
+      const { viewer } = this;
+      this.effectSetting.bloom = e;
+      this.$emit("updateSpin", true);
+      let vm = this;
 
-            setTimeout(function () {
-                viewer.scene.postProcessStages.bloom.enabled = vm.bloom;
-                vm.$emit("updateSpin", false);
-            }, 400);
-        },
-        /*
-         * 泛光亮度
-         * */
-        bloomBrtChange(e) {
-            const { viewer } = this;
-            this.bloomBrt = e;
-            viewer.scene.postProcessStages.bloom.uniforms.brightness =
-                this.bloomBrt;
-        },
-        /*
-         * 泛光对比度
-         * */
-        bloomCtrstChange(e) {
-            const { viewer } = this;
-            this.bloomCtrst = e;
-            viewer.scene.postProcessStages.bloom.uniforms.contrast =
-                this.bloomCtrst;
-        },
-
-        //黑白照片
-        blackAndWhiteChange(e) {
-            const { viewer, Cesium } = this;
-            this.blckWhite = e;
-            let vm = this;
-            if (vm.blckWhite) {
-                vm.removeOtherStages();
-
-                let blackAndWhite = viewer.scene.postProcessStages.add(
-                    Cesium.PostProcessStageLibrary.createBlackAndWhiteStage()
-                );
-                blackAndWhite.uniforms.gradations = 5.0; //(灰度级数)
-            } else {
-                vm.removeOtherStages();
-            }
-        },
-        //夜视效果
-        nightVision(e) {
-            const { viewer, Cesium } = this;
-            this.ntVision = e;
-            let vm = this;
-            if (vm.ntVision) {
-                vm.removeOtherStages();
-                viewer.scene.postProcessStages.add(
-                    Cesium.PostProcessStageLibrary.createNightVisionStage()
-                );
-            } else {
-                vm.removeOtherStages();
-            }
-        },
-        removeOtherStages() {
-            const { viewer } = this;
-            let length = viewer.scene.postProcessStages._activeStages.length;
-
-            if (length > 0) {
-                viewer.scene.postProcessStages.removeAll();
-            }
-        },
+      setTimeout(function() {
+        viewer.scene.postProcessStages.bloom.enabled = vm.effectSetting.bloom;
+        vm.$emit("updateSpin", false);
+      }, 400);
     },
+    /*
+     * 泛光亮度
+     * */
+    bloomBrtChange(e) {
+      const { viewer } = this;
+      this.effectSetting.bloomParams.bloomBrt = e;
+      viewer.scene.postProcessStages.bloom.uniforms.brightness = this.effectSetting.bloomParams.bloomBrt;
+    },
+    /*
+     * 泛光对比度
+     * */
+    bloomCtrstChange(e) {
+      const { viewer } = this;
+      this.effectSetting.bloomParams.bloomCtrst = e;
+      viewer.scene.postProcessStages.bloom.uniforms.contrast = this.effectSetting.bloomParams.bloomCtrst;
+    },
+
+    //黑白照片
+    blackAndWhiteChange(e) {
+      const { viewer, Cesium } = this;
+      this.effectSetting.blckWhite = e;
+      let vm = this;
+      if (vm.effectSetting.blckWhite) {
+        vm.removeOtherStages();
+
+        let blackAndWhite = viewer.scene.postProcessStages.add(
+          Cesium.PostProcessStageLibrary.createBlackAndWhiteStage()
+        );
+        blackAndWhite.uniforms.gradations = 5.0; //(灰度级数)
+      } else {
+        vm.removeOtherStages();
+      }
+    },
+    //夜视效果
+    nightVision(e) {
+      const { viewer, Cesium } = this;
+      this.effectSetting.ntVision = e;
+      let vm = this;
+      if (vm.effectSetting.ntVision) {
+        vm.removeOtherStages();
+        viewer.scene.postProcessStages.add(
+          Cesium.PostProcessStageLibrary.createNightVisionStage()
+        );
+      } else {
+        vm.removeOtherStages();
+      }
+    },
+    removeOtherStages() {
+      const { viewer } = this;
+      let length = viewer.scene.postProcessStages._activeStages.length;
+
+      if (length > 0) {
+        viewer.scene.postProcessStages.removeAll();
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
 ::v-deep .mapgis-ui-row .mapgis-ui-col:nth-child(odd) .mapgis-ui-switch-panel {
-    padding-right: 10px;
+  padding-right: 10px;
 }
 
 ::v-deep
-    .mapgis-ui-row
-    .mapgis-ui-col:nth-child(even)
-    .mapgis-ui-switch-panel::before {
-    content: "";
-    display: block;
-    width: 1px;
-    height: 14px;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    margin: auto;
-    left: 0;
-    background: #dcdcdc;
+  .mapgis-ui-row
+  .mapgis-ui-col:nth-child(even)
+  .mapgis-ui-switch-panel::before {
+  content: "";
+  display: block;
+  width: 1px;
+  height: 14px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: 0;
+  background: #dcdcdc;
 }
 
 ::v-deep .mapgis-ui-row .mapgis-ui-col:nth-child(even) .mapgis-ui-switch-panel {
-    padding-left: 10px;
+  padding-left: 10px;
 }
 
 .dividerWrapper {
-    height: 13px;
+  height: 13px;
 }
 .divider {
-    display: block;
-    height: 1px;
-    position: absolute;
-    left: 16px;
-    right: 16px;
-    margin: 6px 0;
-    background: #f0f0f0;
+  display: block;
+  height: 1px;
+  position: absolute;
+  left: 16px;
+  right: 16px;
+  margin: 6px 0;
+  background: #f0f0f0;
 }
 </style>
