38c76594f93bab2b00516bdd9ca3a8e4f9b44718
gongyuejian <gongyuejian.000@mapgis.com>
2022-07-27 10:07:05 +0800

    【SDK】【Cesium】【场景设置支持初始化设置和保存配置】

--- cesium/src/components/SceneEffect/components/CameraSetting.vue
+++ cesium/src/components/SceneEffect/components/CameraSetting.vue
@@ -1,103 +1,144 @@
 <template>
-    <div class="camera-setting">
-        <mapgis-ui-switch-panel
-            size="default"
-            label="地下模式"
-            :checked="undgrd"
-            @changeChecked="enableUndgrd"
-        >
-            <mapgis-ui-input-number-panel
-                size="large"
-                label="地表透明度"
-                :value="groundAlpha"
-                :range="range"
-                :step="0.1"
-                @change="enableUndgrd"
-            />
-        </mapgis-ui-switch-panel>
-        
-        <!-- <div class="dividerWrapper"><div class="divider" /></div> -->
+  <div class="camera-setting">
+    <mapgis-ui-switch-panel
+      size="default"
+      label="地下模式"
+      :checked="cameraSetting.undgrd"
+      @changeChecked="enableUndgrd"
+    >
+      <mapgis-ui-input-number-panel
+        size="large"
+        label="地表透明度"
+        :value="cameraSetting.undgrdParams.groundAlpha"
+        :range="range"
+        :step="0.1"
+        @change="enableUndgrd"
+      />
+    </mapgis-ui-switch-panel>
 
-        <mapgis-ui-input-number-panel
-            size="large"
-            label="FOV设置"
-            :value="fov"
-            :range="fovRange"
-            :step="15"
-            @change="fovChange"
-        />
-    </div>
+    <!-- <div class="dividerWrapper"><div class="divider" /></div> -->
+
+    <mapgis-ui-input-number-panel
+      size="large"
+      label="FOV设置"
+      :value="cameraSetting.fov"
+      :range="fovRange"
+      :step="15"
+      @change="fovChange"
+    />
+  </div>
 </template>
 
 <script>
 import ServiceLayer from "../../UI/Controls/ServiceLayer";
 
 export default {
-    name: "CameraSetting",
-    mixins: [ServiceLayer],
-    data() {
+  name: "CameraSetting",
+  mixins: [ServiceLayer],
+  props: {
+    initCameraSetting: {
+      type: Object,
+      default: () => {
         return {
-            undgrd: false,
-            undgrdParams: undefined,
-            groundAlpha: 0.5,
-            range: [0, 1],
-
-            fov: 60.0,
-            fovRange: [0, 180],
+          undgrd: false,
+          undgrdParams: {
+            groundAlpha: 0.5
+          },
+          fov: 60
         };
+      }
+    }
+  },
+  computed: {
+    cameraSetting: {
+      get() {
+        return this.initCameraSetting;
+      },
+      set() {
+        this.$emit("updateCameraSetting", this.cameraSetting);
+      }
+    }
+  },
+  data() {
+    return {
+      range: [0, 1],
+      fovRange: [0, 180]
+    };
+  },
+  watch: {
+    initCameraSetting(e) {
+      this.init();
+    }
+  },
+  methods: {
+    init() {
+      if (!this.cameraSetting) {
+        return;
+      }
+      const { undgrd, fov } = this.cameraSetting;
+      this.enableUndgrd(undgrd);
+      this.fovChange(fov);
     },
-    methods: {
-        /*
-         * 地下模式
-         * */
-        enableUndgrd(e) {
-            const { viewer } = this;
-            if (typeof e === "boolean") {
-                this.undgrd = e;
-            } else {
-                this.groundAlpha = e;
-            }
-            this.$emit("updateSpin", true);
-            let vm = this;
+    /*
+     * 地下模式
+     * */
+    enableUndgrd(e) {
+      const { viewer, Cesium } = this;
+      if (typeof e === "boolean") {
+        this.cameraSetting.undgrd = e;
+      } else {
+        this.cameraSetting.undgrdParams.groundAlpha = e;
+      }
+      this.$emit("updateSpin", true);
+      let vm = this;
 
-            setTimeout(function () {
-                viewer.scene.screenSpaceCameraController.enableCollisionDetection =
-                    !vm.undgrd;
-                viewer.scene.globe.translucency.enabled = vm.undgrd;
-                //设置地表透明度
-                viewer.scene.globe.translucency.backFaceAlpha = vm.groundAlpha;
-                viewer.scene.globe.translucency.frontFaceAlpha = vm.groundAlpha;
+      setTimeout(function() {
+        viewer.scene.screenSpaceCameraController.enableCollisionDetection = !vm
+          .cameraSetting.undgrd;
+        viewer.scene.globe.translucency.enabled = vm.cameraSetting.undgrd;
+        const { groundAlpha } = vm.cameraSetting.undgrdParams;
+        //设置地表透明度
+        viewer.scene.globe.translucency.backFaceAlpha = groundAlpha;
+        // 下面这一句会给所有二维图层都设置透明度
+        // viewer.scene.globe.translucency.frontFaceAlpha = groundAlpha;
+        // 只把Cesium自带的天地图底图设置透明，其他图层透明度保持不变
+        const defaultColor = Cesium.Color.clone(viewer.scene.globe.baseColor);
+        defaultColor.alpha = groundAlpha;
+        viewer.scene.globe.baseColor = defaultColor;
+        viewer.imageryLayers._layers[0].alpha = groundAlpha;
 
-                //最小缩放距离
-                //viewer.scene.screenSpaceCameraController.minimumZoomDistance = ;
+        //最小缩放距离
+        //viewer.scene.screenSpaceCameraController.minimumZoomDistance = ;
 
-                vm.$emit("updateSpin", false);
-            }, 300);
-        },
-        /*
-         * FOV设置
-         * */
-        fovChange(e) {
-            const { viewer, Cesium } = this;
-            this.fov = e;
-            //   console.log('default fov',Cesium.Math.toDegrees(1.0471975511965976));
-            viewer.scene.camera.frustum.fov = Cesium.Math.toRadians(this.fov);
-        },
+        vm.$emit("updateSpin", false);
+      }, 300);
     },
+    /*
+     * FOV设置
+     * */
+    fovChange(e) {
+      const { viewer, Cesium } = this;
+      this.cameraSetting.fov = e;
+      //   console.log('default fov',Cesium.Math.toDegrees(1.0471975511965976));
+      viewer.scene.camera.frustum.fov = Cesium.Math.toRadians(
+        this.cameraSetting.fov
+      );
+    }
+  }
 };
 </script>
 
 <style scoped>
 .dividerWrapper {
-    height: 13px;
+  height: 13px;
 }
 .divider {
-    display: block;
-    height: 1px;
-    position: absolute;
-    left: 16px;
-    right: 16px;
-    margin: 6px 0;
-    background: #f0f0f0;
+  display: block;
+  height: 1px;
+  position: absolute;
+  left: 16px;
+  right: 16px;
+  margin: 6px 0;
+  background: #f0f0f0;
 }
 </style>
