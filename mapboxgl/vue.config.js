e97279ffd8f86591b3f6580b43093b7132b3b469
mayuanye <mayuanye2008@163.com>
2022-10-21 13:48:06 +0800

    feat(1133): update version

--- mapboxgl/vue.config.js
+++ mapboxgl/vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
   productionSourceMap: false,
   outputDir: "dist-libs",
   pluginOptions: {
@@ -8,24 +17,37 @@ module.exports = {
   },
   configureWebpack: {
     externals: {
-      "mapbox-gl": {
-        commonjs: "mapbox-gl",
-        commonjs2: "mapbox-gl",
-        amd: "mapbox-gl",
-        root: "mapbox-gl"
-      },
-      vue: {
-        commonjs: "vue",
-        commonjs2: "vue",
-        amd: "vue",
-        root: "vue"
-      },
-      "map-promisified": {
-        commonjs: "map-promisified",
-        commonjs2: "map-promisified",
-        amd: "map-promisified",
-        root: "map-promisified"
-      }
+      "vue": "vue",
+      "@mapbox/mapbox-gl-draw": "@mapbox/mapbox-gl-draw",
+      "@mapbox/mapbox-gl-draw-static-mode": "@mapbox/mapbox-gl-draw-static-mode",
+      "@mapgis/mapbox-gl": "@mapgis/mapbox-gl",
+      "@mapgis/mapbox-gl-compare": "@mapgis/mapbox-gl-compare",
+      "@mapgis/mapbox-gl-draw-circle": "@mapgis/mapbox-gl-draw-circle",
+      "@mapgis/mapbox-gl-draw-radius": "@mapgis/mapbox-gl-draw-radius",
+      "@mapgis/mapbox-gl-inspect": "@mapgis/mapbox-gl-inspect",
+      "@mapgis/webclient-es6-service": "@mapgis/webclient-es6-service",
+      "@mapgis/webclient-es6-mapboxgl": "@mapgis/webclient-es6-mapboxgl",
+      "@mapgis/webclient-store": "@mapgis/webclient-store",
+      "@mapgis/webclient-vue-ui": "@mapgis/webclient-vue-ui",
+      "@mapgis/webclient-plot": "@mapgis/webclient-plot",
+      "@turf/turf": "@turf/turf",
+      "echarts": "echarts",
+      "file-saver": "file-saver",
+      "html2canvas": "html2canvas",
+      "map-promisified": "map-promisified",
+      "mapbox-gl": "mapbox-gl",
+      "mapbox-gl-draw-circle": "mapbox-gl-draw-circle",
+      "mapbox-gl-draw-rectangle-mode": "mapbox-gl-draw-rectangle-mode",
+      "mapv": "mapv",
+      "moment": "moment",
+      "node-sql-parser": "node-sql-parser",
+      "postcss-inline-svg": "postcss-inline-svg",
+      "resize-detector": "resize-detector",
+      "v-contextmenu": "v-contextmenu",
+      "v-jsoneditor": "v-jsoneditor",
+      "vcolorpicker": "vcolorpicker",
+      "vue-runtime-helpers": "vue-runtime-helpers",
+      "vuepress": "vuepress"
     }
   }
 };
