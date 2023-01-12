7c65dc7c1df38335fba9053cc01f40ffca60adce
yangkun <yangkun@mapgis.com>
2023-01-12 15:00:32 +0800

    fix(2810): 路径漫游中勾选/取消勾选“显示路径”，会直接报错不友好，建议复选框置灰处理 问题原因: 代码问题 修改说明: 优化代码

--- cesium/src/components/SceneEffect/PathRoaming/PathRoaming.vue
+++ cesium/src/components/SceneEffect/PathRoaming/PathRoaming.vue
@@ -314,7 +314,12 @@ export default {
         this.$emit("remove-road");
         // 设置播放动画的各项属性
         if (this.positions.length > 0) {
-          const positions = JSON.parse(JSON.stringify(this.positions));
+          let positions;
+          if(this.positions[0] instanceof Object) {
+            positions = JSON.parse(JSON.stringify(this.positions)).flatMap((t)=> [t.x,t.y,t.z]).flat();
+          }else {
+            positions = JSON.parse(JSON.stringify(this.positions));
+          }
           window.SceneWanderManager.animation.positions = this.Cesium.Cartesian3.fromDegreesArrayHeights(
            positions
           );
