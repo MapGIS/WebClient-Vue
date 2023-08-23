263e03768703bb32e80abcc9b3c2c5ef7f884efb
zhouzhenxiong <zhouzhenxiong.zzx@mapgis.com>
2023-08-23 11:25:01 +0800

    fix:三维图层自定义气泡框逻辑提取到mixins中 问题原因: 修改说明: 接口变动:无 接口描述:无 同步版本:dev 统查统改:

--- cesium/src/components/Layer/M3D/M3d.vue
+++ cesium/src/components/Layer/M3D/M3d.vue
@@ -27,15 +27,17 @@
     v-else-if="popupShowType === 'default' && featureposition"
     :position="featureposition"
     :popupOptions="popupOptions"
-    :componentWidth="260"
+    :componentWidth="popupWidth"
+    v-bind="popupConfig"
   >
-    <mapgis-3d-popup-iot
+    <component
+      :is="popupComponent"
       :properties="featureproperties"
       :dataStoreIp="dataStoreIp"
       :dataStorePort="dataStorePort"
       :dataStoreDataset="dataStoreDataset"
-    >
-    </mapgis-3d-popup-iot>
+      v-bind="popupConfig"
+    />
   </mapgis-3d-feature-popup>
 </template>
 
