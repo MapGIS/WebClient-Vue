143e3af9b5564980361d94c5765853cea5f04b28
gongyuejian <gongyuejian.000@mapgis.com>
2024-09-03 17:28:36 +0800

    fix(6170):PTSYB-数据目录的服务类型下拉列表与底图管理中的服务类型下拉列表不一致

--- cesium/src/components/Layer/ServiceLayer.js
+++ cesium/src/components/Layer/ServiceLayer.js
@@ -27,7 +27,7 @@ export default {
     },
     layerStyle: {
       type: Object,
-      default: function () {
+      default: function() {
         return {
           visible: true,
           opacity: 1
@@ -91,7 +91,7 @@ export default {
   },
   watch: {
     layerStyle: {
-      handler: function () {
+      handler: function() {
         let { vueKey, vueIndex } = this;
         let layer = window.vueCesium[this.managerName].findSource(
           vueKey,
@@ -114,7 +114,7 @@ export default {
       deep: true
     },
     options: {
-      handler: function () {
+      handler: function() {
         let vm = this;
         let isEqual = this.$_isEqual(vm.options, vm.optionsBack);
         if (!isEqual) {
@@ -126,7 +126,7 @@ export default {
       deep: true
     },
     id: {
-      handler: function () {
+      handler: function() {
         const { vueIndex, vueKey } = this;
         let layer = window.vueCesium[this.managerName].findSource(
           vueKey,
@@ -180,7 +180,7 @@ export default {
 
       //取得除options、layerStyle和id之外的必要参数
       const { $props, vueIndex, vueKey } = this;
-      Object.keys($props).forEach(function (key) {
+      Object.keys($props).forEach(function(key) {
         if (key !== "options" && key !== "layerStyle" && key !== "id") {
           opt[key] = $props[key];
         }
@@ -371,7 +371,7 @@ export default {
         vm = this;
 
       //遍历window.vueCesium下所有的Manager
-      Object.keys(window.vueCesium).forEach(function (key) {
+      Object.keys(window.vueCesium).forEach(function(key) {
         if (key.indexOf("Manager") > -1 && key !== "GlobesManager") {
           //取出含有与webScene组件相同vueKey的Manager对象
           if (window.vueCesium[key].hasOwnProperty("vueKey")) {
@@ -395,7 +395,7 @@ export default {
       });
 
       //对数组进行排序
-      Layers.sort(function (a, b) {
+      Layers.sort(function(a, b) {
         if (a.options && b.options) {
           return a.options.zIndex - b.options.zIndex;
         }
@@ -637,7 +637,7 @@ export default {
     $_checkProps(checkObj, checkType) {
       let vm = this;
       if (checkObj && checkType) {
-        Object.keys(checkObj).forEach(function (key) {
+        Object.keys(checkObj).forEach(function(key) {
           let result;
           if (checkType.hasOwnProperty(key) && typeof key === "string") {
             result = vm.$_checkValue(checkObj, key, checkType[key]);
@@ -712,7 +712,7 @@ export default {
         let customWKID;
         let axisDirection = {
           x: 1,
-          y: 1
+          y: -1
         };
         let rectangleSouthwest;
         let rectangleNortheast;
@@ -720,7 +720,7 @@ export default {
           customWKID = 20020902;
           axisDirection = {
             x: 1,
-            y: -1
+            y: 1
           };
           rectangleSouthwest = new Cesium.Cartesian2(
             -20037726.37,
@@ -756,10 +756,12 @@ export default {
      * **/
     $_getTileInfoByWKID(wkid) {
       let tileInfo = {};
+      let tileSize;
+      let lods = [];
+      let resolution0;
       if (wkid === 20020902) {
         // 构建自定义Wkid 百度09墨卡托的默认TileInfo
-        const lods = [];
-        const tileSize = [256, 256];
+        tileSize = [256, 256];
         for (let i = 0; i < 19; i++) {
           const resolution = Math.pow(2, 18 - i);
           lods[i] = { level: i, resolution: resolution, scale: null };
@@ -776,11 +778,10 @@ export default {
         };
       } else if (wkid === 20010202) {
         // 构建自定义Wkid 国测局02墨卡托的默认TileInfo
-        const lods = [];
-        const tileSize = [256, 256];
+        tileSize = [256, 256];
         const maxLength = Cesium.Ellipsoid.WGS84.maximumRadius * Math.PI;
         // const maxLength = 20037508.3427892;
-        const resolution0 = (maxLength + maxLength) / tileSize[0];
+        resolution0 = (maxLength + maxLength) / tileSize[0];
         // 总共20级
         for (let i = 0; i < 19; i++) {
           const resolution = resolution0 / Math.pow(2, i);
@@ -793,6 +794,40 @@ export default {
           origin: {
             coordinates: [-maxLength, maxLength], // 裁图原点
             type: "Point" // 裁图原点类型
+          },
+          lods: lods
+        };
+      } else if (wkid === 20020901 || wkid === 20010201) {
+        const extent = {
+          xmin: -180,
+          ymin: -90,
+          xmax: 180,
+          ymax: 90
+        };
+        const numberOfMinLevelTilesX = 2;
+        tileSize = 256;
+        const mapUnitToMeters = 111319490.79327358;
+        // 最大(第0级)分辨率
+        resolution0 =
+          (extent.xmax - extent.xmin) / numberOfMinLevelTilesX / tileSize;
+        // 开始计算分辨率
+        // 默认构造20级分辨率
+        for (let i = 0; i < 19; i++) {
+          const resolution = resolution0 / Math.pow(2, i);
+          const lod = {
+            level: i,
+            resolution,
+            scale: (mapUnitToMeters * (resolution * 96)) / 0.0254
+          };
+          lods.push(lod);
+        }
+        tileInfo = {
+          dpi: 96,
+          format: "PNG",
+          size: tileSize, // 瓦片宽高的像素大小
+          origin: {
+            coordinates: [180, -90], // 裁图原点
+            type: "Point"
           },
           lods: lods
         };
--- cesium/src/components/Layer/WebTile/WebTileLayer.vue
+++ cesium/src/components/Layer/WebTile/WebTileLayer.vue
@@ -6,19 +6,13 @@ import ServiceLayer from "../ServiceLayer";
 
 export default {
   name: "mapgis-3d-web-tile-layer",
+  inject: ["Cesium", "viewer", "vueCesium"],
+  mixins: [ServiceLayer],
   props: {
-    baseUrl: {
-      type: String,
-      default: ""
-    },
     spatialReference: {
       type: Object,
-      default: () => {}
+      default: () => {},
     },
-    srs: {
-      type: String,
-      default: "EPSG:4326"
-    }
   },
   data() {
     return {
@@ -27,16 +21,21 @@ export default {
         visible: "boolean",
         opacity: "number",
         zIndex: "number",
+        rectangle: "object",
+        tilingScheme: "object",
+        ellipsoid: "object",
+        tileWidth: "number",
+        tileHeight: "number",
+        maximumLevel: "number",
+        credit: "object|String",
         vueKey: "string",
-        vueIndex: "string | Number"
+        vueIndex: "string | Number",
       },
       managerName: "WebTileManager",
       // providerName: "BaiduImageryProvider", //cesium调用的类名
-      providerName: "UrlTemplateImageryProvider"
+      providerName: "UrlTemplateImageryProvider",
     };
   },
-  inject: ["Cesium", "vueCesium"],
-  mixins: [ServiceLayer],
   created() {},
   mounted() {
     this.mount();
@@ -45,21 +44,63 @@ export default {
     this.unmount();
   },
   methods: {
+    createCesiumObject() {
+      const { $props } = this;
+      const provider = new Cesium.UrlTemplateImageryProvider($props);
+      return new Cesium.ImageryLayer(provider);
+    },
     mount() {
+      const { baseUrl } = this;
+      //先处理相关参数：
       let options = {};
+      //如果spatialReference存在，则生成tilingScheme对象
       if (this.$props.spatialReference) {
         // 构造CustomTilingScheme
         options.tilingScheme = this.$_setTilingScheme(
           this.$props.spatialReference.wkid
         );
       }
-      this.$_mount(options);
+      const offset = this.$props.options.offset || 0;
+      let tag;
+      if (baseUrl.includes("{") && baseUrl.includes("}/")) {
+        const urlStrs = baseUrl.split("{");
+        tag = urlStrs[1].split("}/")[0];
+      }
+      if (tag) {
+        options.customTags = {};
+        options.customTags[tag] = function (imageryProvider, x, y, level) {
+          return level - offset;
+        };
+        //如果crs不存在，则默认生成4326的tilingScheme对象
+        if (!this.$props.spatialReference) {
+          options.tilingScheme = this.$_setTilingScheme("EPSG:4326");
+        }
+      }
+      let tempBaseUrl = baseUrl;
+      if (baseUrl.includes("format=")) {
+        const urlStrs = baseUrl.split("format=");
+        const strChilds = urlStrs[1].split("&");
+        if (
+          options.hasOwnProperty("extensions") &&
+          options.extensions.length > 0
+        ) {
+          options.extensions.push({
+            key: "format",
+            value: strChilds[0],
+          });
+        } else {
+          options.extensions = [{ key: "format", value: strChilds[0] }];
+        }
+        tempBaseUrl = urlStrs[0] + strChilds[1];
+      }
+      // options.minimumLevel = -2;
+      const allOptions = { ...options, baseUrl: tempBaseUrl };
+
+      this.$_mount(allOptions);
     },
     unmount() {
       this.$_unmount();
-    }
-  }
+    },
+  },
 };
 </script>
-
-<style scoped></style>
