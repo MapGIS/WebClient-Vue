2d33b38c5f650c0ccd61cd44f6ffc60aa908384f
yangkun <yangkun@zondycyber.com>
2024-11-07 17:22:58 +0800

    fix(6503): 地表自适应透明和地下模式使用最新的Cesium接口

--- cesium/src/components/SceneEffect/components/CameraSetting.vue
+++ cesium/src/components/SceneEffect/components/CameraSetting.vue
@@ -26,7 +26,7 @@
       <mapgis-ui-switch-panel
         size="default"
         label="影像透明度独立控制"
-        :checked="cameraSetting.independentTranslucency"
+        :checked="cameraSetting.undgrdParams.enableIndependentTranslucency"
         @changeChecked="enableIndependentTranslucency"
       ></mapgis-ui-switch-panel>
       <mapgis-ui-input-number-panel
@@ -45,7 +45,7 @@
       :value="cameraSetting.fov"
       :range="fovRange"
       :step="15"
-      @change="fovChange"
+      @change="setFOV"
     />
   </div>
 </template>
@@ -57,41 +57,61 @@ export default {
   name: "CameraSetting",
   mixins: [ServiceLayer],
   props: {
+    /**
+     * 外部传入的场景设置参数
+     * @type {Object}
+     * @default this.cameraSetting
+     * */
     initCameraSetting: {
       type: Object,
       default: () => {
         return this.cameraSetting;
-      },
+      }
     },
+    /**
+     * 所有参与地表半透明的模型的包围球合并后半径
+     * @type {Number}
+     * @default 0
+     * */
     boundingSphereRadius: {
       type: Number,
-      default: 0,
-    },
-    baseLayerIds: {
-      type: Array,
-      default: () => [],
-    },
+      default: 0
+    }
   },
   data() {
     return {
+      // 地表透明度的范围
       range: [0, 1],
+      // 相机开合角设置的范围
       fovRange: [0, 180],
+      // 默认的场景设置参数
       cameraSetting: {
+        // 是否开启地表自适应透明
         selfAdaption: false,
+        // 地表自适应透明设置
         selfAdaptionParams: {
-          maxHeigh: 400000,
+          // 自适应起始高度，自适应变化高度在[height, height * 5]，注意height取maxHeigh和boundingSphereRadius的最小值
+          maxHeigh: 400000
         },
+        // 是否开启地下模式
         undgrd: false,
-        // 是否开启图层透明度独立控制
-        independentTranslucency: false,
+        // 地下模式参数
         undgrdParams: {
+          // 地表透明度，0到1之间的值
           groundAlpha: 0.5,
+          // 是否开启图层透明度独立控制
+          enableIndependentTranslucency: false
         },
-        fov: 60,
-      },
+        // 相机开合角度
+        fov: 60
+      }
     };
   },
   watch: {
+    /**
+     * 外部传入的场景设置参数，传入后更新场景设置参数，并重新初始化设置
+     * @type {Object}
+     * */
     initCameraSetting: {
       handler(e) {
         this.cameraSetting = JSON.parse(JSON.stringify(this.initCameraSetting));
@@ -100,20 +120,27 @@ export default {
       deep: true,
       immediate: true,
     },
+    /**
+     * 更新场景设置参数后，发起事件通知父组件进行更新
+     * @type {Object}
+     * */
     cameraSetting: {
       handler(e) {
         this.$emit("updateCameraSetting", this.cameraSetting);
       },
-      deep: true,
+      deep: true
     },
-    // 如果boundingSphereRadius发生变化，重新设置地表自适应透明度
+    /**
+     * 如果boundingSphereRadius发生变化，重新设置地表自适应透明度
+     * @type {Object}
+     * */
     boundingSphereRadius: {
       handler(e) {
         this.boundingSphereRadius = e
         // 确保boundingSphereRadius大于0，才能设置地表自适应透明度，这个是一张图业务
         if (this.boundingSphereRadius > 0) {
           // 设置地表自适应透明度
-          this.setSelfAdaption()
+          this._setSelfAdaption()
           this.openSelfAdaptionPanel()
         }
       },
@@ -122,7 +149,7 @@ export default {
   },
   methods: {
     /**
-     * 初始化组件
+     * 初始化场景设置
      * */
     init() {
       if (!this.cameraSetting) {
@@ -144,17 +171,29 @@ export default {
       this.initImageLayersTranslucencyManager()
       // 确保boundingSphereRadius大于0，才能设置地表自适应透明度，这个是一张图业务
       if (this.boundingSphereRadius > 0) {
-        this.enableSelfAdaption(selfAdaption);
+        this._enableSelfAdaption(selfAdaption);
       }
-      this.fovChange(fov);
+      // 设置相机开合角度
+      this._setFOV(fov);
       // 初始化地下模式设置
-      this.enableUndgrd(undgrd);
+      this._enableUndgrd(undgrd);
     },
     /**
-     * 开启或关闭图层透明度独立控制，开启后图层透明度和地表透明度分开控制
-     * @param {Boolean} e 开启或关闭图层透明度独立控制
+     * 开启或关闭图层透明度独立控制，开启后图层透明度和地表透明度分开控制，会触发设置更新事件
+     * @param {Boolean} value 开启或关闭图层透明度独立控制
      * */
-    enableIndependentTranslucency(e) {
+    enableIndependentTranslucency(value) {
+      // 1 更新设置参数，并触发设置更新事件
+      this.cameraSetting.undgrdParams.enableIndependentTranslucency = value;
+      // 2 开启或关闭图层透明度独立控制
+      this._enableIndependentTranslucency(this.cameraSetting.undgrdParams.enableIndependentTranslucency)
+    },
+    /**
+     * 开启或关闭图层透明度独立控制，开启后图层透明度和地表透明度分开控制，不会触发设置更新事件
+     * @param {Boolean} value 开启或关闭图层透明度独立控制
+     * @private
+     * */
+    _enableIndependentTranslucency(value) {
       // 1 获取透明度工具
       const _independentTranslucency = this.getImageLayersTranslucencyManager();
 
@@ -162,11 +201,13 @@ export default {
       if (_independentTranslucency) {
         const imageryLayers = [];
         // 2.1 开启地表透明度独立控制
-        if (e) {
+        if (value) {
           // i从1开始，因为第0个是Cesium初始底图
           for (let i = 1; i < viewer.imageryLayers._layers.length; i++) {
             imageryLayers.push(viewer.imageryLayers._layers[i]);
           }
+          // 当imageryLayers数组的长度大于0，开启影像透明度独立控制，否则不开启，开启操做必须在设置透明度之前执行
+          _independentTranslucency.imageryLayers = imageryLayers;
           // 开启影像图层透明度独立控制，则通过globeFaceAlpha设置球体透明度
           _independentTranslucency.globeFaceAlpha = this.cameraSetting.undgrdParams.groundAlpha;
         }
@@ -175,38 +216,63 @@ export default {
           // 关闭开启影像图层透明度独立控制，则通过frontFaceAlpha和backFaceAlpha设置球体透明度
           _independentTranslucency.frontFaceAlpha = this.cameraSetting.undgrdParams.groundAlpha;
           _independentTranslucency.backFaceAlpha = this.cameraSetting.undgrdParams.groundAlpha;
+          // 当imageryLayers数组的长度等于0，关闭影像透明度独立控制
+          _independentTranslucency.imageryLayers = imageryLayers;
         }
-        // 2.3 当imageryLayers数组的长度大于0，开启影像透明度独立控制，否则不开启
-        _independentTranslucency.imageryLayers = imageryLayers;
       }
     },
     /**
-     * 开启或关闭地下模式，开启后地表半透明，视角可以穿梭到地下
-     * @param {Boolean} e 开启或关闭地下模式
+     * 更新二维影像图层独立控制影响的图层
      * */
-    enableUndgrd(e) {
-      this.cameraSetting.undgrd = e;
-      this.$emit("updateSpin", true);
-      let vm = this;
-      // 给一个延迟，方便外部给一个遮罩，由于是对外事件，不进行更改
-      setTimeout(function () {
-        if (vm.cameraSetting.undgrd) {
-          // 设置地表透明度
-          vm.setGlobeBackFaceAlpha(
-            vm.cameraSetting.undgrdParams.groundAlpha
-          );
-        } else {
-          // 不开启地表透明度，地表透明度应该为1
-          vm.setGlobeBackFaceAlpha(1);
-        }
-        vm.$emit("updateSpin", false);
-      }, 300);
+    updateIndependentTranslucency() {
+      this._enableIndependentTranslucency(this.cameraSetting.undgrdParams.enableIndependentTranslucency)
     },
     /**
-     * 地表自适应透明
-     * @param {Boolean} e 是否开启或关闭地表自适应透明
+     * 开启或关闭地下模式，开启后地表半透明，视角可以穿梭到地下，会触发设置更新事件
+     * @param {Boolean} value 开启或关闭地下模式
+     * */
+    enableUndgrd(value) {
+      // 1 更新设置
+      this.cameraSetting.undgrd = value;
+      // 2 开启或关闭地下模式
+      this._enableUndgrd(this.cameraSetting.undgrd)
+    },
+    /**
+     * 开启或关闭地下模式，开启后地表半透明，视角可以穿梭到地下，不会触发设置更新事件
+     * @param {Boolean} value 开启或关闭地下模式
+     * @private
+     * */
+    _enableUndgrd(value) {
+      const { viewer } = this
+      // 1 设置相机视角是否可以穿梭到地下
+      viewer.scene.screenSpaceCameraController.enableCollisionDetection = !this.cameraSetting.undgrd;
+      // 2 设置地表透明度
+      if (this.cameraSetting.undgrd) {
+        // 2.1 设置地表透明度
+        this._setGlobeBackFaceAlpha(
+          this.cameraSetting.undgrdParams.groundAlpha
+        );
+      } else {
+        // 2.2 不开启地表透明度，地表透明度应该为1
+        this._setGlobeBackFaceAlpha(1);
+      }
+    },
+    /**
+     * 开启或关闭地表自适应透明，会触发设置更新事件
+     * @param {Boolean} value 开启或关闭地表自适应透明
      */
-    enableSelfAdaption(e) {
+    enableSelfAdaption(value) {
+      // 1 更新设置
+      this.cameraSetting.selfAdaption = value;
+      // 2 开启或关闭地表自适应透明
+      this._enableSelfAdaption(this.cameraSetting.selfAdaption)
+    },
+    /**
+     * 开启或关闭地表自适应透明，不会触发设置更新事件
+     * @param {Boolean} value 开启或关闭地表自适应透明
+     * @private
+     */
+    _enableSelfAdaption(value) {
       // 1 确保boundingSphereRadius大于0，才能设置地表自适应透明度，这个是一张图业务
       if (this.boundingSphereRadius <= 0) {
         this.closeSelfAdaptionPanel()
@@ -217,18 +283,29 @@ export default {
       // 2 开启或关闭地表自适应透明
       let _independentTranslucency = this.getImageLayersTranslucencyManager();
       if (_independentTranslucency) {
-        if (e) {
-          this.setSelfAdaption();
+        if (value) {
+          this._setSelfAdaption();
         } else {
           this.closeSelfAdaption();
         }
       }
     },
     /**
-     * 设置地表透明度
-     * @param {Number} groundAlpha 地表透明度
+     * 设置地表透明度，会触发设置更新事件
+     * @param {Number} groundAlpha 地表透明度，0到1之间的值，0表示完全透明，1表示完全不透明
      */
     setGlobeBackFaceAlpha(groundAlpha) {
+      // 1 更新设置值
+      this.cameraSetting.undgrdParams.groundAlpha = groundAlpha
+      // 2 设置地表透明度
+      this._setGlobeBackFaceAlpha(this.cameraSetting.undgrdParams.groundAlpha)
+    },
+    /**
+     * 设置地表透明度，不会触发设置更新事件
+     * @param {Number} groundAlpha 地表透明度，0到1之间的值，0表示完全透明，1表示完全不透明
+     * @private
+     */
+    _setGlobeBackFaceAlpha(groundAlpha) {
       // 1 获取透明度工具
       const _independentTranslucency = this.getImageLayersTranslucencyManager()
 
@@ -246,20 +323,41 @@ export default {
       }
     },
     /**
-     * FOV设置
-     * @param {Number} e 相机的开合角度
+     * 设置相机的FOV值，会触发设置更新事件
+     * @param {Number} degree 相机的开合角度
      * */
-    fovChange(e) {
+    setFOV(degree) {
+      // 1 更新设置
+      this.cameraSetting.fov = degree;
+      // 2 设置相机的FOV值
+      this._setFOV(this.cameraSetting.fov)
+    },
+    /**
+     * 设置相机的FOV值，不会触发设置更新事件
+     * @param {Number} degree 相机的开合角度
+     * @private
+     * */
+    _setFOV(degree) {
       const { viewer, Cesium } = this;
-      this.cameraSetting.fov = e;
       viewer.scene.camera.frustum.fov = Cesium.Math.toRadians(
         this.cameraSetting.fov
       );
     },
     /**
-     * 设置地表自适应透明的值
+     * 设置地表自适应透明的值，会触发设置更新事件
+     * @param {Number} value
      * */
-    setSelfAdaption() {
+    setSelfAdaption(value) {
+      // 1 更新设置
+      this.cameraSetting.selfAdaptionParams.maxHeigh = value
+      // 2 设置地表自适应透明的值
+      this._setSelfAdaption()
+    },
+    /**
+     * 设置地表自适应透明的值，不会触发设置更新事件
+     * @private
+     * */
+    _setSelfAdaption() {
       // 1 获取地表自适应透明度的间隔高度，自适应透明度范围为[stepHeight, stepHeight * 5]
       const _stepHeight = this._getStepHeight();
 
@@ -312,7 +410,6 @@ export default {
     },
     /**
      * 获取控制地表透明度的工具对象
-     * @private
      * @return {Cesium.GlobeIndependentTranslucency} 控制地表透明度的工具对象
      * */
     getImageLayersTranslucencyManager() {
@@ -356,16 +453,4 @@ export default {
 </script>
 
 <style scoped>
-.dividerWrapper {
-  height: 13px;
-}
-.divider {
-  display: block;
-  height: 1px;
-  position: absolute;
-  left: 16px;
-  right: 16px;
-  margin: 6px 0;
-  background: #f0f0f0;
-}
 </style>
