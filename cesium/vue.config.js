9cb126b061e32441cd477c653e6533c7482f112a
gongyuejian <gongyuejian.000@mapgis.com>
2025-01-03 14:58:06 +0800

    update version to 17.4.0

--- cesium/vue.config.js
+++ cesium/vue.config.js
@@ -24,6 +24,7 @@ module.exports = {
       "@mapgis/mapbox-gl": "@mapgis/mapbox-gl",
       "@mapgis/supercluster": "@mapgis/supercluster",
       "@mapgis/webclient-cesium-plugin": "@mapgis/webclient-cesium-plugin",
+      "@mapgis/webclient-mapboxgl-plugin": "@mapgis/webclient-mapboxgl-plugin",
       "@mapgis/webclient-common": "@mapgis/webclient-common",
       "@mapgis/webclient-es6-service": "@mapgis/webclient-es6-service",
       "@mapgis/webclient-plot": "@mapgis/webclient-plot",
