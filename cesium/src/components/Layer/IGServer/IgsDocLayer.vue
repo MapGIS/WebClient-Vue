b0bf956e90206030543c1056a68e48b6226f469d
gongyuejian <gongyuejian.000@mapgis.com>
2025-01-13 16:25:24 +0800

    fix(23849): 一张图平台三位模式下加载地图服务渲染问题

--- cesium/src/components/Layer/IGServer/IgsDocLayer.vue
+++ cesium/src/components/Layer/IGServer/IgsDocLayer.vue
@@ -10,20 +10,20 @@ export default {
   props: {
     layers: {
       type: String,
-      default: null
+      default: null,
     },
     srs: {
       type: String,
-      default: "EPSG:4326"
-    }
+      default: "EPSG:4326",
+    },
   },
   watch: {
     layers: {
-      handler: function() {
+      handler: function () {
         this.unmount();
         this.mount();
-      }
-    }
+      },
+    },
   },
   data() {
     return {
@@ -33,8 +33,8 @@ export default {
         tileWidth: "number",
         tileHeight: "number",
         minimumLevel: "number",
-        maximumLevel: "number"
-      }
+        maximumLevel: "number",
+      },
     };
   },
   mounted() {
@@ -43,31 +43,19 @@ export default {
   methods: {
     mount() {
       //处理独有参数
-      let tilingScheme = this.$_setTilingScheme(this.srs),
-        extensions = [];
-      if (
-        this.srs === "EPSG:4326" ||
-        this.srs === "EPSG:4490" ||
-        this.srs === "EPSG:4610" ||
-        this.srs === "EPSG:4214"
-      ) {
-        extensions = [{ key: "proj", value: "WGS1984_度" }];
-      } else if (this.srs === "EPSG:3857") {
-        extensions = [{ key: "proj", value: "Web墨卡托_WGS1984" }];
-      }
+      const tilingScheme = this.$_setTilingScheme(this.srs);
       const baseUrl = this.$_initUrl("/igs/rest/mrms/docs/");
       this.$_mount({
         baseUrl: baseUrl,
         tilingScheme: tilingScheme,
-        extensions: extensions
       });
     },
     unmount() {
       this.$_unmount();
-    }
+    },
   },
   destroyed() {
     this.unmount();
-  }
+  },
 };
 </script>
