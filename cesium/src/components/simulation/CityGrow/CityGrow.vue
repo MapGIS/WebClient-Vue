ae3f6404b4865715b07b82268b10796d0c264293
chengwj <longrangerc@163.com>
2025-02-26 18:32:29 +0800

    feat(7228): 重构城市生长组件

--- cesium/src/components/simulation/CityGrow/CityGrow.vue
+++ cesium/src/components/simulation/CityGrow/CityGrow.vue
@@ -1,129 +1,104 @@
 <template>
-      <mapgis-ui-plot-timeline
-          v-model="sliderValue"
-          :curTimeWidth="curTimeWidth"
-          :max="maxSlider"
-          :min="minSlider"
-          :speed="speedValue"
-          :speedStep="1"
-          :minSpeed="minSpeed"
-          :maxSpeed="maxSpeed"
-          :interval="growInterval"
-          :intervalOptions="dataFields"
-          :tipFormatter="formatter"
-          :currentTime="String(formatDate(sliderValue))"
-          :forwardActive="playBtn"
-          :backActive="backBtn"
-          :pauseActive="suspendBtn"
-          :enableStart="false"
-          :enableEnd="false"
-          :disableBackward="true"
-          :disableForward="true"
-          :disablePause="true"
-          :loop="true"
-          @backward="backSetting"
-          @pause="suspendSetting"
-          @forward="playSetting"
-          @intervalChange="onFieldChange"
-          @speedChange="onChange"
-          @change="onSliderChange"
-      ></mapgis-ui-plot-timeline>
+  <mapgis-ui-plot-timeline v-model="sliderValue" :curTimeWidth="curTimeWidth" :max="maxSlider" :min="minSlider"
+    :speed="speedValue" :speedStep="1" :minSpeed="minSpeed" :maxSpeed="maxSpeed" :interval="growInterval"
+    :intervalOptions="dataFields" :tipFormatter="formatter" :currentTime="String(formatDate(sliderValue))"
+    :forwardActive="playBtn" :backActive="backBtn" :pauseActive="suspendBtn" :enableStart="false" :enableEnd="false"
+    :disableBackward="true" :disableForward="true" :disablePause="true" :loop="true" @backward="backSetting"
+    @pause="suspendSetting" @forward="playSetting" @intervalChange="onFieldChange" @speedChange="onChange"
+    @change="onSliderChange"></mapgis-ui-plot-timeline>
 </template>
 
 <script>
 import moment from "moment";
-import {deepEqual} from "../../Utils/deepequal";
-
+import { IGSFeatureLayer, Extent, PolygonSymbol3D, ExtrudeSymbol3DLayer, ColorMaterial, Color, RandomRenderer, TimeExtent } from "@mapgis/webclient-common";
+import { MapGISFeatureGridCollection } from "@mapgis/webclient-cesium-plugin";
 export default {
   name: "mapgis-3d-city-grow",
   inject: ["Cesium", "vueCesium", "viewer"],
   props: {
     baseUrl: {
       type: [String, Object],
-      default: null
+      default: null,
     },
     // --------------------矢量文档-----------------
     tileFeaturesCount: {
       type: Number,
-      default: 400
+      default: 400,
     },
     filter: {
       type: String,
-      default: null
+      default: null,
     },
     autoReset: {
       type: Boolean,
-      default: true
+      default: true,
     },
     featureStyle: {
       type: Object,
       default() {
-        return {}
-      }
+        return {};
+      },
     },
     formatType: {
       type: String,
-      default: "month"
-    }
+      default: "month",
+    },
+    // 城市生长属性
+    growAttributes: {
+      type: Object,
+      default: () => { }
+    },
   },
   watch: {
     featureStyle: {
       handler(next) {
         if (next.startTime) {
-          this.startTimeCopy = this.formatDate(next.startTime);
           this.minSlider = next.startTime;
+          this.sliderValue = this.minSlider
         }
         if (next.endTime) {
-          this.endTimeCopy = this.formatDate(next.endTime);
           this.maxSlider = next.endTime;
-        }
-        if (!deepEqual(next.colors, this.featureStyleCopy.colors)) {
-          this.unmount();
         }
         this.initCityGrowObject();
       },
       deep: true,
-      immediate: true
+      immediate: true,
     },
     formatType: {
       handler(next) {
         this.formatTypeCopy = next;
-      }
+      },
     },
-    speedValue:{
-      handler(next){
-        let vm = this;
-        if (vm.layer) {
-          vm.layer.setCityGrowPlayRate(next);
-        }
-          if (next > 0){
-            this.playBtn = true;
-            this.backBtn = false;
-            this.suspendBtn = false;
-          } else if (next<0){
-            this.playBtn = false;
-            this.backBtn = true;
-            this.suspendBtn = false;
-          } else {
-            this.playBtn = false;
-            this.backBtn = false;
-            this.suspendBtn = true;
-          }
-      }
-    },
-    playBtn:{
-      handler(next){
-        if (next){
+    speedValue: {
+      handler(next) {
+        if (next > 0) {
+          this.playBtn = true;
+          this.backBtn = false;
+          this.suspendBtn = false;
           this.startGrow()
+        } else if (next < 0) {
+          this.playBtn = false;
+          this.backBtn = true;
+          this.suspendBtn = false;
+          this.startGrow()
+        } else {
+          this.playBtn = false;
+          this.backBtn = false;
+          this.suspendBtn = true;
         }
-      }
+      },
     },
+    playBtn: {
+      handler(next) {
+        if (next) {
+          this.startGrow();
+        }
+      },
+    }
   },
   data() {
     return {
-      curTimeWidth:98,
-      startTimeCopy: '',
-      endTimeCopy: '',
+      curTimeWidth: 98,
       sliderValue: 0,
       // 城市生长对应图层
       layer: undefined,
@@ -136,25 +111,25 @@ export default {
         startTimeField: "startTime",
         endTimeField: "endTime",
         displayWithTile: false,
-        growTime: 60, //总播放时长 默认60秒  60秒演示完整个播放流程
+        growTime: 40, //总播放时长 默认40秒  40秒演示完整个播放流程
         updateInterval: 1, // 建筑生长的颜色和高度更新间隔，默认1s更新一次
         buildingsLimit: 200,
         heightRatio: 1,
-        colors: ["#fff0f6", "#ff85c0", "#eb2f96"]
+        colors: ["#fff0f6", "#ff85c0", "#eb2f96"],
       },
       info: "注意：颜色设置只能在城市生长未开启时设置，生长开启后设置颜色无效。",
       // 播放间隔
       growInterval: "月",
-      dataFields: ['年', '月', '日'],
+      dataFields: ["年", "月", "日"],
       minSpeed: -25,
       maxSpeed: 25,
       speedValue: 1,
-      formatTypeCopy: 'month',
+      formatTypeCopy: "month",
       clickBtn: false,
       backBtn: false,
       suspendBtn: false,
-      playBtn: true
-    }
+      playBtn: true,
+    };
   },
   created() {
     moment.locale();
@@ -172,110 +147,143 @@ export default {
     },
     unmount() {
       let vm = this;
-      if (this.Cesium.defined(vm.layerIndex)) {
-        this.viewer.scene.layers.removeVectorLayerByID(vm.layerIndex);
-      }
       vm.sliderValue = 0;
       vm.speedValue = 1;
       vm.playBtn = true;
       vm.suspendBtn = false;
     },
+    // 改变建筑生长的当前时间
+    changeTimeExtent(t) {
+      const { start, end } = this.featureGridCollection.timeInfo.fullTimeExtent
+      this.featureGridCollection.timeExtent = new TimeExtent({
+        start,
+        end: new Date(t)
+      })
+    },
+    // 初始化城市生长参数
     initCityGrowObject() {
       let vm = this;
       let url = vm.baseUrl;
       let colors = [];
       let times = [];
       let options = {};
-
       vm.featureStyleCopy = Object.assign(vm.featureStyleCopy, vm.featureStyle);
-      let cityGrowOptions = {
-        heightField: vm.featureStyleCopy.heightField,
-        heightRatio: vm.featureStyleCopy.heightRatio,
-        startTimeField: vm.featureStyleCopy.startTimeField,
-        endTimeField: vm.featureStyleCopy.endTimeField,
-        startTime: vm.featureStyleCopy.startTime,
-        endTime: vm.featureStyleCopy.endTime,
-        buildingsLimit: vm.featureStyleCopy.buildingsLimit,
-        updateInterval: vm.featureStyleCopy.updateInterval,
-        playTime: vm.featureStyleCopy.growTime,
-        colorSampling: false,
-        updateHeight: vm.featureStyleCopy.isGrowHeight,
-        replay: true,
-        displayWithTile: vm.featureStyleCopy.displayWithTile,
-        onReady: function (timeControl) {
-          let startTime = timeControl.startTime;
-          let endTime = timeControl.endTime;
-          vm.startTimeCopy = vm.formatDate(startTime);
-          vm.minSlider = startTime;
-          vm.endTimeCopy = vm.formatDate(endTime);
-          vm.maxSlider = endTime;
-          vm.startGrow();
-          // if (!vm.featureStyleCopy.startTime || !vm.featureStyleCopy.endTime){
-          //   vm.startGrow();
-          // }
-        },
-        onUpdate: function (currentPlayTime, currentTimeStamp) {
-          vm.timeSliderPlay(currentPlayTime, currentTimeStamp);
-        }
-      };
-      if (vm.featureStyleCopy.colors) {
-        for (let i = 0; i < vm.featureStyleCopy.colors.length; i++) {
-          let color = this.Cesium.Color.fromCssColorString(vm.featureStyleCopy.colors[i]);
-          colors.push(color);
-        }
-        cityGrowOptions.colors = colors
-      }
-      if (vm.featureStyleCopy.times) {
-        for (let j = 0; j < vm.featureStyleCopy.times.length; j++) {
-          let timestamp = new Date(vm.featureStyleCopy.times[j], 1, 1).valueOf() / 1000;
-          times.push(timestamp);
-        }
-        cityGrowOptions.times = times
-      }
+      const igsFeatureLayer = new IGSFeatureLayer({
+        url,
+        // url: 'http://192.168.82.91:8089/igs/rest/services/Map/深圳市白模/FeatureServer',
+      })
+      igsFeatureLayer.load().then(() => {
+        vm.featureGridCollection = vm.viewer.scene.primitives.add(new MapGISFeatureGridCollection({
+          debugGrid: vm.featureStyleCopy.isGrowHeight,
+          maxTileCacheCount: 300, // 最大缓存数
+          minimumLevel: 11, // 最小级别
+          idField: "FID",
+          updateInterval: 20, // 更新时间间隔
+          timeInfo: {
+            fullTimeExtent: {
+              start: new Date(Number(vm.growAttributes.startTime)), // 开始生长时间
+              end: new Date(Number(vm.growAttributes.endTime)) // 结束生长时间
+            },
+            startField: vm.featureStyleCopy.startTimeField, // 开始字段
+            endField: vm.featureStyleCopy.endTimeField, // 结束字段
+            interval: {
+              unit: 'milliseconds',
+              value: 1
+            }
+          },
+          timeExtent: {
+            start: new Date(Number(vm.growAttributes.startTime)),
+            end: new Date(Number(vm.growAttributes.startTime)),
+          },
+          queryFeaturesByGrid: (grid) => {
+            return igsFeatureLayer.queryFeatures({
+              sublayerId: '0',
+              spatialRel: 'Intersects',
+              geometry: Extent.fromJSON(grid.extent.toJSON()),
+              resultRecordCount: 40
+            }).then((res) => {
+              res.features.forEach((feature) => {
+                const featureStartTime = parseInt(feature.attributes['startTime'])
+                const featureEndTime = parseInt(feature.attributes['endTime'])
+                // 不同时间范围筛选出不同颜色的建筑
+                const styleItem = vm.featureStyle.colors.find(item => featureStartTime <= item.maxStamp && featureStartTime >= item.minStamp)
+                let colorArray
+                if (styleItem) {
+                  colorArray = styleItem.colorArray
+                } else {
+                  colorArray = [255, 255, 255, 1]
+                }
+                feature.attributes['endTime'] = featureEndTime
+                feature.attributes['startTime'] = featureStartTime
+                // 生产模型随机高度
+                feature.symbol = new PolygonSymbol3D({
+                  symbolLayers: [
+                    // 创建一个三维拉伸区符号突出
+                    new ExtrudeSymbol3DLayer({
+                      // 覆盖物颜色
+                      material: new ColorMaterial({
+                        color: new Color(colorArray[0], colorArray[1], colorArray[2], colorArray[3])
+                      }),
+                      // 拉伸高度
+                      size: Math.random() * 100 + 20
+                    }),
 
-      if (vm.featureStyleCopy.displayWithTile) {
-        options.tileFeaturesCount = vm.tileFeaturesCount;
-      } else {
-        cityGrowOptions.buildingsLimit = vm.featureStyleCopy.buildingsLimit;
-      }
-      let style = {
-        type: 'cityGrow',
-        styleOptions: cityGrowOptions
-      };
-      options = {autoReset: vm.autoReset, style: style};
+                  ]
+                })
+              })
+              return Promise.resolve(res.features)
+            })
 
-      vm.viewer.scene.layers.appendVectorLayer(url, {
-        ...options,
-        getDocLayerIndexes: function (indexs) {
-          vm.layerIndex = indexs[0];
-          vm.layer = vm.viewer.scene.layers.getLayer(vm.layerIndex);
-          vm.$emit("CityGrow", vm.layer);
-          vm.startGrow();
-        }
-      });
-      // }
+          }
+        }))
+        const { xmin, ymin, xmax, ymax } = igsFeatureLayer.extent
+        const rectangle = new Cesium.Rectangle.fromDegrees(
+          xmin,
+          ymin,
+          xmax,
+          ymax
+        );
+        // 飞到指定范围
+        this.viewer.camera.flyTo({
+          destination: rectangle
+        })
+        this.startGrow()
+      })
+
     },
-    //开始加载
+    // 移除城市生长
+    removeCityGrid() {
+      if (this.featureGridCollection) {
+        this.viewer.scene.primitives.remove(this.featureGridCollection)
+      }
+    },
+    // 开始城市生长
     startGrow() {
-      let vm = this;
-      if (vm.layer && vm.startTimeCopy && vm.endTimeCopy) {
-        vm.layer.cityGrowPlay();
+      const growTime = this.featureStyleCopy.growTime
+      const speedValue = this.speedValue
+      // 总的生长时间
+      const totalTime = parseFloat(growTime * 1000 / speedValue)
+      const start = Number(this.growAttributes.startTime)
+      const end = Number(this.growAttributes.endTime)
+      const step = parseFloat((end - start) / (totalTime / 20))
+      // 城市生长动画
+      const sliderAnimation = () => {
+        this.sliderValue += step
+        if (this.sliderValue >= end) {
+          this.sliderValue = end
+          cancelAnimationFrame(this.raf)
+        } else if (this.sliderValue <= start) {
+          this.sliderValue = start
+          cancelAnimationFrame(this.raf)
+        }
+        this.raf = requestAnimationFrame(sliderAnimation)
       }
-    },
-    //暂停生长
-    stopGrow() {
-      let vm = this;
-      if (vm.layer) {
-        vm.layer.cityGrowStop();
+      if (this.raf) {
+        cancelAnimationFrame(this.raf)
       }
+      sliderAnimation()
     },
-    //时间轴播放效果：
-    timeSliderPlay(currentPlayTime, currentTimeStamp) {
-      let vm = this;
-      //已知当前播放秒数和当前城市建筑的时间戳
-      //slider绑定的sliderValue随之增加
-      vm.sliderValue = currentTimeStamp;
-    },
+
     formatter(value) {
       let date = this.formatDate(value);
       return `${date}`;
@@ -291,79 +299,62 @@ export default {
         case "year":
           return y;
         case "month":
-          return y + '-' + this.addT(m);
+          return y + "-" + this.addT(m);
         case "day":
-          return y + '-' + this.addT(m) + '-' + this.addT(d);
+          return y + "-" + this.addT(m) + "-" + this.addT(d);
       }
       // 时间戳转时间 方法二：
       // return moment(timestamp).format("YYYY-MM-DD");
-
     },
     addT(m) {
-      return m < 10 ? '0' + m : m
+      return m < 10 ? "0" + m : m;
     },
     onSliderChange(e) {
-      let vm = this;
-      if (vm.layer && e) {
-        vm.layer.cityGrowSetTime(e, false);
-      }
+      this.changeTimeExtent(e);
     },
     onFieldChange(val) {
-      let vm = this
+      let vm = this;
       this.playBtn = true;
       this.backBtn = false;
       this.suspendBtn = false;
-      let startDate = new Date(vm.minSlider * 1000);
-      let endDate = new Date(vm.maxSlider * 1000);
-      let years = (endDate.getFullYear() - startDate.getFullYear());
       switch (val) {
         case "年":
-          this.formatTypeCopy = 'year';
-          vm.featureStyleCopy.growTime = years;
+          this.formatTypeCopy = "year";
+          vm.featureStyleCopy.growTime = 20;
           break;
         case "月":
-          this.formatTypeCopy = 'month';
-          let months = (years * 365) / 30;
-          vm.featureStyleCopy.growTime = months;
+          this.formatTypeCopy = "month";
+          vm.featureStyleCopy.growTime = 40;
           break;
         case "日":
-          this.formatTypeCopy = 'day';
-          vm.featureStyleCopy.growTime = (years * 365);
+          this.formatTypeCopy = "day";
+          vm.featureStyleCopy.growTime = 80;
           break;
       }
       this.unmount();
-      this.initCityGrowObject();
-    },
-    onSpeedChange(val) {
-      let vm = this;
-      let speed = parseFloat(val);
-      if (vm.layer) {
-        vm.layer.setCityGrowPlayRate(speed);
-      }
+      this.startGrow();
     },
     backSetting() {
-      this.startGrow();
       this.speedValue = -this.speedValue;
+      this.startGrow();
     },
     suspendSetting() {
-      const vm = this;
-      if (vm.layer) {
-        vm.layer.cityGrowStop();
+      if (this.raf) {
+        cancelAnimationFrame(this.raf)
       }
       this.playBtn = false;
       this.backBtn = false;
       this.suspendBtn = true;
     },
     playSetting() {
-      this.startGrow();
       this.speedValue = Math.abs(this.speedValue);
+      this.startGrow();
     },
     onChange(value) {
       this.speedValue = value;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
