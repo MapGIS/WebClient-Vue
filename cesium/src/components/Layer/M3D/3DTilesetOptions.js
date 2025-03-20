e22a030bf758d360474c7a37c768cc0cb968a82a
zhouzhenxiong <zhouzhenxiong.zzx@mapgis.com>
2025-03-20 14:35:57 +0800

    fix(24842):M3D白膜加载瞬间颜色异常

--- cesium/src/components/Layer/M3D/3DTilesetOptions.js
+++ cesium/src/components/Layer/M3D/3DTilesetOptions.js
@@ -15,7 +15,7 @@ export default {
    */
   vueIndex: {
     type: [String, Number],
-    default: () => (Math.random() * 100000000).toFixed(0)
+    default: () => (Math.random() * 100000000).toFixed(0),
   },
 
   // highlightStyle: {
@@ -27,7 +27,7 @@ export default {
 
   highlightStyle: {
     type: String,
-    default: "rgba(255, 255, 0, 0.6)"
+    default: "rgba(255, 255, 0, 0.6)",
   },
 
   /** @deprecated
@@ -65,7 +65,7 @@ export default {
     }
    */
   offset: {
-    type: Object
+    type: Object,
   },
 
   /**
@@ -79,7 +79,7 @@ export default {
     }
    */
   scale: {
-    type: Object
+    type: Object,
   },
 
   /**
@@ -160,7 +160,7 @@ export default {
   /* sphericalHarmonicCoefficients: { type: Array, default: undefined }, */
   specularEnvironmentMaps: { type: String, default: "" },
 
-  debugHeatmapTilePropertyName: { type: String, default: "" },
+  debugHeatmapTilePropertyName: { type: String },
   debugFreezeFrame: { type: Boolean, default: false },
   debugColorizeTiles: { type: Boolean, default: false },
   debugWireframe: { type: Boolean, default: false },
@@ -176,5 +176,5 @@ export default {
   // 但是优先级低于单个传入属性，即如果单个属性有传入值，优先使用传入的值，
   // 如果没有传入，但是extensions中有该属性，则使用extensions里对应的值
   // 修改者：龚跃健 2024/10/28
-  extensions: { type: Object }
+  extensions: { type: Object },
 };
