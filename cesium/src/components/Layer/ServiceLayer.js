ed658b7153514efe79657df1ed283148cf1e9eef
zhouzhenxiong <zhouzhenxiong.zzx@mapgis.com>
2025-03-27 15:51:14 +0800

    fix(24843):WMTS服务加载请求过多

--- cesium/src/components/Layer/ServiceLayer.js
+++ cesium/src/components/Layer/ServiceLayer.js
@@ -4,40 +4,40 @@ export default {
   props: {
     baseUrl: {
       type: String,
-      default: null
+      default: null,
     },
     domain: {
       type: String,
-      default: null
+      default: null,
     },
     protocol: {
       type: String,
-      default: "http://"
+      default: "http://",
     },
     ip: {
       type: String,
-      default: null
+      default: null,
     },
     port: {
       type: String,
-      default: null
+      default: null,
     },
     serverName: {
       type: String,
-      default: null
+      default: null,
     },
     layerStyle: {
       type: Object,
-      default: function() {
+      default: function () {
         return {
           visible: true,
-          opacity: 1
+          opacity: 1,
         };
-      }
+      },
     },
     id: { type: String, default: "" },
     token: {
-      type: Object
+      type: Object,
     },
     options: {
       type: Object,
@@ -52,20 +52,20 @@ export default {
           enablePickFeatures: false,
           minimumLevel: 0,
           maximumLevel: 20,
-          credit: undefined
+          credit: undefined,
         };
-      }
+      },
     },
     vueKey: {
       type: String,
-      default: "default"
+      default: "default",
     },
     vueIndex: {
       type: Number,
       default() {
         return Number((Math.random() * 100000000).toFixed(0));
-      }
-    }
+      },
+    },
   },
   data() {
     return {
@@ -87,12 +87,12 @@ export default {
       **/
       checkType: undefined,
       layerId: undefined,
-      optionsBack: undefined
+      optionsBack: undefined,
     };
   },
   watch: {
     layerStyle: {
-      handler: function() {
+      handler: function () {
         let { vueKey, vueIndex } = this;
         let layer = window.vueCesium[this.managerName].findSource(
           vueKey,
@@ -112,10 +112,10 @@ export default {
         }
         this.layerStyleCopy = { ...this.layerStyle };
       },
-      deep: true
+      deep: true,
     },
     options: {
-      handler: function() {
+      handler: function () {
         let vm = this;
         let isEqual = this.$_isEqual(vm.options, vm.optionsBack);
         if (!isEqual) {
@@ -124,18 +124,18 @@ export default {
           this.optionsBack = this.options;
         }
       },
-      deep: true
+      deep: true,
     },
     id: {
-      handler: function() {
+      handler: function () {
         const { vueIndex, vueKey } = this;
         let layer = window.vueCesium[this.managerName].findSource(
           vueKey,
           vueIndex
         );
         layer.source.id = this.id;
-      }
-    }
+      },
+    },
   },
   mounted() {
     this.optionsBack = this.options;
@@ -181,7 +181,7 @@ export default {
 
       //取得除options、layerStyle和id之外的必要参数
       const { $props, vueIndex, vueKey } = this;
-      Object.keys($props).forEach(function(key) {
+      Object.keys($props).forEach(function (key) {
         if (key !== "options" && key !== "layerStyle" && key !== "id") {
           opt[key] = $props[key];
         }
@@ -198,15 +198,56 @@ export default {
           ) {
             options.extensions.push({
               key: this.token.key,
-              value: this.token.value
+              value: this.token.value,
             });
           } else {
             options.extensions = [
-              { key: this.token.key, value: this.token.value }
+              { key: this.token.key, value: this.token.value },
             ];
           }
         } else if (this.token.value) {
           options.baseUrl += "?" + this.token.key + "=" + this.token.value;
+        }
+      }
+
+      // 将rectangle转为cesium中的rectangle对象
+      if (options.rectangle) {
+        const {
+          rectangle: { xmin, ymin, xmax, ymax },
+          srs,
+        } = options;
+        // 对rectangle中的数据进行坐标系判断，将数据装换成cesium中的rectangle对象 4326/3857
+        if (srs === "EPSG:4326") {
+          options.rectangle = Cesium.Rectangle.fromDegrees(
+            rectangle.xmin,
+            rectangle.ymin,
+            rectangle.xmax,
+            rectangle.ymax
+          );
+        } else if (srs === "EPSG:102100" || srs === "EPSG:3857") {
+          const projectInfo = zondy.geometry.Projection.project(
+            new zondy.geometry.Extent({
+              xmin,
+              ymin,
+              xmax,
+              ymax,
+              spatialReference: new zondy.SpatialReference({
+                wkid: 3857,
+              }),
+            }),
+            new zondy.SpatialReference({
+              wkid: 4326,
+            })
+          ).toJSON();
+          options.rectangle = Cesium.Rectangle.fromDegrees(
+            projectInfo.xmin,
+            projectInfo.ymin,
+            projectInfo.xmax,
+            projectInfo.ymax
+          );
+        } else {
+          // 其他坐标系先不考虑，先将rectangle属性移除，防止因rectangle属性问题导致图层不加载
+          delete options.rectangle;
         }
       }
 
@@ -283,7 +324,7 @@ export default {
 
       let manageOptions = {
         zIndex: providerZIndex,
-        id: imageryLayer.id
+        id: imageryLayer.id,
       };
 
       //如果providerZIndex为0，表示初始化地图时，没有设置zIndex，因此会按照初始化的顺序向上叠放
@@ -364,7 +405,7 @@ export default {
       }
       return {
         currentLayer: currentLayer,
-        index: index
+        index: index,
       };
     },
     $_getLayers() {
@@ -372,7 +413,7 @@ export default {
         vm = this;
 
       //遍历window.vueCesium下所有的Manager
-      Object.keys(window.vueCesium).forEach(function(key) {
+      Object.keys(window.vueCesium).forEach(function (key) {
         if (key.indexOf("Manager") > -1 && key !== "GlobesManager") {
           //取出含有与webScene组件相同vueKey的Manager对象
           if (window.vueCesium[key].hasOwnProperty("vueKey")) {
@@ -396,7 +437,7 @@ export default {
       });
 
       //对数组进行排序
-      Layers.sort(function(a, b) {
+      Layers.sort(function (a, b) {
         if (a.options && b.options) {
           return a.options.zIndex - b.options.zIndex;
         }
@@ -638,7 +679,7 @@ export default {
     $_checkProps(checkObj, checkType) {
       let vm = this;
       if (checkObj && checkType) {
-        Object.keys(checkObj).forEach(function(key) {
+        Object.keys(checkObj).forEach(function (key) {
           let result;
           if (checkType.hasOwnProperty(key) && typeof key === "string") {
             result = vm.$_checkValue(checkObj, key, checkType[key]);
@@ -716,7 +757,7 @@ export default {
         let customWKID;
         let axisDirection = {
           x: 1,
-          y: -1
+          y: -1,
         };
         let rectangleSouthwest;
         let rectangleNortheast;
@@ -724,7 +765,7 @@ export default {
         if ([CustomWKID.bd09ll, CustomWKID.bd09mc].indexOf(customWKID) > -1) {
           axisDirection = {
             x: 1,
-            y: 1
+            y: 1,
           };
           rectangleSouthwest = new Cesium.Cartesian2(
             -20037726.37,
@@ -745,7 +786,7 @@ export default {
           axisDirection: axisDirection,
           rectangleSouthwest: rectangleSouthwest,
           rectangleNortheast: rectangleNortheast,
-          tileInfo: tileInfo
+          tileInfo: tileInfo,
         });
       } else {
         tilingScheme = new Cesium.GeographicTilingScheme();
@@ -791,9 +832,9 @@ export default {
           size: tileSize, // 瓦片宽高的像素大小
           origin: {
             coordinates: [0, 0], // 裁图原点
-            type: "Point" // 裁图原点类型
+            type: "Point", // 裁图原点类型
           },
-          lods: lods
+          lods: lods,
         };
       } else if (wkid === 20010202) {
         // 构建自定义Wkid 国测局02墨卡托的默认TileInfo
@@ -812,16 +853,16 @@ export default {
           size: tileSize, // 瓦片宽高的像素大小
           origin: {
             coordinates: [-maxLength, maxLength], // 裁图原点
-            type: "Point" // 裁图原点类型
+            type: "Point", // 裁图原点类型
           },
-          lods: lods
+          lods: lods,
         };
       } else if (wkid === 20020901 || wkid === 20010201) {
         const extent = {
           xmin: -180,
           ymin: -90,
           xmax: 180,
-          ymax: 90
+          ymax: 90,
         };
         const numberOfMinLevelTilesX = 2;
         tileSize = 256;
@@ -836,7 +877,7 @@ export default {
           const lod = {
             level: i,
             resolution,
-            scale: (mapUnitToMeters * (resolution * 96)) / 0.0254
+            scale: (mapUnitToMeters * (resolution * 96)) / 0.0254,
           };
           lods.push(lod);
         }
@@ -846,12 +887,12 @@ export default {
           size: tileSize, // 瓦片宽高的像素大小
           origin: {
             coordinates: [180, -90], // 裁图原点
-            type: "Point"
+            type: "Point",
           },
-          lods: lods
+          lods: lods,
         };
       }
       return tileInfo;
-    }
-  }
+    },
+  },
 };
