874c13e71953c3957730bda7d5c1ad8364931568
gongyuejian <gongyuejian.000@mapgis.com>
2025-04-07 11:46:36 +0800

    fix(25284): JC-加载3DTiles地图服务报错

--- cesium/src/components/Layer/M3D/3dTileset.vue
+++ cesium/src/components/Layer/M3D/3dTileset.vue
@@ -140,43 +140,44 @@ export default {
       const { luminanceAtZenith, maximumMemoryUsage } = this;
       if (viewer.isDestroyed()) return;
       const options = this.getOptions();
-      const tilesetLayer = new zondy.layer.Cesium3DTilesCacheLayer({
-        // 服务基地址
-        url,
-        ...options,
-        extensionOptions: { ...options },
-      });
-      const cesiumOptions = initializeOptions(tilesetLayer, viewer);
-      zondy.cesium.Cesium3DTileset.fromUrl(url, cesiumOptions).then(
-        (tileset) => {
-          if (!tileset) {
-            return;
-          }
-          tileset.imageBasedLighting.luminanceAtZenith = luminanceAtZenith;
-          tileset.cacheBytes = maximumMemoryUsage;
-          if (options.autoReset) {
-            const boundingSphere = tileset.boundingSphere;
-            const orientation = new Cesium.HeadingPitchRange(
-              0.0,
-              -0.5,
-              boundingSphere.radius * 2.5
-            );
-            viewer.camera.flyToBoundingSphere(boundingSphere, {
-              duration: 0,
-              offset: orientation,
-            });
-          }
-          viewer.scene.primitives.add(tileset);
-          tileset.style = new Cesium.Cesium3DTileStyle({
-            color: `color('#FFFFFF', ${opacity})`,
-          });
-          vueCesium.Tileset3DManager.addSource(vueKey, vueIndex, tileset, {
-            url: url,
-          });
-          vm.$emit("loaded", { tileset: tileset, m3ds: [tileset] });
-          vm.bindPopupEvent();
+      console.log("options: ", options);
+      // const tilesetLayer = new zondy.layer.Cesium3DTilesCacheLayer({
+      //   // 服务基地址
+      //   url,
+      //   ...options,
+      //   extensionOptions: { ...options },
+      // });
+      // tilesetLayer.load().then((layer) => {
+      // const cesiumOptions = initializeOptions(layer, viewer);
+      zondy.cesium.Cesium3DTileset.fromUrl(url, options).then((tileset) => {
+        if (!tileset) {
+          return;
         }
-      );
+        tileset.imageBasedLighting.luminanceAtZenith = luminanceAtZenith;
+        tileset.cacheBytes = maximumMemoryUsage;
+        if (options.autoReset) {
+          const boundingSphere = tileset.boundingSphere;
+          const orientation = new Cesium.HeadingPitchRange(
+            0.0,
+            -0.5,
+            boundingSphere.radius * 2.5
+          );
+          viewer.camera.flyToBoundingSphere(boundingSphere, {
+            duration: 0,
+            offset: orientation,
+          });
+        }
+        viewer.scene.primitives.add(tileset);
+        tileset.style = new Cesium.Cesium3DTileStyle({
+          color: `color('#FFFFFF', ${opacity})`,
+        });
+        vueCesium.Tileset3DManager.addSource(vueKey, vueIndex, tileset, {
+          url: url,
+        });
+        vm.$emit("loaded", { tileset: tileset, m3ds: [tileset] });
+        vm.bindPopupEvent();
+      });
+      // });
     },
     /**
      * @description 清空组件
