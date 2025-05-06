c82341272c3211a10959285df12431d4faf990bc
zhouzhenxiong <zhouzhenxiong.zzx@mapgis.com>
2025-05-06 16:54:20 +0800

    fix(25880):【场景设置】无论是否开启鼠标控制模式，都可以通过鼠标和键盘快捷键漫游

--- cesium/src/components/SceneEffect/AutonomousRoaming/components/AutonomousRoaming.vue
+++ cesium/src/components/SceneEffect/AutonomousRoaming/components/AutonomousRoaming.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="autonomous-roaming">
     <mapgis-ui-switch-row-left
-      title="鼠标控制模式"
-      :disabled="isRunning"
-      v-model="roamingSettings.enableMouseLook"
-      @change="enableEnableMouseLook"
-    />
-    <mapgis-ui-switch-row-left
       title="碰撞检测"
       v-model="roamingSettings.enableCollisionDetection"
       @change="enableCollisionDetectionChange"
@@ -27,19 +21,12 @@
 export default {
   name: "AutonomousRoaming",
   inject: ["Cesium", "vueCesium", "viewer"],
-  props: {
-    isRunning: {
-      type: Boolean,
-      default: false
-    }
-  },
   data() {
     return {
       roamingSettings: {
         enableCollisionDetection: false,
-        enableMouseLook: false,
-        moveStep: 1
-      }
+        moveStep: 1,
+      },
     };
   },
 
@@ -51,15 +38,11 @@ export default {
   },
   methods: {
     mount() {
-      const {
-        enableCollisionDetection,
-        enableMouseLook,
-        moveStep
-      } = this.roamingSettings;
+      const { enableCollisionDetection, moveStep } = this.roamingSettings;
       this.roamingTool = new zondy.cesium.KeyboardCameraController(viewer, {
         // 是否开启鼠标控制模式，开启后将进入全屏模式，此时相机视角可通过鼠标移动进行操作
         // 由于浏览器限制，不允许自动全屏，需通过用户操作切换全屏，所以这里初始化设置为false
-        enableMouseLook: enableMouseLook,
+        enableMouseLook: false,
         // 鼠标灵敏度，该参数仅在鼠标控制模式下有效
         mouseSensitivity: 6,
         // 是否开启动态移动步长，开启后将根据视角距离地面高度动态计算移动步长，moveStep参数将不再生效
@@ -69,7 +52,7 @@ export default {
         // 旋转步长，单位为度
         rotateStep: 1,
         // 是否开启碰撞检测
-        enableCollisionDetection: enableCollisionDetection
+        enableCollisionDetection: enableCollisionDetection,
         // 初始视角，开始漫游时视角将重置到该视角，若不设置则默认从当前视角开始漫游
         // initView: initViewOfIndoor
       });
@@ -87,10 +70,7 @@ export default {
     moveStepChange(val) {
       this.roamingTool.moveStep = val;
     },
-    enableEnableMouseLook(val) {
-      this.roamingTool.enableMouseLook = val;
-    }
-  }
+  },
 };
 </script>
 
