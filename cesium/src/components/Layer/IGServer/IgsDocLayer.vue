83a1457e89d0669a6a79835493103809c9392200
gongyuejian <gongyuejian.000@mapgis.com>
2025-09-08 13:45:32 +0800

    ffix: 三维视图支持加载4537地图服务和栅格瓦片服务

--- cesium/src/components/Layer/IGServer/IgsDocLayer.vue
+++ cesium/src/components/Layer/IGServer/IgsDocLayer.vue
@@ -79,22 +79,23 @@ export default {
       const igsMapImageLayer = new IGSMapImageLayer({
         url: baseUrl,
         renderMode: "image",
-        spatialReference: new SpatialReference({ wkid }),
         sublayers,
       });
       const self = this;
       igsMapImageLayer.load().then((layer) => {
-        // 获取provider的初始化参数
-        const cesiumOptions = initializeOptions(layer, viewer);
-        const { rectangle } = cesiumOptions;
-        if (rectangle) {
-          const { west, south, east, north } = rectangle;
-          // 如果范围无效，则不加载
-          if (west >= east || south >= north) {
-            return;
+        if(layer.loaded){
+          // 获取provider的初始化参数
+          const cesiumOptions = initializeOptions(layer, viewer);
+          const { rectangle } = cesiumOptions;
+          if (rectangle) {
+            const { west, south, east, north } = rectangle;
+            // 如果范围无效，则不加载
+            if (west >= east || south >= north) {
+              return;
+            }
           }
+          self.$_mount(cesiumOptions);
         }
-        self.$_mount(cesiumOptions);
       });
       // 如果是一张图出图，那么providerName为MapGISMapServer一张图出图provider
       if (this.renderMode && this.renderMode === "image-map") {
