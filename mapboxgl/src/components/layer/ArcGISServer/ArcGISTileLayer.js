81229096a6706ed80d628f4d9435e1a5fcbe6c3d
gongyuejian <gongyuejian.000@mapgis.com>
2025-10-31 10:43:14 +0800

    fix(28143):【数据目录】MapGIS IGServer发布arcgis服务，一张图中通过瓦片服务方式调用，服务显示范围不对 问题原因: arcgis发布的服务，默认情况下需要设置zoomOffset=-1，这个在代码中已添加；但是通过igs转发的，则不需要这个设置。 修改说明: 组件层使用common的接口判断服务是否为ArcGIS发布的服务 接口变动: 否 同步版本: 10.7.6.10、dev 统查统改: 是，未发现同类问题

--- mapboxgl/src/components/layer/ArcGISServer/ArcGISTileLayer.js
+++ mapboxgl/src/components/layer/ArcGISServer/ArcGISTileLayer.js
@@ -1,5 +1,6 @@
 import rasterLayer from "../RasterLayer";
 import layerEvents from "../../../lib/layerEvents";
+import { ServerBaseUrlRegExp } from "@mapgis/webclient-common";
 
 export default {
   name: "mapgis-arcgis-tile-layer",
@@ -38,7 +39,10 @@ export default {
         this._zoomOffset = zoomOffset;
         this._url = this.baseUrl + "/tile/{z}/{y}/{x}";
         if (this.map.getCRS().epsgCode.includes("4326")) {
-          if (!this.baseUrl.includes("/igs/") && zoomOffset == 0) {
+          if (
+            zoomOffset == 0 &&
+            ServerBaseUrlRegExp.testByRegExpNames(this.url, ["ArcGISMapServer"])
+          ) {
             // 这个地方会导致4326无法主动传入offset=0的情况，但是默认的arcgis
             // 测试10.3 10.5 10.7后发现arcigs默认情况下就是offset=-1,
             // 因此忽略主动传入0的场景. 这种情况只会发生在操作arcserver的时候
